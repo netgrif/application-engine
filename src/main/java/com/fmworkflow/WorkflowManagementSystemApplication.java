02fdfc1783bc05d4e9be2330ef2db6b726165b97
Juraj Mažári <mazari@netgrif.com>
2017-01-23 23:33:36 +0100

    basic project structure

---
+++ src/main/java/com/fmworkflow/WorkflowManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.fmworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowManagementSystemApplication.class, args);
+	}
+}
