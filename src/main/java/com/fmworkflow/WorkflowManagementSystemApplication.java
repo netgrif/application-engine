3242ddea337aeebafb705c155bbbfd880e95fb28
Juraj Mažári <mazari.juraj@gmail.com>
2017-01-28 20:10:57 +0100

    WMS-15 Mail sending service refactor mail configuration

--- src/main/java/com/fmworkflow/WorkflowManagementSystemApplication.java
+++ src/main/java/com/fmworkflow/WorkflowManagementSystemApplication.java
@@ -2,10 +2,8 @@ package com.fmworkflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.fmworkflow", "it.ozimov.springboot"})
 public class WorkflowManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkflowManagementSystemApplication.class, args);
