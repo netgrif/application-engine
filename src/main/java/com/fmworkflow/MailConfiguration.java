47902490d1624e54ffd188eeec6e0b2566a5a7f2
Juraj Mažári <mazari@netgrif.com>
2017-01-28 18:15:24 +0000

    WMS-15 Mail sending service refactor mail configuration to separate file

---
+++ src/main/java/com/fmworkflow/MailConfiguration.java
@@ -0,0 +1,45 @@
+package com.fmworkflow;
+
+import com.fmworkflow.mail.IMailService;
+import com.fmworkflow.mail.MailService;
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+    @Bean
+    public JavaMailSenderImpl mailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setPort(587);
+        sender.setHost("smtp.mailgun.org");
+        sender.setUsername("workflow@sandbox77b26b1bdb5a40439459609a0292b926.mailgun.org");
+        sender.setPassword("workflow");
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", true);
+        mailProperties.put("mail.smtp.starttls.enable", true);
+        mailProperties.put("mail.smtp.starttls.required", true);
+        sender.setJavaMailProperties(mailProperties);
+        return sender;
+    }
+
+    @Bean
+    public VelocityEngine velocityEngine() {
+        VelocityEngine engine = new VelocityEngine();
+        engine.setProperty("resource.loader", "class");
+        engine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        return engine;
+    }
+
+    @Bean
+    public IMailService mailService(VelocityEngine velocityEngine, JavaMailSender mailSender) {
+        MailService mailService = new MailService();
+        mailService.setVelocityEngine(velocityEngine);
+        mailService.setMailSender(mailSender);
+        return mailService;
+    }
+}
