53d4e54956c2e50635f10938898bcd6b73326e06
Juraj Mažári <mazari.juraj@gmail.com>
2017-01-29 17:21:18 +0100

    WMS-12 - User login enable Spring Cache, Batch, Security and Session configure Session and Security

--- src/main/java/com/fmworkflow/WorkflowManagementSystemApplication.java
+++ src/main/java/com/fmworkflow/WorkflowManagementSystemApplication.java
@@ -2,9 +2,20 @@ package com.fmworkflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@EnableCaching
 @SpringBootApplication
+@Controller
 public class WorkflowManagementSystemApplication {
+
+	@RequestMapping(value = "/{path:[^\\.]*}")
+	public String redirect() {
+		return "forward:/";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkflowManagementSystemApplication.class, args);
 	}
---
+++ src/main/java/com/fmworkflow/configuration/SessionConfiguration.java
@@ -0,0 +1,16 @@
+package com.fmworkflow.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+@Configuration
+public class SessionConfiguration {
+
+    @Bean
+    public JedisConnectionFactory lettuceConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+}
