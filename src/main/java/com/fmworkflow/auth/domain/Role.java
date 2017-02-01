43488f912c2e317a4452cb75b5dbde14cf8c038e
Milan Mladoniczky <milan.befinger@gmail.com>
2017-02-01 14:37:03 +0100

    WMS-12 - User login fixed empty constructor on User model add small changes for dev

--- src/main/java/com/fmworkflow/auth/domain/Role.java
+++ src/main/java/com/fmworkflow/auth/domain/Role.java
@@ -1,13 +1,16 @@
 package com.fmworkflow.auth.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Entity
+@Table(name = "role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotNull
     private String name;
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
