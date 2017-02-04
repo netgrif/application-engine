4a133e68227cc582d1ef750a0ab79066bf15f139
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-02-04 22:09:16 +0100

    Move RequestBody inner classes into separate classes

---
+++ src/main/java/com/fmworkflow/auth/web/requestbodies/RegistrationRequest.java
@@ -0,0 +1,15 @@
+package com.fmworkflow.auth.web.requestbodies;
+
+/**
+ * Created by Milan on 4.2.2017.
+ */
+public class RegistrationRequest {
+
+    public String token;
+    public String email;
+    public String name;
+    public String surname;
+    public String password;
+
+    public RegistrationRequest() {}
+}
