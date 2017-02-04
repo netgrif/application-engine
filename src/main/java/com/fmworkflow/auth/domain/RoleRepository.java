6c66ec01feb954fd200ce4da8540f8d860f5350b
Milan Mladoniczky <milan.befinger@gmail.com>
2017-02-04 22:53:25 +0100

    WMS-11 - User registration - add role 'user' to newly registrated user

--- src/main/java/com/fmworkflow/auth/domain/RoleRepository.java
+++ src/main/java/com/fmworkflow/auth/domain/RoleRepository.java
@@ -3,4 +3,5 @@ package com.fmworkflow.auth.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
