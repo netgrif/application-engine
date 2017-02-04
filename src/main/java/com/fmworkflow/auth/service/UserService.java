8f61a2645a517058a6cb01f6331d4aa2b6f35762
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-02-04 22:53:25 +0100

    WMS-11 - User registration - add role 'user' to newly registrated user

--- src/main/java/com/fmworkflow/auth/service/UserService.java
+++ src/main/java/com/fmworkflow/auth/service/UserService.java
@@ -1,21 +1,32 @@
 package com.fmworkflow.auth.service;
 
+import com.fmworkflow.auth.domain.Role;
+import com.fmworkflow.auth.domain.RoleRepository;
 import com.fmworkflow.auth.domain.User;
 import com.fmworkflow.auth.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+
 @Service
 public class UserService implements IUserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        if(user.getRoles().isEmpty()) {
+            HashSet<Role> roles = new HashSet<Role>();
+            roles.add(roleRepository.findByName("user"));
+            user.setRoles(roles);
+        }
         userRepository.save(user);
     }
 
