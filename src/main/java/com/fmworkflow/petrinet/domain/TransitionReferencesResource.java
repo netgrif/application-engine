994af9b5b69496dcc6ba39941963207aae4f6c5b
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-02-21 16:18:36 +0100

    WMS-10 - Task management screen - add transition references resource for autocomplete search

---
+++ src/main/java/com/fmworkflow/petrinet/domain/TransitionReferencesResource.java
@@ -0,0 +1,22 @@
+package com.fmworkflow.petrinet.domain;
+
+import com.fmworkflow.petrinet.web.PetriNetController;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
+import java.util.ArrayList;
+
+
+public class TransitionReferencesResource extends Resources<TransitionReference> {
+
+    public TransitionReferencesResource(Iterable<TransitionReference> content) {
+        super(content, new ArrayList<Link>());
+        buildLinks();
+    }
+
+    private void buildLinks(){
+        add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(PetriNetController.class)
+                .getTransitionReferences(new ArrayList<>())).withSelfRel());
+    }
+}
