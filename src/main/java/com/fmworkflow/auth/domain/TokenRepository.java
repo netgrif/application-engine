960306917d469d3aa62bbb92de6524ad9c8e4a11
Juraj Mažári <mazari@netgrif.com>
2017-02-27 17:04:52 +0100

    WMS-79 - Replace Joda time

--- src/main/java/com/fmworkflow/auth/domain/TokenRepository.java
+++ src/main/java/com/fmworkflow/auth/domain/TokenRepository.java
@@ -3,12 +3,12 @@ package com.fmworkflow.auth.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Transactional
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Token findByEmail(String email);
     Token findByHashedToken(String token);
-    List<Token> removeByExpirationDateBefore(Date now);
+    List<Token> removeByExpirationDateBefore(LocalDateTime now);
 }
