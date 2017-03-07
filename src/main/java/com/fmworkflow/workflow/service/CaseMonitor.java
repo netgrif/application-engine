5e81e4a1d80d1431e4369ad99ee0321e31537b56
Juraj Mažári <mazari@netgrif.com>
2017-03-07 16:12:02 +0100

    add aspect CaseMonitor

---
+++ src/main/java/com/fmworkflow/workflow/service/CaseMonitor.java
@@ -0,0 +1,19 @@
+package com.fmworkflow.workflow.service;
+
+import com.fmworkflow.workflow.domain.Case;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CaseMonitor {
+    @AfterReturning(
+            pointcut = "execution(* com.fmworkflow.workflow.domain.CaseRepository.findOne(..))",
+            returning= "result")
+    public void afterFindOne(JoinPoint joinPoint, Object result) {
+        Case useCase = (Case) result;
+        useCase.getPetriNet().initializeArcs();
+    }
+}
