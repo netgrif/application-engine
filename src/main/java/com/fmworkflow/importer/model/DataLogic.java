a7e6ea90e0edb19c0d090c6e36f9dc2e370841e1
Juraj Mažári <mazari@netgrif.com>
2017-03-09 16:41:58 +0100

    WMS-83 - Role logic - assign to other add assignToOther logic function

---
+++ src/main/java/com/fmworkflow/importer/model/DataLogic.java
@@ -0,0 +1,11 @@
+package com.fmworkflow.importer.model;
+
+public interface DataLogic {
+    public Boolean getEditable();
+
+    public void setEditable(Boolean editable);
+
+    public Boolean getVisible();
+
+    public void setVisible(Boolean visible);
+}
