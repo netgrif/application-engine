c958a1517cfdc8f0519a312beaface8a7af0a823
Juraj Mažári <mazari@netgrif.com>
2017-03-16 11:25:48 +0100

    WMS-100 - Remove old token added deleteAllByEmail method

--- src/main/java/com/fmworkflow/auth/service/ITokenService.java
+++ src/main/java/com/fmworkflow/auth/service/ITokenService.java
@@ -4,6 +4,7 @@ import com.fmworkflow.auth.domain.Token;
 
 public interface ITokenService {
     boolean authorizeToken(String email, String token);
+
     void removeExpired();
 
     Token createToken(String email);
