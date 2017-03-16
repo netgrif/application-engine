d65ddb5304eb9f44c7576ca92e6f7171a6c821fb
Juraj Mažári <mazari@netgrif.com>
2017-03-16 11:25:48 +0100

    WMS-100 - Remove old token added deleteAllByEmail method

--- src/main/java/com/fmworkflow/auth/domain/TokenRepository.java
+++ src/main/java/com/fmworkflow/auth/domain/TokenRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 @Transactional
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Token findByEmail(String email);
+
     Token findByHashedToken(String token);
+
     List<Token> removeByExpirationDateBefore(LocalDateTime now);
+
+    void deleteAllByEmail(String email);
 }
