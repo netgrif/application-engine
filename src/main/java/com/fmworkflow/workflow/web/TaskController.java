9e0815eb6a9e0d4206ec2d35b36b1a10ff54d5d9
Juraj Mažári <mazari@netgrif.com>
2017-03-16 09:48:26 +0100

    WMS-83 - Role logic - assign to other add delegate endpoint

--- src/main/java/com/fmworkflow/workflow/web/TaskController.java
+++ src/main/java/com/fmworkflow/workflow/web/TaskController.java
@@ -20,7 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/res/task")
@@ -28,14 +27,15 @@ public class TaskController {
 
     @Autowired
     private ITaskService taskService;
+
     @Autowired
     private IFilterService filterService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public TasksResource getAll(Authentication auth){
+    public TasksResource getAll(Authentication auth) {
         List<TaskResource> resources = new ArrayList<>();
         LoggedUser loggedUser = (LoggedUser) auth.getPrincipal();
-        for(Task task : taskService.getAll(loggedUser)){
+        for (Task task : taskService.getAll(loggedUser)) {
             resources.add(TaskResource.createFrom(task, null));
         }
 
@@ -46,27 +46,39 @@ public class TaskController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public TaskResource getOne(@PathVariable("id") Long taskId){
-        return TaskResource.createFrom(taskService.findById(taskId),null);
+    public TaskResource getOne(@PathVariable("id") Long taskId) {
+        return TaskResource.createFrom(taskService.findById(taskId), null);
     }
 
     @RequestMapping(value = "/assign/{id}", method = RequestMethod.GET)
-    public MessageResource assign(Authentication auth, @PathVariable("id") Long taskId){
+    public MessageResource assign(Authentication auth, @PathVariable("id") Long taskId) {
         LoggedUser loggedUser = (LoggedUser) auth.getPrincipal();
         try {
             taskService.assignTask(loggedUser.transformToUser(), taskId);
-            return MessageResource.successMessage("Task "+taskId+" assigned to "+loggedUser.getFullName());
-        } catch (TransitionNotStartableException e){
-            return MessageResource.errorMessage("Task "+taskId+" cannot be assigned");
+            return MessageResource.successMessage("Task " + taskId + " assigned to " + loggedUser.getFullName());
+        } catch (TransitionNotStartableException e) {
+            return MessageResource.errorMessage("Task " + taskId + " cannot be assigned");
+        }
+    }
+
+    @RequestMapping(value = "/delegate/{id}", method = RequestMethod.POST)
+    public MessageResource delegate(Authentication auth, @PathVariable("id") Long taskId, @RequestParam("email") String delegatedEmail) {
+        LoggedUser loggedUser = (LoggedUser) auth.getPrincipal();
+        try {
+            taskService.delegateTask(delegatedEmail, taskId);
+            return MessageResource.successMessage("Task " + taskId + " assigned to " + delegatedEmail);
+        } catch (Exception ignored) {
+            ignored.printStackTrace();
+            return MessageResource.errorMessage("Task " + taskId + " cannot be assigned");
         }
     }
 
     @RequestMapping(value = "/finish/{id}", method = RequestMethod.GET)
-    public MessageResource finish(Authentication auth, @PathVariable("id") Long taskId){
+    public MessageResource finish(Authentication auth, @PathVariable("id") Long taskId) {
         LoggedUser loggedUser = (LoggedUser) auth.getPrincipal();
         try {
             taskService.finishTask(loggedUser.getId(), taskId);
-            return MessageResource.successMessage("Task "+taskId+" finished");
+            return MessageResource.successMessage("Task " + taskId + " finished");
         } catch (Exception e) {
             e.printStackTrace();
             return MessageResource.errorMessage(e.getMessage());
@@ -88,8 +100,8 @@ public class TaskController {
     @RequestMapping(value = "/my", method = RequestMethod.GET)
     public TasksResource getMy(Authentication auth) {
         List<TaskResource> resources = new ArrayList<>();
-        for(Task task:taskService.findByUser(((LoggedUser)auth.getPrincipal()).transformToUser())){
-            resources.add(TaskResource.createFrom(task,auth));
+        for (Task task : taskService.findByUser(((LoggedUser) auth.getPrincipal()).transformToUser())) {
+            resources.add(TaskResource.createFrom(task, auth));
         }
 
         TasksResource tasksResource = new TasksResource(resources);
@@ -99,9 +111,9 @@ public class TaskController {
     }
 
     @RequestMapping(value = "/my/finished", method = RequestMethod.GET)
-    public TasksResource getMyFinished(Authentication auth){
+    public TasksResource getMyFinished(Authentication auth) {
         List<TaskResource> resources = new ArrayList<>();
-        for(Task task:taskService.findByUser(((LoggedUser)auth.getPrincipal()).transformToUser())){
+        for (Task task : taskService.findByUser(((LoggedUser) auth.getPrincipal()).transformToUser())) {
             resources.add(TaskResource.createFrom(task, auth));
         }
 
@@ -112,51 +124,51 @@ public class TaskController {
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public TasksResource search(Authentication auth, @RequestBody TaskSearchBody searchBody){
+    public TasksResource search(Authentication auth, @RequestBody TaskSearchBody searchBody) {
         return getAll(auth); //TODO: 9.2.2017 - search on tasks according to posted json
     }
 
     @RequestMapping(value = "/{id}/data", method = RequestMethod.GET)
-    public DataFieldsResource getData(@PathVariable("id") Long taskId){
+    public DataFieldsResource getData(@PathVariable("id") Long taskId) {
         List<Field> dataFields = taskService.getData(taskId);
         return new DataFieldsResource(dataFields, taskId);
     }
 
     @RequestMapping(value = "/{id}/data", method = RequestMethod.POST)
-    public MessageResource saveData(@PathVariable("id") Long taskId, @RequestBody ObjectNode dataBody){
+    public MessageResource saveData(@PathVariable("id") Long taskId, @RequestBody ObjectNode dataBody) {
         taskService.setDataFieldsValues(taskId, dataBody);
-        return MessageResource.successMessage("Data for task "+taskId+" saved");
+        return MessageResource.successMessage("Data for task " + taskId + " saved");
     }
 
     @RequestMapping(value = "/{id}/file/{field}", method = RequestMethod.POST)
     public MessageResource saveFile(@PathVariable("id") Long taskId, @PathVariable("field") String fieldId,
-                                    @RequestParam(value = "file")MultipartFile multipartFile){
-        if(taskService.saveFile(taskId, fieldId, multipartFile))
-            return MessageResource.successMessage("File "+multipartFile.getOriginalFilename()+" successfully uploaded");
+                                    @RequestParam(value = "file") MultipartFile multipartFile) {
+        if (taskService.saveFile(taskId, fieldId, multipartFile))
+            return MessageResource.successMessage("File " + multipartFile.getOriginalFilename() + " successfully uploaded");
         else
-            return  MessageResource.errorMessage("File "+multipartFile.getOriginalFilename()+" failed to upload");
+            return MessageResource.errorMessage("File " + multipartFile.getOriginalFilename() + " failed to upload");
     }
 
     @RequestMapping(value = "/{id}/file/{field}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public FileSystemResource getFile(@PathVariable("id") Long taskId, @PathVariable("field") String fieldId, HttpServletResponse response){
+    public FileSystemResource getFile(@PathVariable("id") Long taskId, @PathVariable("field") String fieldId, HttpServletResponse response) {
         FileSystemResource fileResource = taskService.getFile(taskId, fieldId);
         response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        response.setHeader("Content-Disposition","attachment; filename="+fileResource.getFilename().substring(fileResource.getFilename().indexOf('-')+1));
+        response.setHeader("Content-Disposition", "attachment; filename=" + fileResource.getFilename().substring(fileResource.getFilename().indexOf('-') + 1));
         return fileResource;
     }
 
     @RequestMapping(value = "/filter", method = RequestMethod.GET)
-    public FiltersResource getAllFilters(Authentication auth){
+    public FiltersResource getAllFilters(Authentication auth) {
         return new FiltersResource(filterService.getAll());
     }
 
     @RequestMapping(value = "/filter", method = RequestMethod.POST)
-    public MessageResource saveFilter(Authentication auth, @RequestBody CreateFilterBody filterBody){
-        boolean saveSuccess = filterService.saveFilter(((LoggedUser) auth.getPrincipal()),filterBody);
-        if(saveSuccess){
-            return MessageResource.successMessage("Filter "+filterBody.name+" saved");
+    public MessageResource saveFilter(Authentication auth, @RequestBody CreateFilterBody filterBody) {
+        boolean saveSuccess = filterService.saveFilter(((LoggedUser) auth.getPrincipal()), filterBody);
+        if (saveSuccess) {
+            return MessageResource.successMessage("Filter " + filterBody.name + " saved");
         } else {
-            return MessageResource.errorMessage("Filter "+filterBody.name+" saving failed!");
+            return MessageResource.errorMessage("Filter " + filterBody.name + " saving failed!");
         }
     }
 }
