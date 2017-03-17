927b03c60858c9dc934271029ef588f41d43c2d9
Milan Mladoniczky <milan.befinger@gmail.com>
2017-03-17 02:02:36 +0100

    WMS-83 - Role logic - assign to other - add delegating task to user

--- src/main/java/com/fmworkflow/workflow/domain/TaskRepository.java
+++ src/main/java/com/fmworkflow/workflow/domain/TaskRepository.java
@@ -12,7 +12,7 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByUserAndFinishDateNotNull(User user);
 
-    List<Task> findAllByAssignRoleIn(List<String> roles);
+    List<Task> findAllByAssignRoleOrDelegateRoleIn(List<String> roles, List<String> roles2);
 
     List<Task> findAllByCaseId(String caseId);
 
