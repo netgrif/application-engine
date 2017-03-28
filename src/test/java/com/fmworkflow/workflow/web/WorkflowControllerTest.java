7ef9e94703c92c24ac2acaeb1993d1566040edb8
Juraj Mažári <mazari.juraj@gmail.com>
2017-03-28 19:45:21 +0200

    fix tests

--- src/test/java/com/fmworkflow/workflow/web/WorkflowControllerTest.java
+++ src/test/java/com/fmworkflow/workflow/web/WorkflowControllerTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
 @SpringBootTest
+@ActiveProfiles({"test"})
 @RunWith(SpringRunner.class)
 public class WorkflowControllerTest {
 
@@ -18,5 +18,6 @@ public class WorkflowControllerTest {
     @Test
     public void createCase() throws Exception {
         // TODO: 4. 2. 2017
+//        workflowService.createCase(net.getStringId(), "Storage Unit " + i, randomColor());
     }
 }
