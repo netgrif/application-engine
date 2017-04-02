8c1f21167f5774117860c72e308329ed250085bd
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-04-02 20:27:42 +0200

    WMS-108 - Search on task - add search tasks by petriNets and by transitions

--- src/main/java/com/fmworkflow/workflow/domain/TaskRepository.java
+++ src/main/java/com/fmworkflow/workflow/domain/TaskRepository.java
@@ -3,18 +3,19 @@ package com.fmworkflow.workflow.domain;
 import com.fmworkflow.auth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByCaseId(String id);
+    List<Task> findByCaseIdIn(Collection<String> ids);
+
+    List<Task> findByTransitionIdIn(Collection<String> ids);
 
     List<Task> findByUser(User user);
 
     List<Task> findByUserAndFinishDateNotNull(User user);
 
     List<Task> findAllByAssignRoleInOrDelegateRoleIn(List<String> roles, List<String> roles2);
-
-    List<Task> findAllByCaseId(String caseId);
 
     void deleteAllByCaseIdAndUserIsNull(String caseId);
 
--- src/main/java/com/fmworkflow/workflow/web/requestbodies/TaskSearchBody.java
+++ src/main/java/com/fmworkflow/workflow/web/requestbodies/TaskSearchBody.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class TaskSearchBody {
 
-    public List<String> petrinetIds;
-    public List<String> transitionIds;
+    public static final int SEARCH_TIER_1 = 1;
+    public static final int SEARCH_TIER_2 = 2;
+    public static final int SEARCH_TIER_3 = 3;
 
+    public int searchTier;
+    public List<SearchPetriNet> petriNets;
+
+    public TaskSearchBody() {}
 }
