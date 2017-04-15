849d89dabbd741e110d50ca99f99124b00330dab
Juraj Mažári <valdyreinn@gmail.com>
2017-04-15 11:49:36 +0200

    WMS-144, WMS-145 - Inhibitor & Reset Arc fix reset arc isExecutable logic

--- src/main/java/com/fmworkflow/petrinet/domain/ResetArc.java
+++ src/main/java/com/fmworkflow/petrinet/domain/ResetArc.java
@@ -4,6 +4,11 @@ public class ResetArc extends Arc {
     private Integer removedTokens;
 
     @Override
+    public boolean isExecutable() {
+        return true;
+    }
+
+    @Override
     public void execute() {
         Place place = ((Place) source);
         removedTokens = place.getTokens();
