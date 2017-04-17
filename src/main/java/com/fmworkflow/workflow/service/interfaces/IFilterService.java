3e34e8233518bad0b80e261e5fa07aa8400f4e8a
Milan Mladoniczky <milan.befinger@gmail.com>
2017-04-17 20:38:14 +0200

    WMS-66 - Filters - get filters with roles

--- src/main/java/com/fmworkflow/workflow/service/interfaces/IFilterService.java
+++ src/main/java/com/fmworkflow/workflow/service/interfaces/IFilterService.java
@@ -10,5 +10,6 @@ import java.util.List;
 public interface IFilterService {
 
     List<Filter> getAll();
+    List<Filter> getWithRoles(List<String> roles);
     boolean saveFilter(LoggedUser user, CreateFilterBody filterBody);
 }
