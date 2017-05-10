b7e9f4653ce898bda32dee63815f333485602a63
Juraj Mažári <mazari@netgrif.com>
2017-05-10 18:26:58 +0200

    WMS-156 - Import sample csv create new net Xml file, update xls importer

--- src/main/java/com/fmworkflow/XlsImporter.groovy
+++ src/main/java/com/fmworkflow/XlsImporter.groovy
@@ -1,8 +1,12 @@
 package com.fmworkflow
 
+import com.fmworkflow.auth.domain.Role
 import com.fmworkflow.auth.domain.User
+import com.fmworkflow.auth.domain.UserProcessRole
+import com.fmworkflow.auth.domain.repositories.RoleRepository
 import com.fmworkflow.auth.domain.repositories.UserProcessRoleRepository
 import com.fmworkflow.auth.domain.repositories.UserRepository
+import com.fmworkflow.auth.service.interfaces.IUserService
 import com.fmworkflow.importer.Importer
 import com.fmworkflow.petrinet.domain.PetriNet
 import com.fmworkflow.petrinet.domain.dataset.FieldType
@@ -26,6 +30,9 @@ class XlsImporter implements CommandLineRunner {
     private PetriNetRepository petriNetRepository
 
     @Autowired
+    private IUserService userService
+
+    @Autowired
     private UserRepository userRepository
 
     @Autowired
@@ -33,6 +40,9 @@ class XlsImporter implements CommandLineRunner {
 
     @Autowired
     private UserProcessRoleRepository userProcessRoleRepository
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Autowired
     private TaskService taskService
@@ -53,7 +63,7 @@ class XlsImporter implements CommandLineRunner {
             ["Odovzdávajúci", FieldType.USER],
             ["Dátum odovzdania", FieldType.DATE],
             ["Forma", FieldType.TEXT],
-            ["LU", FieldType.TEXT],
+            ["Lehota uloženia", FieldType.TEXT],
             ["Znak hodnoty", FieldType.TEXT],
             ["Čiarový kód", FieldType.TEXT],
             ["Lokalizácia", FieldType.TEXT],
@@ -68,23 +78,84 @@ class XlsImporter implements CommandLineRunner {
 
     @Override
     void run(String... strings) throws Exception {
-        net = importer.importPetriNet(new File("src/test/resources/prikladFM.xml"), "Archiv", "FMS")
+        net = importer.importPetriNet(new File("src/test/resources/prikladFM_import.xml"), "Archiv", "FMS")
 
         def user = userRepository.findByEmail("super@netgrif.com")
         user.setUserProcessRoles(userProcessRoleRepository.findAll() as Set)
-        userRepository.save(user)
+        user.setRoles(roleRepository.findAll() as Set)
+        userService.save(user)
 
+        def userRole = roleRepository.findByName("user")
+        def client_manager = new User(
+                email: "manager@client.com",
+                name: "Manager",
+                surname: "Client",
+                password: "password",
+                roles: [userRole] as Set<Role>)
+        client_manager.addProcessRole(userProcessRoleRepository.save(new UserProcessRole(
+                roleId: net.roles.values().find { it -> it.name == "client manager" }.stringId)))
+        def client_worker = new User(
+                email: "worker@client.com",
+                name: "Worker",
+                surname: "Client",
+                password: "password",
+                roles: [userRole] as Set<Role>)
+        client_worker.addProcessRole(userProcessRoleRepository.save(new UserProcessRole(
+                roleId: net.roles.values().find { it -> it.name == "client" }.stringId)))
+        def fm_manager = new User(
+                email: "manager@fmservis.com",
+                name: "Manager",
+                surname: "FM Servis",
+                password: "password",
+                roles: [userRole] as Set<Role>)
+        fm_manager.addProcessRole(userProcessRoleRepository.save(new UserProcessRole(
+                roleId: net.roles.values().find { it -> it.name == "fm servis" }.stringId)))
+        def fm_worker = new User(
+                email: "worker@fmservis.com",
+                name: "Worker",
+                surname: "FM Servis",
+                password: "password",
+                roles: [userRole] as Set<Role>)
+        fm_worker.addProcessRole(userProcessRoleRepository.save(new UserProcessRole(
+                roleId: net.roles.values().find { it -> it.name == "fm servis" }.stringId)))
+        userService.save(client_manager)
+        userService.save(client_worker)
+        userService.save(fm_manager)
+        userService.save(fm_worker)
+
+        def index = 0
         new File("src/test/resources/Vzor preberacieho protokolu.csv").splitEachLine("\t") { fields ->
             useCase = new Case(petriNet: net, title: "Ukladacia jednotka ${fields[1]}")
 
-            useCase.activePlaces.put(net.places.find { it -> it.value.title == "A" }.key, 1)
 
-            header_fields.eachWithIndex { entry, int i ->
-                putValue(entry[0] as String, getValue(fields[i], entry[1] as FieldType))
+            if (index < 10) {
+                useCase.activePlaces.put(net.places.find { it -> it.value.title == "B" }.key, 1)
+
+                header_fields.eachWithIndex { entry, int i ->
+                    if (i in [0, 5, 8, 14,])
+                        return
+                    putValue(entry[0] as String, getValue(fields[i], entry[1] as FieldType))
+                }
+            } else if (index < 20) {
+                useCase.activePlaces.put(net.places.find { it -> it.value.title == "K" }.key, 1)
+
+                header_fields.eachWithIndex { entry, int i ->
+                    if (i in [0, 5, 8, 14,])
+                        return
+                    putValue(entry[0] as String, getValue(fields[i], entry[1] as FieldType))
+                }
+            } else {
+                useCase.activePlaces.put(net.places.find { it -> it.value.title == "J" }.key, 1)
+
+                header_fields.eachWithIndex { entry, int i ->
+                    putValue(entry[0] as String, getValue(fields[i], entry[1] as FieldType))
+                }
             }
 
             caseRepository.save(useCase)
             taskService.createTasks(useCase)
+
+            index++
         }
     }
 
