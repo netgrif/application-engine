3e0de675d2feba426ad24005d16b9afb3b286bb2
Milan Mladoniczky <milan.befinger@gmail.com>
2017-06-02 20:39:24 +0200

    WMS-99 Organization - add organizations

---
+++ src/main/java/com/fmworkflow/auth/domain/Organization.java
@@ -0,0 +1,61 @@
+package com.fmworkflow.auth.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "organizations")
+    private Set<User> users;
+
+    public Organization() {
+        users = new HashSet<>();
+    }
+
+    public Organization(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User user){
+        this.users.add(user);
+    }
+}
