ea7a967872c689ab10a38fd3036cdfa9a83863b0
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-06-03 20:03:25 +0200

    WMS-161 Roles rework - fix find users by role query

--- src/main/java/com/netgrif/workflow/auth/domain/repositories/UserRepository.java
+++ src/main/java/com/netgrif/workflow/auth/domain/repositories/UserRepository.java
@@ -16,9 +16,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByOrganizationsIn(List<Organization> orgs);
 
-    @Query("SELECT user FROM user LEFT OUTER JOIN user_process_roles\n" +
-            "  ON user.id = user_process_roles.user_id\n" +
-            "  LEFT OUTER JOIN  user_process_role ON user_process_roles.user_process_role_id = user_process_role.id\n" +
-            "  WHERE role_id = ?1")
-    List<User> findByUserProcessRolesId(String roleId);
+    List<User> findByUserProcessRoles_RoleId(String roleId);
 }
