d09eda3a38b874b46bad4f6de752d3e3e7c47512
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-06-04 16:32:11 +0200

    WMS-162 Subtype of Text field - add subtype simple and area

--- src/main/java/com/netgrif/workflow/petrinet/domain/dataset/TextField.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/dataset/TextField.java
@@ -6,11 +6,20 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class TextField extends Field<String> {
 
+    public static final String SIMPLE_SUBTYPE = "simple";
+    public static final String AREA_SUBTYPE = "area";
+
+    private String subType;
     @Transient
     private String value;
 
     public TextField() {
         super();
+    }
+
+    public TextField(String[] values){
+        this();
+        this.subType = values != null ? values[0] : SIMPLE_SUBTYPE;
     }
 
     public String getValue() {
@@ -25,5 +34,13 @@ public class TextField extends Field<String> {
     @Override
     public void setType(FieldType type){
         this.type = FieldType.TEXT;
+    }
+
+    public String getSubType() {
+        return subType;
+    }
+
+    public void setSubType(String subType) {
+        this.subType = subType;
     }
 }
