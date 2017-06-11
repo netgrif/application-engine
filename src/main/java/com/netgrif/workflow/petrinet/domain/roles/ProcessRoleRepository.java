de55fb655b76b99b019ed94d93273194299c80c4
Milan Mladoniczky <milan.befinger@gmail.com>
2017-06-11 14:59:59 +0200

    WMS-161 - Roles rework - fix display process roles names in user profile

--- src/main/java/com/netgrif/workflow/petrinet/domain/roles/ProcessRoleRepository.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/roles/ProcessRoleRepository.java
@@ -2,5 +2,8 @@ package com.netgrif.workflow.petrinet.domain.roles;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Set;
+
 public interface ProcessRoleRepository extends MongoRepository<ProcessRole, String> {
+    Set<ProcessRole> findAll(Iterable<String> strings);
 }
