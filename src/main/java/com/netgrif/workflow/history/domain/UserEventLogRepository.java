988c9f67a6a07c4403f8ec3f3525251735003504
Juraj Mažári <mazari@netgrif.com>
2017-06-19 12:06:33 +0200

    WMS-141 - History - fix Pageable error in event repository

--- src/main/java/com/netgrif/workflow/history/domain/UserEventLogRepository.java
+++ src/main/java/com/netgrif/workflow/history/domain/UserEventLogRepository.java
@@ -2,11 +2,12 @@ package com.netgrif.workflow.history.domain;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserEventLogRepository extends MongoRepository<UserEventLog, ObjectId> {
 
-    Page<UserEventLog> findAllByEmail(String email);
+    Page<UserEventLog> findAllByEmail(Pageable pageable,String email);
 }
--- src/main/java/com/netgrif/workflow/history/service/IHistoryService.java
+++ src/main/java/com/netgrif/workflow/history/service/IHistoryService.java
@@ -3,8 +3,9 @@ package com.netgrif.workflow.history.service;
 import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.history.domain.UserEventLog;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IHistoryService {
 
-    Page<UserEventLog> findAllByUser(User user);
+    Page<UserEventLog> findAllByUser(Pageable pageable, User user);
 }
