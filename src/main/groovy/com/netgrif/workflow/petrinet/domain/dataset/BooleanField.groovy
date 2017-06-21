f24e50e8229f815001339ef7ac2e48e1eb293aef
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-06-22 01:25:51 +0200

    WMS-173 - Data logic DSL - operator overload

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/BooleanField.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/BooleanField.groovy
@@ -8,4 +8,12 @@ public class BooleanField extends Field<Boolean> {
     public BooleanField() {
         super();
     }
+
+    Boolean or(final BooleanField field){
+        return this.value || field.value
+    }
+
+    Boolean and(final BooleanField field){
+        return this.value && field.value
+    }
 }
