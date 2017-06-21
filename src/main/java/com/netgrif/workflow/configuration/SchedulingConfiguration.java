789df082afe2f4ae890292a2db4cbd0bc436b191
Juraj Mažári <mazari@netgrif.com>
2017-06-21 12:15:42 +0200

    WMS-137 - Server run tasks - refactor TimeTrigger into two separate triggers - update SchedulingConfig. by enabling scheduling

--- src/main/java/com/netgrif/workflow/configuration/SchedulingConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/SchedulingConfiguration.java
@@ -3,8 +3,10 @@ package com.netgrif.workflow.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@EnableScheduling
 @Configuration
 public class SchedulingConfiguration {
 
