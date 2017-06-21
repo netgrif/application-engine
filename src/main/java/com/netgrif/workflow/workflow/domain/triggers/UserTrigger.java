1b26d2bbaf2d67ba8debb2523167e499d5c13a50
Juraj Mažári <mazari@netgrif.com>
2017-06-21 16:44:38 +0200

    WMS-137 - Server run tasks - refactor Triggers - add Trigger constructor - add auto trigger to PN

--- src/main/java/com/netgrif/workflow/workflow/domain/triggers/UserTrigger.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/triggers/UserTrigger.java
@@ -1,11 +1,9 @@
 package com.netgrif.workflow.workflow.domain.triggers;
 
-import org.bson.types.ObjectId;
-
 public class UserTrigger extends Trigger {
 
     public UserTrigger() {
-        this._id = new ObjectId();
+        super();
     }
 
     @Override
