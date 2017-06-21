066e53a9d1c2a8587667aa1c6f78a91459e6fcec
Juraj Mažári <mazari.juraj@gmail.com>
2017-06-21 18:42:54 +0200

    WMS-137 - Server run tasks - refactor TaskService - refactor events

--- src/main/java/com/netgrif/workflow/event/events/UserAssignTaskEvent.java
+++ src/main/java/com/netgrif/workflow/event/events/UserAssignTaskEvent.java
@@ -1,11 +1,12 @@
 package com.netgrif.workflow.event.events;
 
+import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.workflow.domain.Case;
 import com.netgrif.workflow.workflow.domain.Task;
 
 public class UserAssignTaskEvent extends UserTaskEvent{
 
-    public UserAssignTaskEvent(Object user, Task task, Case useCase) {
+    public UserAssignTaskEvent(User user, Task task, Case useCase) {
         super(user, task, useCase);
     }
 
