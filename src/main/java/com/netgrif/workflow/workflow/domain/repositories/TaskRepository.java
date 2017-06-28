1aab75d8343a1d73faaceaa6937446ba37db61d5
Milan Mladoniczky <milan.befinger@gmail.com>
2017-06-28 22:07:08 +0200

    PI-18 - Task status - add attribute if all required field are filled with data in task

--- src/main/java/com/netgrif/workflow/workflow/domain/repositories/TaskRepository.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/repositories/TaskRepository.java
@@ -17,6 +17,8 @@ public interface TaskRepository extends MongoRepository<Task, String> {
 
     List<Task> findByUserIdAndFinishDateNotNull(Long userId);
 
+    Task findByTransitionIdAndCaseId(String transitionId, String caseId);
+
     void deleteAllByCaseIdAndUserIdIsNull(String caseId);
 
     void deleteAllByCaseIdAndFinishDateIsNotNull(String caseId);
