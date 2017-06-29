4b4a8a515bf2383f61ce7f4fe5236e2345fca516
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-06-29 23:29:38 +0200

    PI-19 - Field validation DSL - add runner for validation DSL - add delegate classes

---
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/validation/NumberValidationDelegate.groovy
@@ -0,0 +1,24 @@
+package com.netgrif.workflow.petrinet.domain.dataset.logic.validation
+
+import com.netgrif.workflow.petrinet.domain.dataset.Field
+
+
+class NumberValidationDelegate extends ValidationDelegate {
+
+    NumberValidationDelegate(Field field) {
+        super(field)
+    }
+
+    def odd = { field.value % 2 }
+    def even = { !(field.value % 2) }
+    def positive = { field.value >= 0 }
+    def negative = { field.value < 0 }
+    def decimal = { field.value % 1 == 0 }
+    def inf = { return "inf" }
+    def inrange = { n, m ->
+        if (n instanceof Closure && n() == "inf") return field.value <= m
+        if (m instanceof Closure && m() == "inf") return field.value >= n
+        return field.value >= n && field.value <= m
+    }
+
+}
