4ac524fe98a60e0657c74029aa6f7772fb42864c
Milan Mladoniczky <milan.befinger@gmail.com>
2017-06-29 23:29:38 +0200

    PI-19 - Field validation DSL - add runner for validation DSL - add delegate classes

---
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/validation/FieldValidationRunner.groovy
@@ -0,0 +1,20 @@
+package com.netgrif.workflow.petrinet.domain.dataset.logic.validation
+
+import com.netgrif.workflow.petrinet.domain.dataset.Field
+
+
+class FieldValidationRunner {
+
+    /**
+     * Validation function for field value
+     * @param field - field to validate
+     * @param rules - validation rules in groovy DSL
+     * @return true if value in the field is valid according to validation rules
+     */
+    static boolean validate(Field field, String rules){
+        def shell = new GroovyShell()
+        def code = (Closure) shell.evaluate("{-> validate(${rules})}")
+        code.delegate = ValidationDelefateFactory.getDelegate(field)
+        return code()
+    }
+}
