4a9afcc061ebe396ad9ab18f892aeca0fea350ad
Milan Mladoniczky <milan.befinger@gmail.com>
2017-07-08 23:32:47 +0200

    PI-36 - Hidden behavior - add hidden behavior

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/FieldBehavior.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/FieldBehavior.groovy
@@ -4,20 +4,59 @@ enum FieldBehavior {
     REQUIRED("required"),
     OPTIONAL("optional"),
     VISIBLE("visible"),
-    EDITABLE("editable")
+    EDITABLE("editable"),
+    HIDDEN("hidden"),
+    ANTONYM_SETUP("",true);
 
 
-    private String name
+    private final String name
+    private FieldBehavior[] antonyms
 
     FieldBehavior(String name) {
         this.name = name
     }
 
-    public static FieldBehavior fromString(String string){
+    FieldBehavior(String name, boolean populate) {
+        this.name = name
+        if (populate) {
+            REQUIRED.setAntonyms()
+            OPTIONAL.setAntonyms()
+            VISIBLE.setAntonyms()
+            EDITABLE.setAntonyms()
+            HIDDEN.setAntonyms()
+        }
+    }
+
+    private FieldBehavior[] addAntonyms() {
+        switch (name) {
+            case "required":
+                return (FieldBehavior[]) [VISIBLE, HIDDEN, OPTIONAL].toArray()
+            case "optional":
+                return (FieldBehavior[]) [REQUIRED].toArray()
+            case "visible":
+                return (FieldBehavior[]) [REQUIRED, EDITABLE, HIDDEN].toArray()
+            case "editable":
+                return (FieldBehavior[]) [VISIBLE, HIDDEN].toArray()
+            case "hidden":
+                return (FieldBehavior[]) [EDITABLE, VISIBLE, REQUIRED].toArray()
+            default:
+                return null
+        }
+    }
+
+    public static FieldBehavior fromString(String string) {
         return FieldBehavior.valueOf(string.toUpperCase());
     }
 
-    public String toString(){
+    FieldBehavior[] getAntonyms() {
+        return antonyms
+    }
+
+    private void setAntonyms() {
+        this.antonyms = addAntonyms()
+    }
+
+    public String toString() {
         return this.name
     }
 }
