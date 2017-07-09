39351fba65add1312132f2d3731a5533efc1780e
Juraj Mažári <mazari.juraj@gmail.com>
2017-07-09 12:25:17 +0200

    PI-4 - PDF generator - overload pdf method (todo) - update Insurance logic

--- src/main/groovy/com/netgrif/workflow/pdf/service/PdfFormFiller.groovy
+++ src/main/groovy/com/netgrif/workflow/pdf/service/PdfFormFiller.groovy
@@ -6,16 +6,36 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm
 import org.springframework.stereotype.Component
 
 
-@Component
 class PdfFormFiller {
 
-    File fillPdfForm(String outPdfName, InputStream pdfFile, InputStream xmlFile) throws IllegalArgumentException {
+    static File fillPdfForm(String outPdfName, InputStream pdfFile, InputStream xmlFile) throws IllegalArgumentException {
         try {
             PDDocument document = PDDocument.load(pdfFile)
             File file = new File(outPdfName)
             PDDocumentCatalog docCatalog = document.getDocumentCatalog()
             PDAcroForm acroForm = docCatalog.getAcroForm()
             def fieldValues = new XmlSlurper().parseText(xmlFile.getText())
+
+            fieldValues.children().each {
+                acroForm.getField(it["@xfdf:original"] as String).setValue(it as String)
+            }
+
+            acroForm.flatten()
+            document.save(file)
+            return file
+        } catch (IOException e) {
+            e.printStackTrace()
+            throw new IllegalArgumentException(e)
+        }
+    }
+
+    static File fillPdfForm(String outPdfName, File pdfFile, String xml) throws IllegalArgumentException {
+        try {
+            PDDocument document = PDDocument.load(pdfFile)
+            File file = new File(outPdfName)
+            PDDocumentCatalog docCatalog = document.getDocumentCatalog()
+            PDAcroForm acroForm = docCatalog.getAcroForm()
+            def fieldValues = new XmlSlurper().parseText(xml)
 
             fieldValues.children().each {
                 acroForm.getField(it["@xfdf:original"] as String).setValue(it as String)
