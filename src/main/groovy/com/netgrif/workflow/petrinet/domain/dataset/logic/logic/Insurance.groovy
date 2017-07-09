ea2761e72612b76793ff7dfb5886575bd551ef8b
Juraj Mažári <mazari@netgrif.com>
2017-07-09 20:30:21 +0200

    PI-4 - PDF generator - add dataset mapping draft

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/logic/Insurance.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/logic/Insurance.groovy
@@ -5,7 +5,6 @@ import com.netgrif.workflow.petrinet.domain.dataset.FileField
 import com.netgrif.workflow.workflow.domain.Case
 import groovy.xml.MarkupBuilder
 
-
 class Insurance {
 
     private Case useCase
@@ -16,14 +15,30 @@ class Insurance {
         this.field = field
     }
 
-    File offerPDF(){
+    File offerPDF() {
         String name = "offer.pdf"
         File input = new File("src/main/resources/pdf/zmluva_editovatelna.pdf")
-        File xml = new File("src/main/resources/pdf/zmluva_editovatelna.xml")
+        String xml = datasetToXml()
 
-        File pdf = PdfFormFiller.fillPdfForm(name, new FileInputStream(input), new FileInputStream(xml))
+        File pdf = PdfFormFiller.fillPdfForm(name, input, xml)
         useCase.dataSet.get(field.objectId).value = name
 
         return pdf
+    }
+
+    private String datasetToXml() {
+        def writer = new StringWriter()
+        def builder = new MarkupBuilder(writer)
+
+        builder.fields() {
+            TextField50('xfdf:original' : "Text Field 50", "${value('Právna subjektivita poisteného?')}")
+            TextField70('xfdf:original' : "Text Field 70", "${value('Obec')}")
+        }
+
+        return writer.toString()
+    }
+
+    private String value(String name) {
+        return useCase.dataSet[useCase.petriNet.dataSet.find {it.value.name == name}?.key]?.value?.toString()
     }
 }
