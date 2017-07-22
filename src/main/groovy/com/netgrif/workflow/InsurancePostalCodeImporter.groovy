1cfaa45264d34f01cf5e6a4aa476b0d341446963
Juraj Mažári <mazari@netgrif.com>
2017-07-22 13:54:23 +0200

    - add dev psc list

--- src/main/groovy/com/netgrif/workflow/InsurancePostalCodeImporter.groovy
+++ src/main/groovy/com/netgrif/workflow/InsurancePostalCodeImporter.groovy
@@ -3,6 +3,7 @@ package com.netgrif.workflow
 import com.netgrif.workflow.psc.PostalCodeService
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,12 +11,15 @@ class InsurancePostalCodeImporter {
 
     private static final Logger log = Logger.getLogger(InsurancePostalCodeImporter.class.name)
 
+    @Value("\${postal.codes.csv}")
+    String postalCodesPath
+
     @Autowired
     private PostalCodeService service
 
     void run(String... strings) {
         log.info("Importing postal codes")
-        def importFile = new File("src/main/resources/postal_codes.csv")
+        def importFile = new File(postalCodesPath)
         importFile.splitEachLine(',') { items ->
             service.createPostalCode(items[0],items[1],items[2],items[3])
         }
