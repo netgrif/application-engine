a2ac39599ce94260afb76b3b525c4bf2d7e51acf
Juraj Mažári <mazari.juraj@gmail.com>
2017-07-26 11:23:48 +0200

    add ApplicationContextProvider

---
+++ src/main/groovy/com/netgrif/workflow/context/ApplicationContextProvider.groovy
@@ -0,0 +1,19 @@
+package com.netgrif.workflow.context
+
+import org.springframework.beans.BeansException
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+
+class ApplicationContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext context
+
+    @Override
+    void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext
+    }
+
+    static ApplicationContext getAppContext() {
+        return context
+    }
+}
