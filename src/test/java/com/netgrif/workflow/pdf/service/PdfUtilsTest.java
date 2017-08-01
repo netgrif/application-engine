6c64e11241c76be647d32b732b5a27b2d498a11c
Juraj Mažári <mazari@netgrif.com>
2017-08-01 10:29:34 +0200

    PI-4 - PDF generator - fix accent characters

--- src/test/java/com/netgrif/workflow/pdf/service/PdfUtilsTest.java
+++ src/test/java/com/netgrif/workflow/pdf/service/PdfUtilsTest.java
@@ -19,7 +19,7 @@ public class PdfUtilsTest {
         File input = new File("src/test/resources/pdf/test.pdf");
         File xml = new File("src/test/resources/pdf/test.xml");
 
-        File out = PdfUtils.fillPdfForm("test_out.pdf", new FileInputStream(input), new FileInputStream(xml));
+        File out = PdfUtils.fillPdfForm("target/test_out.pdf", new FileInputStream(input), new FileInputStream(xml));
 
         assert out != null;
     }
@@ -30,7 +30,7 @@ public class PdfUtilsTest {
         File f2 = new File("src/test/resources/pdf/test.pdf");
         File f3 = new File("src/test/resources/pdf/test.pdf");
 
-        File out = PdfUtils.mergePdfFiles("test_out_2.pdf", f1, f2, f3);
+        File out = PdfUtils.mergePdfFiles("target/test_out_2.pdf", f1, f2, f3);
 
         assert out != null;
     }
