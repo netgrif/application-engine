83203a92d705cee136ed2570df50a5bf7548d94b
Juraj Mažári <mazari@netgrif.com>
2017-08-10 11:22:03 +0200

    PI-70 -

--- src/main/java/com/netgrif/workflow/premiuminsurance/IdGenerator.java
+++ src/main/java/com/netgrif/workflow/premiuminsurance/IdGenerator.java
@@ -2,8 +2,6 @@ package com.netgrif.workflow.premiuminsurance;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class IdGenerator {
@@ -11,12 +9,8 @@ public class IdGenerator {
     @Autowired
     private OfferIdRepository repository;
 
-    @Transactional(isolation = Isolation.SERIALIZABLE)
     public Long getId() {
-        OfferId id = repository.findAll().get(0);
-        id.setOfferId(id.getOfferId() + 1);
-        id = repository.save(id);
-
-        return id.getOfferId();
+        OfferId id = repository.save(new OfferId());
+        return id.getId();
     }
 }
