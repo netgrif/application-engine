dc7862162f575eebef3e0cd9b6e56e4f22c120d2
Juraj Mažári <mazari@netgrif.com>
2017-08-16 00:30:41 +0200

    PI-87 - Model corrections - fix sum calculation

--- src/main/java/com/netgrif/workflow/premiuminsurance/OfferId.java
+++ src/main/java/com/netgrif/workflow/premiuminsurance/OfferId.java
@@ -12,6 +12,6 @@ import javax.persistence.Id;
 public class OfferId {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
