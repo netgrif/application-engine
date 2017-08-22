9fb5784f1547785e8073e00f0b0b725a46f28e49
Juraj Mažári <mazari@netgrif.com>
2017-08-22 14:55:26 +0200

    PI-98 - PDF - Poistenie Prémiové bývanie - fix dataset transformation to xml - fix pdf content - add test

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/action/Insurance.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/action/Insurance.groovy
@@ -24,14 +24,15 @@ class Insurance {
     File offerPDF() {
         String draftPath = (field as FileField).getFilePath(DRAFT_FILENAME)
         String offerPath = (field as FileField).getFilePath(OFFER_FILENAME)
+        String finalPath = (field as FileField).getFilePath(FINAL_FILENAME)
         String draftXml = datasetToDraftXml()
         String offerXml = datasetToOfferXml()
 
-        File draftPdfFile = PdfUtils.fillPdfForm(draftPath, new FileInputStream("$PDF_PATH/$OFFER_FILENAME"), draftXml)
-        File offerPdfFile = PdfUtils.fillPdfForm(offerPath, new FileInputStream("$PDF_PATH/$DRAFT_FILENAME"), offerXml)
-        File mergedPdf = PdfUtils.mergePdfFiles(FINAL_FILENAME, draftPdfFile, offerPdfFile)
+        File draftPdfFile = PdfUtils.fillPdfForm(draftPath, new FileInputStream("$PDF_PATH/$DRAFT_FILENAME"), draftXml)
+        File offerPdfFile = PdfUtils.fillPdfForm(offerPath, new FileInputStream("$PDF_PATH/$OFFER_FILENAME"), offerXml)
+        File mergedPdf = PdfUtils.mergePdfFiles(finalPath, draftPdfFile, offerPdfFile)
 
-        useCase.dataSet.get(field.stringId).setValue(FINAL_FILENAME)
+        useCase.dataSet.get(field.stringId).setValue(finalPath)
 
         return mergedPdf
     }
@@ -61,10 +62,10 @@ class Insurance {
         def builder = new MarkupBuilder(writer)
 
         builder.fields('xmlns:xfdf': '"http://ns.adobe.com/xfdf-transition/"') {
-            field("xfdf:original": " Text Field 143 ", "${value(309001) ?: ''}")
+            field("xfdf:original": "Text Field 143", "${value(309001) ?: ''}")
 //            Poistník todo
-            field("xfdf:original": " Text Field 50 ", "${value(109007) ?: ''}")
-            field("xfdf:original": " Text Field 51 ", "${value(109008) ?: ''}")
+            field("xfdf:original": "Text Field 50", "${value(109007) ?: ''}")
+            field("xfdf:original": "Text Field 51", "${value(109008) ?: ''}")
             field("xfdf:original": "1", "${value(109010) ?: ''} ${value(109011) ?: ''}")
             field("xfdf:original": "2", "${value(109012) ?: ''}")
             field("xfdf:original": "3", "${value(109009) ?: ''}")
@@ -113,42 +114,42 @@ class Insurance {
             field("xfdf:original": "453", "${value(105005) ?: ''}")
             field("xfdf:original": "775", "${value(106001) ?: ''}")
 //            Rekapitulácia poistného
-            field("xfdf:original": "110", "${value(305002) ?: ''}")
-            field("xfdf:original": "112", "${value() ?: ''}") //todo
-            field("xfdf:original": "114", "${value(306001) ?: ''}")
-            field("xfdf:original": "116", "${value(308003) ?: ''}")
-            field("xfdf:original": "118", "${value() ?: ''}") //todo
-            field("xfdf:original": "120", "${value(308004) ?: ''}")
-            field("xfdf:original": "89323", "${value(308006) ?: ''}")
+            field("xfdf:original": "110", "${valueRound(305002) ?: ''}")
+//            field("xfdf:original": "112", "${valueRound() ?: ''}") //todo
+            field("xfdf:original": "114", "${valueRound(306001) ?: ''}")
+            field("xfdf:original": "116", "${valueRound(308003) ?: ''}")
+//            field("xfdf:original": "118", "${valueRound() ?: ''}") //todo
+            field("xfdf:original": "120", "${valueRound(308004) ?: ''}")
+            field("xfdf:original": "89323", "${valueRound(308006) ?: ''}")
             field("xfdf:original": "111", "${value(108003) ?: ''}")
             field("xfdf:original": "113", "${valuePercentageDiscount(208004) ?: ''}")
             field("xfdf:original": "115", "${valuePercentageDiscount(203004) ?: ''}")
             field("xfdf:original": "117", "${valuePercentageDiscount(208008) ?: ''}")
             field("xfdf:original": "119", "${valuePercentageDiscount(208005) ?: ''}")
             field("xfdf:original": "121", "${valuePercentageDiscount(208007) ?: ''}")
-            field("xfdf:original": "mjjttzeželljsd", "${value() ?: ''}") // todo
+//            field("xfdf:original": "mjjttzeželljsd", "${value() ?: ''}") // todo
 //            Úhrada poistného
             field("xfdf:original": "126", "${value(309001) ?: ''}")
 //            Vinkulácia poistného plnenia
             field("xfdf:original": "129", "${value(109004) ?: ''}")
             field("xfdf:original": "130", "${value(109005) ?: ''}")
 //            Indexácia poistnej sumy
-            field("xfdf:original": "131", "${value(109060) ?: ''}")
+            field("xfdf:original": "131", "${valueBool(109060) ?: ''}")
 //            Základné informácie
-            field("xfdf:original": " Text Field 226 ", "${valueBool(101002) ?: ''}")
-            field("xfdf:original": " Text Field 238 ", "${valueBool(101003) ?: ''}")
-            field("xfdf:original": " Text Field 239 ", "${valueBool(101004) ?: ''}")
-            field("xfdf:original": " Text Field 240 ", "${value(101005) ?: ''}")
-            field("xfdf:original": " Text Field 241 ", "${value(101006) ?: ''}")
+            field("xfdf:original": "Text Field 226", "${valueBool(101002) ?: ''}")
+            field("xfdf:original": "Text Field 238", "${valueBool(101003) ?: ''}")
+            field("xfdf:original": "Text Field 239", "${valueBool(101004) ?: ''}")
+            field("xfdf:original": "Text Field 240", "${value(101005) ?: ''}")
+            field("xfdf:original": "Text Field 241", "${value(101006) ?: ''}")
             field("xfdf:original": "Text Field dadadsasda", "${value(101007) ?: ''}")
             dasdasdda("${value(101008) ?: ''}")
-            field("xfdf:original": " Text Field 243 ", "${valueBool(101009) ?: ''}")
-            field("xfdf:original": " Text Field 244 ", "Nie")
-            field("xfdf:original": " Text Field 245 ", "${valueBool(101011) ?: ''}")
-            field("xfdf:original": " Text Field 246 ", "${valueBool(101012) ?: ''}")
-            field("xfdf:original": " Text Field 247 ", "${value(101013) ?: ''}")
-            field("xfdf:original": " Text Field 248 ", "${valueBool(101014) ?: ''}")
-            field("xfdf:original": " Text Field 249 ", "${value(101016) ?: ''}")
+            field("xfdf:original": "Text Field 243", "${valueBool(101009) ?: ''}")
+            field("xfdf:original": "Text Field 244", "Nie")
+            field("xfdf:original": "Text Field 245", "${valueBool(101011) ?: ''}")
+            field("xfdf:original": "Text Field 246", "${valueBool(101012) ?: ''}")
+            field("xfdf:original": "Text Field 247", "${value(101013) ?: ''}")
+            field("xfdf:original": "Text Field 248", "${valueBool(101014) ?: ''}")
+            field("xfdf:original": "Text Field 249", "${value(101016) ?: ''}")
 //            Poistenie nehnutelnosti
             field("xfdf:original": "25", "${value(102001) ?: ''}")
             field("xfdf:original": "26", "${value(102002) ?: ''}")
@@ -159,87 +160,87 @@ class Insurance {
             field("xfdf:original": "31", "${value(102007) ?: ''}")
             ZHSNSAKXCAKS("${value(102008) ?: ''}")
 //            Poistenie nehnutelnosti - plochy
-            field("xfdf:original": "32", "${value(105001) ?: ''}")
-            field("xfdf:original": "33", "${value(105002) ?: ''}")
-            field("xfdf:original": "34", "${value(105003) ?: ''}")
-            field("xfdf:original": "adadafcmmklopíágh", "${value(305001) ?: ''}")
-            field("xfdf:original": "adsadadgnnggtujkklôôp", "${value(105006) ?: ''}")
-            field("xfdf:original": "adadadvvcvbnžťžťtfg", "${value(105036) ?: ''}")
-            field("xfdf:original": "jklopopopoéííikkkjk", "${value(305002) ?: ''}")
+            field("xfdf:original": "32", "${valueRound(105001) ?: ''}")
+            field("xfdf:original": "33", "${valueRound(105002) ?: ''}")
+            field("xfdf:original": "34", "${valueRound(105003) ?: ''}")
+            field("xfdf:original": "adadafcmmklopíágh", "${valueRound(305001) ?: ''}")
+            field("xfdf:original": "adsadadgnnggtujkklôôp", "${valueRound(105006) ?: ''}")
+            field("xfdf:original": "adadadvvcvbnžťžťtfg", "${valueRound(105036) ?: ''}")
+            field("xfdf:original": "jklopopopoéííikkkjk", "${valueRound(305002) ?: ''}")
 //            Poistenie vedľajsich stavieb PS
-            field("xfdf:original": "500", "${value(105010) ?: ''}")
-            field("xfdf:original": "501", "${value(105012) ?: ''}")
-            field("xfdf:original": "502", "${value(105014) ?: ''}")
-            field("xfdf:original": "503", "${value(105016) ?: ''}")
-            field("xfdf:original": "504", "${value(105018) ?: ''}")
-            field("xfdf:original": "505", "${value(105020) ?: ''}")
-            field("xfdf:original": "506", "${value(105022) ?: ''}")
-            field("xfdf:original": "507", "${value(105024) ?: ''}")
-            field("xfdf:original": "508", "${value(105026) ?: ''}")
-            field("xfdf:original": "509", "${value(105028) ?: ''}")
-            field("xfdf:original": "510", "${value(105030) ?: ''}")
+            field("xfdf:original": "500", "${valueRound(105010) ?: ''}")
+            field("xfdf:original": "501", "${valueRound(105012) ?: ''}")
+            field("xfdf:original": "502", "${valueRound(105014) ?: ''}")
+            field("xfdf:original": "503", "${valueRound(105016) ?: ''}")
+            field("xfdf:original": "504", "${valueRound(105018) ?: ''}")
+            field("xfdf:original": "505", "${valueRound(105020) ?: ''}")
+            field("xfdf:original": "506", "${valueRound(105022) ?: ''}")
+            field("xfdf:original": "507", "${valueRound(105024) ?: ''}")
+            field("xfdf:original": "508", "${valueRound(105026) ?: ''}")
+            field("xfdf:original": "509", "${valueRound(105028) ?: ''}")
+            field("xfdf:original": "510", "${valueRound(105030) ?: ''}")
 //            Poistenie vedľajsich stavieb P
-            field("xfdf:original": "513", "${value(305004) ?: ''}")
-            field("xfdf:original": "514", "${value(305005) ?: ''}")
-            field("xfdf:original": "515", "${value(305006) ?: ''}")
-            field("xfdf:original": "516", "${value(305007) ?: ''}")
-            field("xfdf:original": "517", "${value(305008) ?: ''}")
-            field("xfdf:original": "518", "${value(305009) ?: ''}")
-            field("xfdf:original": "519", "${value(305010) ?: ''}")
-            field("xfdf:original": "520", "${value(305011) ?: ''}")
-            field("xfdf:original": "521", "${value(305012) ?: ''}")
-            field("xfdf:original": "522", "${value(305013) ?: ''}")
-            field("xfdf:original": "523", "${value(305014) ?: ''}")
+            field("xfdf:original": "513", "${valueRound(305004) ?: ''}")
+            field("xfdf:original": "514", "${valueRound(305005) ?: ''}")
+            field("xfdf:original": "515", "${valueRound(305006) ?: ''}")
+            field("xfdf:original": "516", "${valueRound(305007) ?: ''}")
+            field("xfdf:original": "517", "${valueRound(305008) ?: ''}")
+            field("xfdf:original": "518", "${valueRound(305009) ?: ''}")
+            field("xfdf:original": "519", "${valueRound(305010) ?: ''}")
+            field("xfdf:original": "520", "${valueRound(305011) ?: ''}")
+            field("xfdf:original": "521", "${valueRound(305012) ?: ''}")
+            field("xfdf:original": "522", "${valueRound(305013) ?: ''}")
+            field("xfdf:original": "523", "${valueRound(305014) ?: ''}")
 //            Poistne garaze
-            field("xfdf:original": "527", "${value(105004) ?: ''}")
-            field("xfdf:original": "525", "${value(105007) ?: ''}")
-            field("xfdf:original": "526", "${value(305003) ?: ''}")
+            field("xfdf:original": "527", "${valueRound(105004) ?: ''}")
+            field("xfdf:original": "525", "${valueRound(105007) ?: ''}")
+            field("xfdf:original": "526", "${valueRound(305003) ?: ''}")
 //            Garáž na inej adrese
             field("xfdf:original": "38", "${value(109057) ?: ''}")
             field("xfdf:original": "39", "${value(109054) ?: ''}")
             field("xfdf:original": "40", "${value(109055) ?: ''}")
             field("xfdf:original": "41", "${value(109056) ?: ''}")
 //            Doplnkove nehnutelnosti
-            asdasdasdadd("${value(105032) ?: ''}")
-            field("xfdf:original": "529", "${value(305015) ?: ''}")
-            field("xfdf:original": "528", "${value(105034) ?: ''}")
-            field("xfdf:original": "530", "${value(305016) ?: ''}")
+            asdasdasdadd("${valueRound(105032) ?: ''}")
+            field("xfdf:original": "529", "${valueRound(305015) ?: ''}")
+            field("xfdf:original": "528", "${valueRound(105034) ?: ''}")
+            field("xfdf:original": "530", "${valueRound(305016) ?: ''}")
 //            Poistenie zodp. za skodu - nehnutelnost
             field("xfdf:original": "531", "${value(107001) ?: ''}")
-            field("xfdf:original": "532", "${value(308008) ?: ''}")
+            field("xfdf:original": "532", "${valueRound(308008) ?: ''}")
 //            Poistenie domácnosti
             field("xfdf:original": "67", "${value(103001) ?: ''}")
             field("xfdf:original": "68", "${value(103002) ?: ''}")
             field("xfdf:original": "69", "${valueBool(103003) ?: ''}")
             field("xfdf:original": "70", "${valueBool(103004) ?: ''}")
             field("xfdf:original": "71", "${valueBool(103005) ?: ''}")
-            field("xfdf:original": "72", "${value(106003) ?: ''}")
-            field("xfdf:original": "73", "${value(106002) ?: ''}")
-            field("xfdf:original": "74", "${value(106022) ?: ''}")
-            field("xfdf:original": "vvbcbcvcvcvnmjkl,,,", "${value(306001) ?: ''}")
+            field("xfdf:original": "72", "${valueRound(106003) ?: ''}")
+            field("xfdf:original": "73", "${valueRound(106002) ?: ''}")
+            field("xfdf:original": "74", "${valueRound(106022) ?: ''}")
+            field("xfdf:original": "vvbcbcvcvcvnmjkl,,,", "${valueRound(306001) ?: ''}")
 //            Doplnkové poistenie domácnosti - poistna suma
-            field("xfdf:original": "91", "${value(106005) ?: ''}")
-            field("xfdf:original": "93", "${value(106007) ?: ''}")
-            field("xfdf:original": "95", "${value(106009) ?: ''}")
-            field("xfdf:original": "97", "${value(106011) ?: ''}")
-            field("xfdf:original": "99", "${value(106013) ?: ''}")
-            field("xfdf:original": "101", "${value(106015) ?: ''}")
-            field("xfdf:original": "103", "${value(106017) ?: ''}")
-            field("xfdf:original": "105", "${value(106019) ?: ''}")
+            field("xfdf:original": "91", "${valueRound(106005) ?: ''}")
+            field("xfdf:original": "93", "${valueRound(106007) ?: ''}")
+            field("xfdf:original": "95", "${valueRound(106009) ?: ''}")
+            field("xfdf:original": "97", "${valueRound(106011) ?: ''}")
+            field("xfdf:original": "99", "${valueRound(106013) ?: ''}")
+            field("xfdf:original": "101", "${valueRound(106015) ?: ''}")
+            field("xfdf:original": "103", "${valueRound(106017) ?: ''}")
+            field("xfdf:original": "105", "${valueRound(106019) ?: ''}")
             fvbgggztzhjg("${value(106021) ?: ''}")
 //            Doplnkové poistenie domácnosti - poistne
-            field("xfdf:original": "92", "${value(306002) ?: ''}")
-            field("xfdf:original": "94", "${value(306003) ?: ''}")
-            field("xfdf:original": "96", "${value(306004) ?: ''}")
-            field("xfdf:original": "98", "${value(306005) ?: ''}")
-            field("xfdf:original": "100", "${value(306006) ?: ''}")
-            field("xfdf:original": "102", "${value(306007) ?: ''}")
-            field("xfdf:original": "104", "${value(306008) ?: ''}")
-            field("xfdf:original": "sdasdavkphouíáít", "${value(306009) ?: ''}")
-            field("xfdf:original": "fdsfsfsdtttttttgdsfsôôôôô", "${value(306010) ?: ''}")
+            field("xfdf:original": "92", "${valueRound(306002) ?: ''}")
+            field("xfdf:original": "94", "${valueRound(306003) ?: ''}")
+            field("xfdf:original": "96", "${valueRound(306004) ?: ''}")
+            field("xfdf:original": "98", "${valueRound(306005) ?: ''}")
+            field("xfdf:original": "100", "${valueRound(306006) ?: ''}")
+            field("xfdf:original": "102", "${valueRound(306007) ?: ''}")
+            field("xfdf:original": "104", "${valueRound(306008) ?: ''}")
+            field("xfdf:original": "sdasdavkphouíáít", "${valueRound(306009) ?: ''}")
+            field("xfdf:original": "fdsfsfsdtttttttgdsfsôôôôô", "${valueRound(306010) ?: ''}")
 //            Zodp. za skodu domacnost
             field("xfdf:original": "ôôlll", "${value(107003) ?: ''}")
-            ldkdd("${value(308009) ?: ''}")
+            ldkdd("${valueRound(308009) ?: ''}")
             field("xfdf:original": "pooríííčáčôfúääňňsss", "${value(104003) ?: ''}")
 //            Sposob uhradenia
 //            x("${value() ?: ''}") //todo
@@ -251,7 +252,7 @@ class Insurance {
 //            X3("${value() ?: ''}") //todo
 //            X4("${value() ?: ''}") //todo
 
-            field("xfdf:original": " Text Field 153 ", "${value(309001) ?: ''}")
+            field("xfdf:original": "Text Field 153", "${value(309001) ?: ''}")
         }
 
         return writer.toString()
@@ -415,7 +416,7 @@ class Insurance {
      * @param id data field import id
      * @return <b>'Áno'</b> if value is true, <b>'Nie'</b> otherwise <br><b>null</b> if value is null
      */
-    private Boolean valueBool(Long id) {
+    private String valueBool(Long id) {
         def value = value(id)
         if (value == null)
             return null
@@ -444,7 +445,21 @@ class Insurance {
         def value = value(id)
         if (value == null)
             return null
+//        todo
+        return value
+    }
 
+    /**
+     *
+     * @param id
+     * @param precision
+     * @return
+     */
+    private String valueRound(Long id, int precision = 2) {
+        def value = value(id)
+        if (value == null)
+            return null
 
+        return (value as Double).round(precision) as String
     }
 }
