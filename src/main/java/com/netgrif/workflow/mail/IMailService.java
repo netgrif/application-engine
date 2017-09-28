3fd82e5c8ae89da043bf6ad6aeb2e85ea09480f1
Juraj Mažári <mazari@netgrif.com>
2017-09-28 07:29:30 +0200

    PI-120 - PDF - Send PDF by mail - add auto transition - add sendMail method

--- src/main/java/com/netgrif/workflow/mail/IMailService.java
+++ src/main/java/com/netgrif/workflow/mail/IMailService.java
@@ -1,9 +1,13 @@
 package com.netgrif.workflow.mail;
 
 import javax.mail.MessagingException;
+import java.io.File;
 import java.net.UnknownHostException;
 
 public interface IMailService {
     void sendRegistrationEmail(String recipient, String token) throws MessagingException, UnknownHostException;
+
+    void sendDraftEmail(String recipient, File pdf) throws MessagingException;
+
     void testConnection();
 }
