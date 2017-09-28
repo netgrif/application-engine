ddeb53c91a1863021032dcd1a62f018a32bb1106
Juraj Mažári <mazari@netgrif.com>
2017-09-28 14:20:28 +0200

    PI-162 - Send PDF by mail - add blank email template

--- src/main/java/com/netgrif/workflow/mail/IMailService.java
+++ src/main/java/com/netgrif/workflow/mail/IMailService.java
@@ -3,11 +3,12 @@ package com.netgrif.workflow.mail;
 import javax.mail.MessagingException;
 import java.io.File;
 import java.net.UnknownHostException;
+import java.util.Map;
 
 public interface IMailService {
     void sendRegistrationEmail(String recipient, String token) throws MessagingException, UnknownHostException;
 
-    void sendDraftEmail(String recipient, File pdf) throws MessagingException, UnknownHostException;
+    void sendDraftEmail(String recipient, File pdf, Map<String, Object> model) throws MessagingException, UnknownHostException;
 
     void testConnection();
 }
