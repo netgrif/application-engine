275ca19549ee9b5aaf91a76785f5ff062581e17f
Juraj Mažári <mazari@netgrif.com>
2017-11-10 11:21:38 +0100

    WMS-199 - Events - add user login event - add user logout event

---
+++ src/main/java/com/netgrif/workflow/event/events/UserLoginEvent.java
@@ -0,0 +1,16 @@
+package com.netgrif.workflow.event.events;
+
+import com.netgrif.workflow.auth.domain.LoggedUser;
+import com.netgrif.workflow.utils.DateUtils;
+
+public class UserLoginEvent extends UserEvent {
+
+    public UserLoginEvent(LoggedUser user) {
+        super(user);
+    }
+
+    @Override
+    public String getMessage() {
+        return "User " + user.getUsername() + " logged in on " + DateUtils.toString(time);
+    }
+}
