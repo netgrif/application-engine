f12e21917f4d4e6402767592d2fcb163e759e089
Juraj Mažári <mazari@netgrif.com>
2018-01-09 08:04:13 +0100

    External Mapping - update i18 generator command

--- commands/generate_i18n.groovy
+++ commands/generate_i18n.groovy
@@ -1,10 +1,26 @@
-def FILE_PATH = "../src/test/resources/mapping_test.xml"
+import groovy.xml.MarkupBuilder
+
+def FILE_PATH
+def LOCALE
+
+println "Model path:"
+FILE_PATH = System.in.newReader().readLine()
+println "Locale:"
+LOCALE = System.in.newReader().readLine()
 
 def I18N_REGEX = /name="[a-zA-Z_]+"/
 
 def file = new File(FILE_PATH)
 def names = file.text.findAll(I18N_REGEX)
+def writer = new StringWriter()
+def xml = new MarkupBuilder(writer)
 
-names.each {
-    println it
+xml.i18n(locale: LOCALE) {
+    names.each { name ->
+        xml.i18nString("name": "${name.substring(6, name.length() - 1)}", "")
+    }
 }
+
+new File(file.name).text = writer.toString()
+
+println "Locale generated"
