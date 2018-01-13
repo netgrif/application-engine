d65cd86b01c2d3bb10310e42de545226dda8684d
Juraj Mažári <mazari@netgrif.com>
2018-01-13 23:31:20 +0100

    NAE-11 - Model failed to upload - change ModelEventLog content to String - MongoDB fails saving file - add title and initials to UserImportModelEvent

--- src/main/java/com/netgrif/workflow/history/service/listener/ModelEventListener.java
+++ src/main/java/com/netgrif/workflow/history/service/listener/ModelEventListener.java
@@ -3,9 +3,12 @@ package com.netgrif.workflow.history.service.listener;
 import com.netgrif.workflow.event.events.model.UserImportModelEvent;
 import com.netgrif.workflow.history.domain.ModelEventLog;
 import com.netgrif.workflow.history.domain.repository.EventLogRepository;
+import org.aspectj.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class ModelEventListener {
@@ -16,7 +19,11 @@ public class ModelEventListener {
     @EventListener
     public void onUserImportModelEvent(UserImportModelEvent event) {
         ModelEventLog log = new ModelEventLog();
-        log.setModel(event.getModel());
+        try {
+            log.setModel(FileUtil.readAsString(event.getModel()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         log.setMessage(event.getMessage());
         repository.save(log);
     }
