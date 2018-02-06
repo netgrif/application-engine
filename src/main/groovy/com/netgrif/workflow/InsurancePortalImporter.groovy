a15c9aeb8d0be4aefd1ce6c14e6921a98a456778
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-02-06 16:21:27 +0100

    NAE-2 - Tasks - add transform methods to User object - add creation date to Case object - improve creation of the super user - add test cases

--- src/main/groovy/com/netgrif/workflow/InsurancePortalImporter.groovy
+++ src/main/groovy/com/netgrif/workflow/InsurancePortalImporter.groovy
@@ -17,18 +17,23 @@ class InsurancePortalImporter {
     @Autowired
     private ImportHelper importHelper
 
-    void run(String ...strings) {
-        def net = importHelper.createNet("insurance_portal_demo.xml","Insurance Demo", "IPD")
+    @Autowired
+    private SuperCreator superCreator
+
+    void run(String... strings) {
+        def net = importHelper.createNet("insurance_portal_demo.xml", "Insurance Demo", "IPD", superCreator.superUser.transformToLoggedUser())
 
         assert net.isPresent()
 
         def org = importHelper.createOrganization("Insurance Company")
-        def auths = importHelper.createAuthorities(["user":Authority.user, "admin":Authority.admin])
-        def processRoles = importHelper.createUserProcessRoles(["agent":"Agent", "company":"Company"], net.get())
+        def auths = importHelper.createAuthorities(["user": Authority.user])
+        def processRoles = importHelper.createUserProcessRoles(["agent": "Agent", "company": "Company"], net.get())
 
-        importHelper.createUser(new User(name: "Agent", surname: "Smith", email: "agent@company.com", password: "password"),
+        def user = importHelper.createUser(new User(name: "Agent", surname: "Smith", email: "agent@company.com", password: "password"),
                 [auths.get("user")] as Authority[], [org] as Organization[], [processRoles.get("agent")] as UserProcessRole[])
         importHelper.createUser(new User(name: "Great", surname: "Company", email: "company@company.com", password: "password"),
                 [auths.get("user")] as Authority[], [org] as Organization[], [processRoles.get("company")] as UserProcessRole[])
+
+        5.times { importHelper.createCase("Test ${it + 1}", net.get(), user.transformToLoggedUser()) }
     }
 }
