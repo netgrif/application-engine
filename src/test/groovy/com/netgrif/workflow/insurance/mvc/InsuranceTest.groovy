8aa36c89933a21754905815c74d779831878959c
Juraj Mažári <mazari@netgrif.com>
2018-02-06 16:21:38 +0100

    NAE-42 - Integration tests - implement set data methods - change test model

--- src/test/groovy/com/netgrif/workflow/insurance/mvc/InsuranceTest.groovy
+++ src/test/groovy/com/netgrif/workflow/insurance/mvc/InsuranceTest.groovy
@@ -66,6 +66,12 @@ class InsuranceTest {
     private static final String TASK_OFFER = "Údaje o zmluve"
     private static final String TASK_END = "Základné  informácie"
 
+    private static final String FIELD_TEXT = "text"
+    private static final String FIELD_ENUM = "enumeration"
+    private static final String FIELD_BOOL = "boolean"
+    private static final String FIELD_NUM = "number"
+    private static final String FIELD_DATE = "date"
+
     private static final String LOCALE_SK = "sk"
 
     private static final String CASE_NAME = "Test case"
@@ -106,12 +112,14 @@ class InsuranceTest {
                 [processRoles.get("agent"), processRoles.get("company")] as UserProcessRole[])
 
         auth = new UsernamePasswordAuthenticationToken(USER_EMAIL, "password")
+
+        mapper = net.get().dataSet.collectEntries { [(it.value.importId as int): (it.key)] }
     }
 
     private String caseId
     private String netId
     private String taskId
-    private def datagroups
+    private Map mapper
 
     @Test
     void test() {
@@ -124,8 +132,8 @@ class InsuranceTest {
         household()
         householdAdditional()
         summary()
+        info()
         offer()
-        end()
     }
 
     def coverType() {
@@ -137,57 +145,63 @@ class InsuranceTest {
     def basicInfo() {
         searchTasks(TASK_BASIC_INFO, 1)
         assignTask()
-        getData()
-//        setData()
-        getData()
+        setDataBasicInfo()
         finishTask()
     }
 
     def property() {
         searchTasks(TASK_PROPERTY, 2)
         assignTask()
+        setDataProperty()
         finishTask()
     }
 
     def propertyAdditional() {
         searchTasks(TASK_PROPERTY_ADDITIONAL, 3)
         assignTask()
+        setDataPropertyAdditional()
         finishTask()
     }
 
     def propertyBuildings() {
         searchTasks(TASK_PROPERTY_BUILDINGS, 4)
         assignTask()
+        setDataPropertyBuildings()
         finishTask()
     }
 
     def household() {
         searchTasks(TASK_HOUSEHOLD, 5)
         assignTask()
+        setDataHousehold()
         finishTask()
     }
 
     def householdAdditional() {
         searchTasks(TASK_HOUSEHOLD_ADDITIONAL, 6)
         assignTask()
+        setDataHouseholdAdditional()
         finishTask()
     }
 
     def summary() {
         searchTasks(TASK_SUMMARY, 7)
         assignTask()
+        setDataSummary()
         finishTask()
     }
 
     def info() {
         searchTasks(TASK_INFO, 8)
         assignTask()
+        setDataInfo()
         finishTask()
     }
 
     def offer() {
         searchTasks(TASK_OFFER, 9)
         assignTask()
+        setDataOffer()
         finishTask()
     }
 
@@ -243,6 +257,7 @@ class InsuranceTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath('$.success', CoreMatchers.isA(String)))
                 .andReturn()
+        getData()
     }
 
     def finishTask() {
@@ -257,15 +272,479 @@ class InsuranceTest {
     }
 
     def getData() {
-        def result = mvc.perform(get(TASK_DATA_URL(taskId))
+        mvc.perform(get(TASK_DATA_URL(taskId))
                 .accept(APPLICATION_JSON, TEXT_PLAIN)
                 .locale(Locale.forLanguageTag(LOCALE_SK))
                 .with(csrf().asHeader())
                 .with(authentication(this.auth)))
                 .andExpect(status().isOk())
                 .andReturn()
+    }
+
+    def setData(Map data) {
+        def content = JsonOutput.toJson(data)
+        def result = mvc.perform(post(TASK_DATA_URL(taskId))
+                .accept(APPLICATION_JSON, TEXT_PLAIN)
+                .locale(Locale.forLanguageTag(LOCALE_SK))
+                .content(content)
+                .contentType(APPLICATION_JSON)
+                .with(csrf().asHeader())
+                .with(authentication(this.auth)))
+                .andExpect(status().isOk())
+                .andReturn()
         def response = parseResult(result)
-        datagroups = response?._embedded?.dataGroups //TODO: fix get data
+        getData()
+    }
+
+    def setDataBasicInfo() {
+        setData([
+                (mapper[101001]): [
+                        value: "84105",
+                        type : FIELD_TEXT
+                ]
+        ])
+        def data = [
+                (mapper[301005]): [
+                        value: "Bratislava",
+                        type : FIELD_ENUM
+                ],
+                (mapper[101002]): [
+                        value: false,
+                        type : FIELD_BOOL
+                ],
+                (mapper[101003]): [
+                        value: false,
+                        type : FIELD_BOOL
+                ],
+                (mapper[101004]): [
+                        value: false,
+                        type : FIELD_BOOL
+                ],
+                (mapper[101005]): [
+                        value: "6 až 10",
+                        type : FIELD_ENUM
+                ],
+                (mapper[101006]): [
+                        value: "vlastník nehnuteľnosti",
+                        type : FIELD_ENUM
+                ],
+                (mapper[101007]): [
+                        value: "2",
+                        type : FIELD_ENUM
+                ],
+                (mapper[101008]): [
+                        value: "1",
+                        type : FIELD_ENUM
+                ],
+                (mapper[101012]): [
+                        value: false,
+                        type : FIELD_BOOL
+                ],
+                (mapper[101014]): [
+                        value: false,
+                        type : FIELD_BOOL
+                ],
+                (mapper[101016]): [
+                        value: "0",
+                        type : FIELD_ENUM
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataProperty() {
+        def data = [
+                (mapper[102001]): [
+                        value: "byt",
+                        type : FIELD_ENUM
+                ],
+                (mapper[105005]): [
+                        value: "50.00 €",
+                        type : FIELD_ENUM
+                ],
+                (mapper[105001]): [
+                        value: 10,
+                        type : FIELD_NUM,
+                ],
+                (mapper[105002]): [
+                        value: 20,
+                        type : FIELD_NUM,
+                ],
+                (mapper[105003]): [
+                        value: 30,
+                        type : FIELD_NUM,
+                ],
+                (mapper[102002]): [
+                        value: "tehla a/alebo betón",
+                        type : FIELD_ENUM
+                ],
+                (mapper[102003]): [
+                        value: "škridla",
+                        type : FIELD_ENUM
+                ],
+                (mapper[102004]): [
+                        value: "6 až 10",
+                        type : FIELD_ENUM
+                ],
+                (mapper[102006]): [
+                        value: "1",
+                        type : FIELD_ENUM
+                ],
+                (mapper[102007]): [
+                        value: "1",
+                        type : FIELD_ENUM
+                ],
+        ]
+        setData(data)
+        data = [
+                (mapper[107001]): [
+                        value: "15,000.00 €",
+                        type : FIELD_ENUM,
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataPropertyAdditional() {
+        def data = [
+                (mapper[105031]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105033]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ]
+        ]
+        setData(data)
+        data = [
+                (mapper[105032]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105034]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataPropertyBuildings() {
+        def data = [
+                (mapper[105035]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105009]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105011]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105013]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105015]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105017]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105019]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105021]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105023]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105025]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105027]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105029]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ]
+        ]
+        setData(data)
+        data = [
+                (mapper[105004]): [
+                        value: 100,
+                        type : FIELD_NUM
+                ],
+                (mapper[105008]): [
+                        value: false,
+                        type : FIELD_BOOL
+                ],
+                (mapper[105007]): [
+                        value: 90_000,
+                        type : FIELD_NUM
+                ],
+                (mapper[105010]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105012]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105014]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105016]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105018]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105020]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105022]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105024]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105026]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105028]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[105030]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+        ]
+        setData(data)
+    }
+
+    def setDataHousehold() {
+        def data = [
+                (mapper[103001]): [
+                        value: "byt",
+                        type : FIELD_ENUM
+                ],
+                (mapper[106001]): [
+                        value: "150.00 €",
+                        type : FIELD_ENUM
+                ],
+                (mapper[106003]): [
+                        value: 100,
+                        type : FIELD_NUM
+                ],
+                (mapper[103002]): [
+                        value: "trvalá",
+                        type : FIELD_ENUM
+                ],
+                (mapper[103004]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[103005]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ]
+        ]
+        setData(data)
+        data = [
+                (mapper[107003]): [
+                        value: "15,000.00 €",
+                        type : FIELD_ENUM
+                ],
+                (mapper[104003]): [
+                        value: "Slovenská republika",
+                        type : FIELD_ENUM
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataHouseholdAdditional() {
+        def data = [
+                (mapper[106004]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106006]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106008]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106010]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106012]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106014]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106016]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106018]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[106020]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ]
+        ]
+        setData(data)
+        data = [
+                (mapper[106005]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106007]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106009]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106011]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106013]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106015]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106017]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106019]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ],
+                (mapper[106021]): [
+                        value: 500,
+                        type : FIELD_NUM
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataSummary() {
+        def data = [
+                (mapper[108001]): [
+                        value: "polročná",
+                        type : FIELD_ENUM
+                ],
+                (mapper[108002]): [
+                        value: true,
+                        type : FIELD_BOOL
+                ],
+                (mapper[108003]): [
+                        value: "20%",
+                        type : FIELD_ENUM
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataInfo() {
+        def data = [
+                (mapper[109007]): [
+                        value: "fyzická osoba",
+                        type : FIELD_ENUM
+                ],
+                (mapper[109010]): [
+                        value: "meno",
+                        type : FIELD_TEXT
+                ],
+                (mapper[109011]): [
+                        value: "priezvisko",
+                        type : FIELD_TEXT
+                ],
+                (mapper[109016]): [
+                        value: "OP",
+                        type : FIELD_ENUM
+                ],
+                (mapper[109017]): [
+                        value: "AB123456",
+                        type : FIELD_TEXT
+                ],
+                (mapper[109013]): [
+                        value: "SR",
+                        type : FIELD_ENUM
+                ],
+                (mapper[109014]): [
+                        value: "2018-02-05",
+                        type : FIELD_DATE
+                ],
+                (mapper[109015]): [
+                        value: "1234567890",
+                        type : FIELD_TEXT
+                ],
+                (mapper[109019]): [
+                        value: "test@test.com",
+                        type : FIELD_TEXT
+                ],
+                (mapper[109045]): [
+                        value: "ulica",
+                        type : FIELD_TEXT
+                ],
+                (mapper[109046]): [
+                        value: "1",
+                        type : FIELD_TEXT
+                ]
+        ]
+        setData(data)
+    }
+
+    def setDataOffer() {
+        def data = [
+                (mapper[109001]): [
+                        value:"2018-02-21",
+                        type :FIELD_DATE
+                ],
+                (mapper[109006]): [
+                        value:"prevodom",
+                        type :FIELD_ENUM
+                ]
+        ]
+        setData(data)
     }
 
     @SuppressWarnings("GrMethodMayBeStatic")
