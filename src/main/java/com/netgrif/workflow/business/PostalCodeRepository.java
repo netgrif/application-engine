3bf2feaaf5d53dd6dc5888766053d47b4590d2d5
Juraj Mažári <mazari@netgrif.com>
2018-02-07 10:15:16 +0100

    NAE-53 - Test services - cherry-pick PSC service - add PSC service test - fix other tests

---
+++ src/main/java/com/netgrif/workflow/business/PostalCodeRepository.java
@@ -0,0 +1,14 @@
+package com.netgrif.workflow.business;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostalCodeRepository extends JpaRepository<PostalCode, String> {
+
+    List<PostalCode> findAllByCode(String code);
+
+    List<PostalCode> findAllByCity(String city);
+}
