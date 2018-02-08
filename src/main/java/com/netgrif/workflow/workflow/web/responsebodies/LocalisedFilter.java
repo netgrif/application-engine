dc262ae31aaf5ba8c35425ab28e7dcb2b6bad715
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-02-08 19:27:28 +0100

    NAE-2 - Tasks - update filter object to new structure - update filter to support localised strings - add dedicated controller for filters

---
+++ src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedFilter.java
@@ -0,0 +1,43 @@
+package com.netgrif.workflow.workflow.web.responsebodies;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.netgrif.workflow.auth.domain.Author;
+import com.netgrif.workflow.workflow.domain.Filter;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Data
+@JsonRootName("filter")
+public class LocalisedFilter {
+
+    private String stringId;
+
+    private String title;
+
+    private String description;
+
+    private Integer visibility;
+
+    private Author author;
+
+    private LocalDateTime created;
+
+    private String type;
+
+    private String query;
+
+    public LocalisedFilter(Filter filter, Locale locale) {
+        this.stringId = filter.getStringId();
+        this.title = filter.getTitle().getTranslation(locale);
+        this.description = filter.getDescription().getTranslation(locale);
+        this.visibility = filter.getVisibility();
+        this.author = filter.getAuthor();
+        this.created = filter.getCreated();
+        this.type = filter.getType();
+        this.query = filter.getQuery();
+    }
+
+
+}
