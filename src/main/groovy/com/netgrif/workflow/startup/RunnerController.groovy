bc5bdaa617d79bc9b522fb700d19b1dbe9dde1b8
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-03-09 15:31:05 +0100

    NAE-125 - Switching models in admin console (user tab) - add runner to set super user as a member of all groups and set all process roles - fix endpoint for get all users to contain logged user

--- src/main/groovy/com/netgrif/workflow/startup/RunnerController.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/RunnerController.groovy
@@ -14,6 +14,7 @@ class RunnerController {
             FlushSessionsRunner,
             MailRunner,
             PostalCodeImporter,
+            FinisherRunner
     ]
 
     protected List getOrderList() {
