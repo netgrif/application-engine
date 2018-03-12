f6d0d4c1e22a7a941fa6fc87ee4156e2822d05d1
Juraj Mažári <mazari@netgrif.com>
2018-03-12 11:44:41 +0100

    PI-206 - Email update - generalize mail service

--- src/main/java/com/netgrif/workflow/mail/IMailService.java
+++ src/main/java/com/netgrif/workflow/mail/IMailService.java
@@ -3,14 +3,10 @@ package com.netgrif.workflow.mail;
 import freemarker.template.TemplateException;
 
 import javax.mail.MessagingException;
-import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 public interface IMailService {
     void sendRegistrationEmail(String recipient, String token) throws MessagingException, IOException, TemplateException;
-
-    void sendDraftEmail(String recipient, File pdf, Map<String, Object> model) throws MessagingException, IOException, TemplateException;
 
     void testConnection();
 }
