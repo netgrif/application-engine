d7d041b7421fff38cd510ad38a772d30b9b4f24b
Juraj Mažári <mazari@netgrif.com>
2018-03-16 01:16:21 +0100

    fix datagroup alignment

--- src/main/java/com/netgrif/workflow/importer/model/Mapping.java
+++ src/main/java/com/netgrif/workflow/importer/model/Mapping.java
@@ -1,12 +1,12 @@
 
 package com.netgrif.workflow.importer.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
