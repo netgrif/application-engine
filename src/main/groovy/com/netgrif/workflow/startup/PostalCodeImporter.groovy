4db4fe047aaa8452e9feff913600fa0a61afbfd7
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-03-16 02:06:16 +0100

    PI-217 - Update configuration - add update profile for updating server and keeping all persisted data - better flush session - clean storage folder - move log to log folder

--- src/main/groovy/com/netgrif/workflow/startup/PostalCodeImporter.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/PostalCodeImporter.groovy
@@ -5,10 +5,12 @@ import com.netgrif.workflow.business.PostalCode
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("!update")
 class PostalCodeImporter extends AbstractOrderedCommandLineRunner {
 
     private static final Logger log = Logger.getLogger(PostalCodeImporter.class.name)
--- src/main/groovy/com/netgrif/workflow/startup/StorageRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/StorageRunner.groovy
@@ -1,8 +1,12 @@
 package com.netgrif.workflow.startup
 
 import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
+
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @Component
 @Profile("!test")
@@ -10,10 +14,34 @@ class StorageRunner extends AbstractOrderedCommandLineRunner {
 
     private static final Logger log = Logger.getLogger(StorageRunner)
 
+    @Value('${storage.clean}')
+    private boolean cleanStorage
+
     @Override
     void run(String... strings) throws Exception {
         log.info("Creating storage folder")
         File storage = new File("storage/generated/start.txt")
         storage.getParentFile().mkdirs()
+        storage = new File("storage/uploadedModels/model.txt")
+        storage.getParentFile().mkdirs()
+
+        if(cleanStorage) {
+            log.info("Removing files from storage folder and it's sub-folders")
+            purgeDirectory(new File("storage"))
+        }
+
+
+        log.info("Creating log folder")
+        File logDir = new File("log/log.txt")
+        logDir.getParentFile().mkdirs()
+    }
+
+    private static void purgeDirectory(File dir){
+        for(File file: dir.listFiles()){
+            if(file.isDirectory())
+                purgeDirectory(file)
+            else
+                file.delete()
+        }
     }
 }
