b0750fd080b6437ba8e4ea3762cce977cc4837c4
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-03-17 22:36:58 +0100

    NAE-152 - Add today shorcut to date validation - add keyword today for date field validation - replace infinity constant in date field with past and future keywords

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/validation/DateValidationDelegate.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/validation/DateValidationDelegate.groovy
@@ -4,30 +4,45 @@ import com.netgrif.workflow.petrinet.domain.dataset.Field
 
 import java.time.LocalDate
 
-class DateValidationDelegate extends ValidationDelegate{
-
-    protected static final int INFINITY = 0
+class DateValidationDelegate extends ValidationDelegate {
 
     DateValidationDelegate(Field field) {
         super(field)
     }
 
-    def inf = { return INFINITY }
+    protected static LocalDate parseDate(Object date) {
+        if(date instanceof Closure)
+            return date()
+        if(date instanceof String)
+            return LocalDate.parse(date)
+        else
+            return null
+    }
+
+    def future = { return LocalDate.MAX }
+
+    def past = { return LocalDate.MIN }
+
+    def today = { return LocalDate.now() }
 
     def between = { start, end ->
-        LocalDate val = (LocalDate)field.value
-        if(start instanceof Closure && start() == INFINITY) return val.isBefore(LocalDate.parse((String)end))
-        if(end instanceof Closure && end() == INFINITY) return val.isAfter(LocalDate.parse((String)start))
-        return val.isAfter(LocalDate.parse((String)start)) && val.isBefore(LocalDate.parse((String)end))
+        LocalDate val = (LocalDate) field.value
+
+        LocalDate startDate = parseDate(start)
+        LocalDate endDate = parseDate(end)
+
+        if(startDate == null || endDate == null)
+            return false
+        return val.isAfter(startDate) && val.isBefore(endDate)
     }
 
     def workday = {
-        int dayOfWeek = ((LocalDate)field.value).getDayOfWeek().value
+        int dayOfWeek = ((LocalDate) field.value).getDayOfWeek().value
         return dayOfWeek >= 1 && dayOfWeek <= 5
     }
 
     def weekend = {
-        int dayOfWeek = ((LocalDate)field.value).getDayOfWeek().value
+        int dayOfWeek = ((LocalDate) field.value).getDayOfWeek().value
         return dayOfWeek == 6 || dayOfWeek == 7
     }
 }
