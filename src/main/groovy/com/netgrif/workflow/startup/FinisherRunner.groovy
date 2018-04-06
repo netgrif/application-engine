907e398544ae45d04a8bea931bc797e19e981c29
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-04-06 06:09:11 +0200

    NAE-169 - Petriflow nets versions - clear up finisher runner

--- src/main/groovy/com/netgrif/workflow/startup/FinisherRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/FinisherRunner.groovy
@@ -18,27 +18,6 @@ class FinisherRunner extends AbstractOrderedCommandLineRunner{
 
     @Override
     void run(String... strings) throws Exception {
-        LoggedUser user = new LoggedUser(1,"Super","password",new ArrayList<GrantedAuthority>())
-
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/insurance_portal_demo.xml"),
-                new UploadedFileMeta("Insurance prve","INS","Insurance","MAJOR"),user)
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/insurance_portal_demo.xml"),
-                new UploadedFileMeta("Insurance druhe","INS","Insurance","Patch"),user)
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/insurance_portal_demo.xml"),
-                new UploadedFileMeta("Insurance third","INS","Insurance","minor"),user)
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/insurance_role_test.xml"),
-                new UploadedFileMeta("Insurance foooo","INS","Insurance","patch"),user)
-
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/FM_v0_2.xml")
-                , new UploadedFileMeta("FMko prvé", "FM", "FM", "Major"),user)
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/FM_v0_2.xml")
-                , new UploadedFileMeta("FMko druhé", "FM", "FM", "minor"),user)
-        petriNetService.importPetriNet(new File("src/main/resources/petriNets/FM_v0_2.xml")
-                , new UploadedFileMeta("FMko tretucke", "FM", "FM", "patch"),user)
-
-        petriNetService.getReferencesByVersion("^",user, Locale.US)
-
-
         superCreator.setAllToSuperUser()
     }
 }
