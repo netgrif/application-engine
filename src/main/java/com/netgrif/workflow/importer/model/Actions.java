8bbea38334842371016dcc7669e366422ba93c58
Juraj Mažári <mazari@netgrif.com>
2018-04-12 17:23:01 +0200

    NAE-194 - Task event listener - add event element

---
+++ src/main/java/com/netgrif/workflow/importer/model/Actions.java
@@ -0,0 +1,67 @@
+
+package com.netgrif.workflow.importer.model;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element ref="{}action" maxOccurs="unbounded"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "action"
+})
+@XmlRootElement(name = "actions")
+public class Actions {
+
+    @XmlElement(required = true)
+    protected List<ActionType> action;
+
+    /**
+     * Gets the value of the action property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the action property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getAction().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link ActionType }
+     * 
+     * 
+     */
+    public List<ActionType> getAction() {
+        if (action == null) {
+            action = new ArrayList<ActionType>();
+        }
+        return this.action;
+    }
+
+}
