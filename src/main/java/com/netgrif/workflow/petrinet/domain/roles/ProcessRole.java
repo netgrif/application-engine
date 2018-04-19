df602353bafbdab55ea90e27c8b641f98ac05340
Juraj Mažári <mazari@netgrif.com>
2018-04-19 15:14:25 +0200

    NAE-225 - Map keys overhaul - change import id to string

--- src/main/java/com/netgrif/workflow/petrinet/domain/roles/ProcessRole.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/roles/ProcessRole.java
@@ -1,6 +1,9 @@
 package com.netgrif.workflow.petrinet.domain.roles;
 
 import com.netgrif.workflow.petrinet.domain.I18nString;
+import com.netgrif.workflow.petrinet.domain.Imported;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,15 +11,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Locale;
 
 @Document
-public class ProcessRole {
+@Getter
+public class ProcessRole extends Imported {
 
     public static final String DEFAULT_ROLE = "default";
 
     @Id
+    @Setter
     private ObjectId _id;
 
     private I18nString name;
 
+    @Setter
     private String description;
 
     public ProcessRole() {
@@ -24,19 +30,7 @@ public class ProcessRole {
     }
 
     public String getStringId() {
-        return _id.toString();
-    }
-
-    public ObjectId get_id() {
-        return _id;
-    }
-
-    public void set_id(ObjectId _id) {
-        this._id = _id;
-    }
-
-    public I18nString getName() {
-        return name;
+        return importId;
     }
 
     public void setName(String name) {
@@ -47,17 +41,14 @@ public class ProcessRole {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getLocalisedName(Locale locale) {
         if (name == null)
             return null;
         return name.getTranslation(locale);
+    }
+
+    @Override
+    public String toString() {
+        return name.getDefaultValue();
     }
 }
