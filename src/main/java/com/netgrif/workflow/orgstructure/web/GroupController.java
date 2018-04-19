9703c36da54252de5bdb664a1459f3486d5c7917
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-04-19 18:09:37 +0200

    NAE-160 - Forgot password / self registration - refactor endpoint base path from /res to /api

--- src/main/java/com/netgrif/workflow/orgstructure/web/GroupController.java
+++ src/main/java/com/netgrif/workflow/orgstructure/web/GroupController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/res/group")
+@RequestMapping("/api/group")
 public class GroupController {
 
     @Autowired
