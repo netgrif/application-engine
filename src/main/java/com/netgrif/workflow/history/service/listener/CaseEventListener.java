fb3e590baa79f821adb958b2402c7754bacc5bfa
Juraj Mažári <mazari@netgrif.com>
2018-07-24 14:24:26 +0200

    NAE-277 - Stale use case data - fix transactions

--- src/main/java/com/netgrif/workflow/history/service/listener/CaseEventListener.java
+++ src/main/java/com/netgrif/workflow/history/service/listener/CaseEventListener.java
@@ -1,7 +1,13 @@
 package com.netgrif.workflow.history.service.listener;
 
+import com.netgrif.workflow.event.events.usecase.CreateCaseEvent;
+import com.netgrif.workflow.event.events.usecase.DeleteCaseEvent;
+import com.netgrif.workflow.event.events.usecase.SaveCaseDataEvent;
+import com.netgrif.workflow.event.events.usecase.UpdateMarkingEvent;
+import com.netgrif.workflow.history.domain.CaseEventLog;
 import com.netgrif.workflow.history.domain.repository.EventLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,32 +15,32 @@ public class CaseEventListener {
 
     @Autowired
     private EventLogRepository repository;
-//
-//    @EventListener
-//    public void onCreateCaseEvent(CreateCaseEvent event) {
-//        CaseEventLog log = new CaseEventLog(event.getCase());
-//        log.setMessage(event.getMessage());
-//        repository.save(log);
-//    }
-//
-//    @EventListener
-//    public void onDeleteCaseEvent(DeleteCaseEvent event) {
-//        CaseEventLog log = new CaseEventLog(event.getCase());
-//        log.setMessage(event.getMessage());
-//        repository.save(log);
-//    }
-//
-//    @EventListener
-//    public void onSaveCaseDataEvent(SaveCaseDataEvent event) {
-//        CaseEventLog log = new CaseEventLog(event.getCase());
-//        log.setMessage(event.getMessage());
-//        repository.save(log);
-//    }
-//
-//    @EventListener
-//    public void onUpdateMarkingEvent(UpdateMarkingEvent event) {
-//        CaseEventLog log = new CaseEventLog(event.getCase());
-//        log.setMessage(event.getMessage());
-//        repository.save(log);
-//    }
+
+    @EventListener
+    public void onCreateCaseEvent(CreateCaseEvent event) {
+        CaseEventLog log = new CaseEventLog(event.getCase());
+        log.setMessage(event.getMessage());
+        repository.save(log);
+    }
+
+    @EventListener
+    public void onDeleteCaseEvent(DeleteCaseEvent event) {
+        CaseEventLog log = new CaseEventLog(event.getCase());
+        log.setMessage(event.getMessage());
+        repository.save(log);
+    }
+
+    @EventListener
+    public void onSaveCaseDataEvent(SaveCaseDataEvent event) {
+        CaseEventLog log = new CaseEventLog(event.getCase());
+        log.setMessage(event.getMessage());
+        repository.save(log);
+    }
+
+    @EventListener
+    public void onUpdateMarkingEvent(UpdateMarkingEvent event) {
+        CaseEventLog log = new CaseEventLog(event.getCase());
+        log.setMessage(event.getMessage());
+        repository.save(log);
+    }
 }
