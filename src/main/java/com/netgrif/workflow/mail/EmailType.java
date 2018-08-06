1ddc6fe7c837b76490c12927c62ea207fe3368fa
Martin Makáň <makan@netgrif.com>
2018-08-06 23:54:17 +0200

    New email type for Premium

--- src/main/java/com/netgrif/workflow/mail/EmailType.java
+++ src/main/java/com/netgrif/workflow/mail/EmailType.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum EmailType {
     REGISTRATION ("registration.html", "Registration invite"),
     DRAFT("draft.html", "Návrh na uzavretie poistnej zmluvy"),
+    INSURANCE("insurance.html", "Poistka k poistnej zmluve"),
     PASSWORD_RESET("password-reset.html", "Reset password");
 
     @Getter
