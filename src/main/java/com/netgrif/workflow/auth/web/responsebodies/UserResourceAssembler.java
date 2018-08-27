d738dc68c1af4e5efc6bb8d5511f98a323467008
Juraj Mažári <mazari@netgrif.com>
2018-08-27 21:25:19 +0200

    UN-71 - User console - change endpoint response to page

---
+++ src/main/java/com/netgrif/workflow/auth/web/responsebodies/UserResourceAssembler.java
@@ -0,0 +1,24 @@
+package com.netgrif.workflow.auth.web.responsebodies;
+
+import com.netgrif.workflow.auth.domain.User;
+import org.springframework.hateoas.ResourceAssembler;
+
+import java.util.Locale;
+
+public class UserResourceAssembler implements ResourceAssembler<User, UserResource> {
+
+    private Locale locale;
+    private String selfRel;
+    private boolean small;
+
+    public UserResourceAssembler(Locale locale, boolean small, String selfRel) {
+        this.locale = locale;
+        this.selfRel = selfRel;
+        this.small = small;
+    }
+
+    @Override
+    public UserResource toResource(User entity) {
+        return new UserResource(entity, selfRel, locale, small);
+    }
+}
