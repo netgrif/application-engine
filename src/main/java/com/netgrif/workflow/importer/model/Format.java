086fef6b1f8dc460ccf634bfa2d7300a2c442cd7
Juraj Mažári <mazari@netgrif.com>
2018-11-28 23:31:54 +0100

    NAE-328 - Add number format feature - fix missing classes

---
+++ src/main/java/com/netgrif/workflow/importer/model/Format.java
@@ -0,0 +1,62 @@
+
+package com.netgrif.workflow.importer.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;choice&gt;
+ *         &lt;element ref="{}currency"/&gt;
+ *       &lt;/choice&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "currency"
+})
+@XmlRootElement(name = "format")
+public class Format {
+
+    protected Currency currency;
+
+    /**
+     * Gets the value of the currency property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Currency }
+     *     
+     */
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    /**
+     * Sets the value of the currency property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Currency }
+     *     
+     */
+    public void setCurrency(Currency value) {
+        this.currency = value;
+    }
+
+}
