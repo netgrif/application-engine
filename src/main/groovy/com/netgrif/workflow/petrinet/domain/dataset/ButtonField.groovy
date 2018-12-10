b537ab314e5cae170069c2a41ad92e72bbbaae47
Juraj Mažári <mazari@netgrif.com>
2018-12-10 14:25:55 +0100

    NAE-344 - Button data field - backend - add default value - add test action

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/ButtonField.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/ButtonField.groovy
@@ -3,7 +3,12 @@ package com.netgrif.workflow.petrinet.domain.dataset
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-class ButtonField extends Field {
+class ButtonField extends Field<String> {
+
+    ButtonField() {
+        super()
+        value = ""
+    }
 
     @Override
     FieldType getType() {
