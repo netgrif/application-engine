a62286a5ef6c355c1365d9f1837bb7c3f31cd0d0
Juraj Mažári <mazari@netgrif.com>
2018-12-12 16:26:11 +0100

    NAE-311 - Added filters are not persistent in tab area. - fix id and duplicate

--- src/main/java/com/netgrif/workflow/settings/domain/Preferences.java
+++ src/main/java/com/netgrif/workflow/settings/domain/Preferences.java
@@ -1,9 +1,7 @@
 package com.netgrif.workflow.settings.domain;
 
 import lombok.Data;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -25,9 +23,6 @@ import java.util.Map;
 public class Preferences implements Serializable {
 
     @Id
-    private ObjectId id;
-
-    @Indexed
     private Long userId;
 
     private String locale;
