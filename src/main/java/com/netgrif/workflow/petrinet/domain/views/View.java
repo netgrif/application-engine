7c998b13de39107a9cfdb90954c088548f4bd21c
Juraj Mažári <mazari@netgrif.com>
2019-02-06 16:06:28 +0100

    NAE-358 - Enumeration and multichoice list view - add list view - refactor field template naming - add enum list template

--- src/main/java/com/netgrif/workflow/petrinet/domain/views/View.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/views/View.java
@@ -1,4 +1,13 @@
 package com.netgrif.workflow.petrinet.domain.views;
 
-public abstract class View {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class View {
+
+    private String value;
 }
