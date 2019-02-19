7bcb4527292b7aa59c799747b6ce8ba47b41634a
Juraj Mažári <mazari@netgrif.com>
2019-02-19 18:39:54 +0100

    - fix missing files

---
+++ src/main/java/com/netgrif/workflow/importer/model/FieldView.java
@@ -0,0 +1,128 @@
+
+package com.netgrif.workflow.importer.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for fieldView complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="fieldView"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;choice&gt;
+ *         &lt;element name="area" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="image" type="{}booleanImageView"/&gt;
+ *         &lt;element name="list"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;union&gt;
+ *               &lt;simpleType&gt;
+ *                 &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *                   &lt;length value="0"/&gt;
+ *                 &lt;/restriction&gt;
+ *               &lt;/simpleType&gt;
+ *               &lt;simpleType&gt;
+ *                 &lt;restriction base="{http://www.w3.org/2001/XMLSchema}int"&gt;
+ *                 &lt;/restriction&gt;
+ *               &lt;/simpleType&gt;
+ *             &lt;/union&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *       &lt;/choice&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "fieldView", propOrder = {
+    "area",
+    "image",
+    "list"
+})
+public class FieldView {
+
+    protected String area;
+    protected BooleanImageView image;
+    protected String list;
+
+    /**
+     * Gets the value of the area property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * Sets the value of the area property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setArea(String value) {
+        this.area = value;
+    }
+
+    /**
+     * Gets the value of the image property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BooleanImageView }
+     *     
+     */
+    public BooleanImageView getImage() {
+        return image;
+    }
+
+    /**
+     * Sets the value of the image property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BooleanImageView }
+     *     
+     */
+    public void setImage(BooleanImageView value) {
+        this.image = value;
+    }
+
+    /**
+     * Gets the value of the list property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getList() {
+        return list;
+    }
+
+    /**
+     * Sets the value of the list property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setList(String value) {
+        this.list = value;
+    }
+
+}
