e5db4def5978480dfdadab5b052f0b3cacdb788a
Milan Mladoniczky <mladoniczky@netgrif.com>
2019-03-28 21:07:41 +0100

    NAE-295 - Migration from backend static - also NAE-293, NAE-292 - remove forward mappings for frontend index - remove frontend uris from security permit-all patterns - add cors configuration - config http session to use x-auth-token header - remove frontend from static - add surefire test reports

--- src/main/java/com/netgrif/workflow/configuration/ForwardConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/ForwardConfiguration.java
@@ -5,39 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.NoHandlerFoundException;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @ControllerAdvice
 public class ForwardConfiguration {
 
-    private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
-
-    @RequestMapping(value = "/{path:[^api]*}")
-    public String redirect(HttpServletRequest request) {
-        log.info("Forwarding to root for request URI [" + request.getRequestURI() + "]");
-        return "forward:/";
-    }
-
-    @RequestMapping(value = {"/signup/{token}", "/recover/{token}"})
-    public String redirectWithToken(@PathVariable("token") String token, HttpServletRequest request) {
-        log.info("Forwarding to root for URI [ " + request.getRequestURI() + " ] with token " + token);
-        return "forward:/";
-    }
-
-    @RequestMapping("/**/{path:[^.]+}")
-    public String forward() {
-        return "forward:/";
-    }
+    private static final Logger log = LoggerFactory.getLogger(ForwardConfiguration.class);
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public String handle404Error(){
-        log.info("No requested mapping found. Forwarding to index");
-        return "forward:/";
+    public String handle404Error() {
+        log.info("No requested mapping found. Forwarding to api index");
+        return "forward:/api";
     }
 
 }
