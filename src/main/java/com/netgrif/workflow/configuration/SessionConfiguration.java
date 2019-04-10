949dcbf794335ee86c377b2b669e4352b4efc047
Juraj Mažári <mazari@netgrif.com>
2019-04-10 23:45:43 +0200

    NAE-373 - Elasticsearch - add Elasticsearch configuration - fix logback config - fix loggers - fix bean overriding

--- src/main/java/com/netgrif/workflow/configuration/SessionConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/SessionConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
-import org.springframework.session.web.http.HttpSessionStrategy;
 
 @EnableRedisHttpSession
 @Configuration
@@ -15,9 +13,4 @@ public class SessionConfiguration {
     public JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
     }
-
-//    @Bean
-//    public HttpSessionStrategy httpSessionStrategy(){
-//            return new HeaderHttpSessionStrategy();
-//    }
 }
