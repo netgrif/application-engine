9032770afa9ba6eae64802a619753fd9fbd5ea75
Juraj Mažári <mazari@netgrif.com>
2019-04-12 13:51:55 +0200

    NAE-373 - Elasticsearch - add demo runner - update ElasticCase class

--- src/main/java/com/netgrif/workflow/workflow/domain/elastic/ElasticCase.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/elastic/ElasticCase.java
@@ -1,6 +1,7 @@
 package com.netgrif.workflow.workflow.domain.elastic;
 
 import com.netgrif.workflow.workflow.domain.Case;
+import com.netgrif.workflow.workflow.domain.TaskPair;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,23 +26,45 @@ public class ElasticCase {
     private String id;
 
     @Field(type = Keyword)
+    private String visualId;
+
+    @Field(type = Keyword)
     private String processIdentifier;
 
     private String title;
 
+    private LocalDateTime creationDate;
+
     private Long author;
 
+    private String authorName;
+
+    @Field(type = Keyword)
+    private String authorEmail;
+
     private Map<String, String> dataSet;
+
+    @Field(type = Keyword)
+    private Set<String> taskIds;
+
+    @Field(type = Keyword)
+    private Set<String> taskMongoIds;
 
     @Field(type = Keyword)
     private Set<String> enabledRoles;
 
     public ElasticCase(Case useCase) {
         id = useCase.getStringId();
-        title = useCase.getTitle();
-        dataSet = useCase.getDataSet().entrySet().stream().limit(100).collect(Collectors.toMap(Map.Entry::getKey, f -> "" + f.getValue().getValue()));
+        visualId = useCase.getVisualId();
         processIdentifier = useCase.getProcessIdentifier();
-        enabledRoles = useCase.getEnabledRoles();
+        title = useCase.getTitle();
+        creationDate = useCase.getCreationDate();
         author = useCase.getAuthor().getId();
+        authorName = useCase.getAuthor().getFullName();
+        authorEmail = useCase.getAuthor().getEmail();
+        dataSet = useCase.getDataSet().entrySet().stream().limit(1000).collect(Collectors.toMap(Map.Entry::getKey, f -> "" + f.getValue().getValue()));
+        taskIds = useCase.getTasks().stream().map(TaskPair::getTransition).collect(Collectors.toSet());
+        taskMongoIds = useCase.getTasks().stream().map(TaskPair::getTask).collect(Collectors.toSet());
+        enabledRoles = useCase.getEnabledRoles();
     }
 }
