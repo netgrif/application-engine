90bcc49db580d9991e80f1e2038677f56dad6abc
Juraj Mažári <mazari@netgrif.com>
2019-04-26 16:03:51 +0200

    NAE-390 - Elastic search services - add search function - add count function

--- src/main/java/com/netgrif/workflow/elastic/service/ElasticCaseService.java
+++ src/main/java/com/netgrif/workflow/elastic/service/ElasticCaseService.java
@@ -1,11 +1,19 @@
 package com.netgrif.workflow.elastic.service;
 
+import com.netgrif.workflow.auth.domain.LoggedUser;
 import com.netgrif.workflow.elastic.domain.ElasticCase;
 import com.netgrif.workflow.elastic.domain.ElasticCaseRepository;
+import com.netgrif.workflow.elastic.web.ElasticSearchRequest;
+import com.netgrif.workflow.utils.FullPageRequest;
 import com.netgrif.workflow.workflow.domain.Case;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +24,9 @@ public class ElasticCaseService implements IElasticCaseService {
 
     @Autowired
     private ElasticCaseRepository repository;
+
+    @Autowired
+    private ElasticsearchTemplate template;
 
     @Async
     @Override
@@ -34,5 +45,33 @@ public class ElasticCaseService implements IElasticCaseService {
         repository.save(elasticCase);
 
         log.info("[" + useCase.getStringId() + "]: Case \"" + useCase.getTitle() + "\" indexed");
+    }
+
+    @Override
+    public Page<ElasticCase> search(ElasticSearchRequest request, LoggedUser user, Pageable pageable) {
+        if (request == null) {
+            throw new IllegalArgumentException("Request can not be null!");
+        }
+
+        SearchQuery query = buildQuery(request, user, pageable);
+
+        return template.queryForPage(query, ElasticCase.class);
+    }
+
+    @Override
+    public long count(ElasticSearchRequest request, LoggedUser user) {
+        if (request == null) {
+            throw new IllegalArgumentException("Request can not be null!");
+        }
+
+        SearchQuery query = buildQuery(request, user, new FullPageRequest());
+
+        return template.count(query);
+    }
+
+    private SearchQuery buildQuery(ElasticSearchRequest request, LoggedUser user, Pageable pageable) {
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+
+        return builder.withPageable(pageable).build();
     }
 }
--- src/main/java/com/netgrif/workflow/elastic/service/IElasticCaseService.java
+++ src/main/java/com/netgrif/workflow/elastic/service/IElasticCaseService.java
@@ -1,6 +1,11 @@
 package com.netgrif.workflow.elastic.service;
 
+import com.netgrif.workflow.auth.domain.LoggedUser;
+import com.netgrif.workflow.elastic.domain.ElasticCase;
+import com.netgrif.workflow.elastic.web.ElasticSearchRequest;
 import com.netgrif.workflow.workflow.domain.Case;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
 
 public interface IElasticCaseService {
@@ -9,4 +14,8 @@ public interface IElasticCaseService {
     void index(Case useCase);
 
     void indexNow(Case useCase);
+
+    Page<ElasticCase> search(ElasticSearchRequest request, LoggedUser user, Pageable pageable);
+
+    long count(ElasticSearchRequest request, LoggedUser user);
 }
