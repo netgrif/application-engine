0c38394cda7d7c31881119d86167769dfd5432d8
Jakub Kovář <kovar@netgrif.com>
2019-05-03 11:39:01 +0200

    NAE-391 - CORS on Spring Boot 2 - fix x-auth-token not sent

--- src/main/java/com/netgrif/workflow/configuration/SessionConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/SessionConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
-import org.springframework.session.web.http.HttpSessionStrategy;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @EnableRedisHttpSession
 @Configuration
@@ -17,7 +17,7 @@ public class SessionConfiguration {
     }
 
     @Bean
-    public HttpSessionStrategy httpSessionStrategy() {
-        return new HeaderHttpSessionStrategy();
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        return HeaderHttpSessionIdResolver.xAuthToken();
     }
 }
