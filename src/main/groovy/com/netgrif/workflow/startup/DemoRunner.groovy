79fabc06706105928080739e93ce4f72136d3218
Juraj Mažári <mazari@netgrif.com>
2019-07-17 10:19:46 +0200

    NAE-390 - Elastic search services - update demo runner

--- src/main/groovy/com/netgrif/workflow/startup/DemoRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/DemoRunner.groovy
@@ -3,7 +3,6 @@ package com.netgrif.workflow.startup
 import com.netgrif.workflow.elastic.domain.ElasticCaseRepository
 import com.netgrif.workflow.elastic.domain.ElasticTaskRepository
 import com.netgrif.workflow.elastic.service.IElasticCaseService
-import com.netgrif.workflow.petrinet.domain.PetriNet
 import com.netgrif.workflow.workflow.domain.repositories.CaseRepository
 import com.netgrif.workflow.workflow.domain.repositories.TaskRepository
 import org.slf4j.Logger
@@ -38,57 +37,5 @@ class DemoRunner extends AbstractOrderedCommandLineRunner {
 
     @Override
     void run(String... args) throws Exception {
-//        log.info("Elastic")
-//
-        Optional<PetriNet> netOptional = helper.createNet("posudky.xml", "elk", "elastic", "ELK", "major")
-        assert netOptional.isPresent()
-        PetriNet net = netOptional.get()
-
-        5.times { i ->
-            log.info "Processing page ${i+1}/1000"
-
-            def cases = []
-            1.times { j ->
-                cases << helper.createCase("Case_${i}_${j}", net)
-            }
-            caseRepository.saveAll cases
-        }
-
-
-//        Predicate filter = QCase.case$.title.isNotNull()
-//        long caseCount = caseRepository.count(filter)
-//        long numOfPages = ((caseCount/100.0)+1) as Long
-//        numOfPages.times { page->
-//            log.info("Page ${page +1} / $numOfPages")
-//            Page<Case> cases = caseRepository.findAll(filter, new PageRequest(page, 100))
-//            cases.getContent().eachWithIndex { useCase, index ->
-//                log.info("Case $index")
-//                def elastiCases = []
-//                1000.times {
-//                    ElasticCase elasticCase = new ElasticCase(useCase)
-//                    elasticCase.mongoId = new ObjectId() as String
-//                    elastiCases << elasticCase
-//                }
-//                repository.saveAll elastiCases
-//            }
-//        }
-
-//        Predicate filter = QTask.task.title.isNotNull()
-//            long taskCount = taskRepository.count(filter)
-//            long numOfPages = ((taskCount / 100.0) + 1) as long
-//            numOfPages.times { page ->
-//                log.info("Page $page / $numOfPages")
-//                Page<Task> tasks = taskRepository.findAll(filter, new PageRequest(page, 100))
-//                tasks.eachWithIndex { Task task, int index ->
-//                    log.info("Case $index")
-//                    def elasticTasks = []
-//                    80000.times {
-//                        ElasticTask elasticTask = new ElasticTask(task)
-//                        elasticTask.id = new ObjectId() as String
-//                        elasticTasks << elasticTask
-//                    }
-//                    elasticTaskRepository.saveAll elasticTasks
-//                }
-//        }
     }
 }
