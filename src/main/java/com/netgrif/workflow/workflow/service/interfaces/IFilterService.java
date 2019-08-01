a47f4e8750efaa059ce49e5bc2e0c91c9f5f9e4f
Jakub Kovář <kovar@netgrif.com>
2019-08-01 14:21:20 +0200

    NAE-392 - Secure endpoints - resolution of merge conflicts

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IFilterService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IFilterService.java
@@ -2,21 +2,19 @@ package com.netgrif.workflow.workflow.service.interfaces;
 
 
 import com.netgrif.workflow.auth.domain.LoggedUser;
+import com.netgrif.workflow.auth.domain.throwable.UnauthorisedRequestException;
 import com.netgrif.workflow.workflow.domain.Filter;
 import com.netgrif.workflow.workflow.web.requestbodies.CreateFilterBody;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Map;
 
 public interface IFilterService {
 
-    boolean deleteFilter(String filterId, LoggedUser user);
+    boolean deleteFilter(String filterId, LoggedUser user) throws UnauthorisedRequestException;
 
     Filter saveFilter(CreateFilterBody newFilterBody, LoggedUser user);
 
     Page<Filter> search(Map<String, Object> request, Pageable pageable, LoggedUser user);
-
-	Filter findOne(String filterId);
 }
