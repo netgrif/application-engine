d1c7c28a8ffce6d75fccaf3ac550ae8468cc291e
Juraj Mažári <mazari@netgrif.com>
2019-08-12 15:43:19 +0200

    NAE-422 - Optimise elastic indexing - maybe fix concurrency

--- src/main/java/com/netgrif/workflow/elastic/service/executors/Executor.java
+++ src/main/java/com/netgrif/workflow/elastic/service/executors/Executor.java
@@ -1,15 +1,19 @@
 package com.netgrif.workflow.elastic.service.executors;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Component
 public class Executor {
 
     private Map<String, ExecutorService> map;
 
-    public Executor(long maxSize) {
+    public Executor(@Value("${spring.data.elasticsearch.executors:10000}") long maxSize) {
         this.map = Collections.synchronizedMap(new MaxSizeHashMap(maxSize));
     }
 
