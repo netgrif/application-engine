94dc5f1ada6c1ba82b61c01470934b476920f5c7
Juraj Mažári <mazari@netgrif.com>
2019-08-14 13:42:08 +0200

    NAE-419 - Optimise elastic search - auditing

--- src/main/java/com/netgrif/workflow/elastic/service/ElasticCaseService.java
+++ src/main/java/com/netgrif/workflow/elastic/service/ElasticCaseService.java
@@ -17,7 +17,6 @@ import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
