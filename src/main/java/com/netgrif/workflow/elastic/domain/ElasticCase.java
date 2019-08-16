1bb14e52d1e6809a402094ec244ce65400ddeca4
Juraj Mažári <mazari@netgrif.com>
2019-08-16 16:10:58 +0200

    NAE-182 - DateTime field - frontend - fix date indexing

--- src/main/java/com/netgrif/workflow/elastic/domain/ElasticCase.java
+++ src/main/java/com/netgrif/workflow/elastic/domain/ElasticCase.java
@@ -19,6 +19,7 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -136,6 +137,13 @@ public class ElasticCase {
             return Optional.of(new DataField(String.valueOf(date), date.format(DateTimeFormatter.BASIC_ISO_DATE)));
         } else if (dataField.getValue() instanceof LocalDateTime) {
             LocalDateTime date = (LocalDateTime) dataField.getValue();
+            if (date == null)
+                return Optional.empty();
+            return Optional.of(new DataField(String.valueOf(date), date.format(DateTimeFormatter.BASIC_ISO_DATE)));
+        } else if (dataField.getValue() instanceof Date) {
+            LocalDateTime date = ((Date)dataField.getValue()).toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
             if (date == null)
                 return Optional.empty();
             return Optional.of(new DataField(String.valueOf(date), date.format(DateTimeFormatter.BASIC_ISO_DATE)));
