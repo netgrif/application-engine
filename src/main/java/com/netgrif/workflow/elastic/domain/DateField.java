a66ae08c5f38d229a26ce450d7f0652178787462
Juraj Mažári <mazari@netgrif.com>
2019-08-16 09:24:51 +0200

    NAE-419 - Optimise elastic search - add data fields

---
+++ src/main/java/com/netgrif/workflow/elastic/domain/DateField.java
@@ -0,0 +1,20 @@
+package com.netgrif.workflow.elastic.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class DateField extends DataField {
+
+    private long timestamp;
+
+    public DateField(String value, long timestamp) {
+        super(value);
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/com/netgrif/workflow/elastic/domain/NumberField.java
@@ -0,0 +1,4 @@
+package com.netgrif.workflow.elastic.domain;
+
+public class NumberField {
+}
