1d982d152c3a6ab933294ed18beef377062c05d2
Juraj Mažári <mazari@netgrif.com>
2019-11-06 09:20:52 +0100

    NAE-440 - Run Groovy script
    - add AdminActionEvent and AdminActionEventLog
    - publish event on run()
    - save eventLog on run()

--- src/main/java/com/netgrif/workflow/admin/service/interfaces/IAdminService.java
+++ src/main/java/com/netgrif/workflow/admin/service/interfaces/IAdminService.java
@@ -1,9 +1,9 @@
 package com.netgrif.workflow.admin.service.interfaces;
 
+import com.netgrif.workflow.auth.domain.LoggedUser;
 import com.netgrif.workflow.workflow.web.responsebodies.MessageResource;
 
 public interface IAdminService {
 
-   MessageResource run(String code);
-
+    MessageResource run(String code, LoggedUser user);
 }
