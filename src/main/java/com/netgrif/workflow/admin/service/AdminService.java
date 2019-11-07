21cf091a1bd372d5541adf74c16e14df93a7f1ba
Juraj Mažári <mazari@netgrif.com>
2019-11-07 18:23:17 +0100

    NAE-440 - Run Groovy script
    - add admin exception
    - refactor resource creation

--- src/main/java/com/netgrif/workflow/admin/service/AdminService.java
+++ src/main/java/com/netgrif/workflow/admin/service/AdminService.java
@@ -4,15 +4,11 @@ import com.netgrif.workflow.admin.AdminConsoleRunner;
 import com.netgrif.workflow.admin.service.interfaces.IAdminService;
 import com.netgrif.workflow.auth.domain.LoggedUser;
 import com.netgrif.workflow.event.events.user.AdminActionEvent;
-import com.netgrif.workflow.workflow.web.responsebodies.MessageResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-
-import static com.netgrif.workflow.workflow.web.responsebodies.MessageResource.successMessage;
-import static com.netgrif.workflow.workflow.web.responsebodies.MessageResource.errorMessage;
 
 @Service
 public class AdminService implements IAdminService {
@@ -29,14 +25,13 @@ public class AdminService implements IAdminService {
      * @param code maximal length is 65535 bytes
      */
     @Override
-    public MessageResource run(String code, LoggedUser user) {
+    public String run(String code, LoggedUser user) throws AdminActionException {
         try {
             publisher.publishEvent(new AdminActionEvent(user, code));
-            String result = actionsRunner.run(code);
-            return successMessage("OK", result);
+            return actionsRunner.run(code);
         } catch (Exception e) {
             log.error("Admin console ERROR: ", e);
-            return errorMessage("ERROR", e.toString());
+            throw new AdminActionException(e);
         }
     }
 }
