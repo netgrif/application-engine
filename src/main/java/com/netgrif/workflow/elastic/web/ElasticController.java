8c004e5a87196106e4fb6789bae8a3c8deb5ff07
Juraj Mažári <mazari@netgrif.com>
2019-11-26 12:54:43 +0100

    NAE-480 - Task Reindex Task
    - update ES Task service
    - refactor reindexing task
    - refactor reindexing endpoint

--- src/main/java/com/netgrif/workflow/elastic/web/ElasticController.java
+++ src/main/java/com/netgrif/workflow/elastic/web/ElasticController.java
@@ -1,16 +1,14 @@
 package com.netgrif.workflow.elastic.web;
 
 import com.netgrif.workflow.auth.domain.LoggedUser;
-import com.netgrif.workflow.elastic.domain.ElasticCase;
-import com.netgrif.workflow.elastic.service.interfaces.IElasticCaseService;
-import com.netgrif.workflow.workflow.domain.Case;
+import com.netgrif.workflow.elastic.service.ReindexingTask;
+import com.netgrif.workflow.workflow.service.CaseSearchService;
 import com.netgrif.workflow.workflow.service.interfaces.IWorkflowService;
 import com.netgrif.workflow.workflow.web.responsebodies.MessageResource;
+import com.querydsl.core.types.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +29,10 @@ public class ElasticController {
     private IWorkflowService workflowService;
 
     @Autowired
-    private IElasticCaseService elasticCaseService;
+    private CaseSearchService searchService;
+
+    @Autowired
+    private ReindexingTask reindexingTask;
 
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "/reindex", method = RequestMethod.POST)
@@ -48,8 +49,8 @@ public class ElasticController {
 
                 for (int page = 0; page < numOfPages; page++) {
                     log.info("Indexing page " + (page + 1));
-                    Page<Case> cases = workflowService.search(searchBody, PageRequest.of(page, 100), user, locale);
-                    cases.forEach(aCase -> elasticCaseService.index(new ElasticCase(aCase)));
+                    Predicate predicate = searchService.buildQuery(searchBody, user, locale);
+                    reindexingTask.forceReindexPage(predicate, page, numOfPages);
                 }
             }
 
