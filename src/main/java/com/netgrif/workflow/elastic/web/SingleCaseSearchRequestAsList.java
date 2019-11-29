40a970727a6811e6cbffeb4a14b0801e3451d652
Jakub Kovář <kovar@netgrif.com>
2019-11-29 11:15:21 +0100

    NAE-472 - Filters don't merge correctly - generic implementation of "SingleItemAsList" deserialization for webservice endpoints

--- src/main/java/com/netgrif/workflow/elastic/web/SingleCaseSearchRequestAsList.java
+++ src/main/java/com/netgrif/workflow/elastic/web/SingleCaseSearchRequestAsList.java
@@ -1,74 +1,8 @@
 package com.netgrif.workflow.elastic.web;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import lombok.Getter;
+import com.netgrif.workflow.utils.SingleItemAsList;
+import com.netgrif.workflow.utils.SingleItemAsListDeserializer;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter
-@JsonDeserialize(using = SingleCaseSearchRequestAsList.SingleItemAsListDeserializer.class, contentAs = CaseSearchRequest.class)
-public class SingleCaseSearchRequestAsList {
-
-    private List<CaseSearchRequest> list;
-
-    public SingleCaseSearchRequestAsList() {
-        list = new ArrayList<>();
-    }
-
-    public SingleCaseSearchRequestAsList(CaseSearchRequest item) {
-        this();
-        this.list.add(item);
-    }
-
-    public SingleCaseSearchRequestAsList(List<CaseSearchRequest> requests) {
-        list = requests;
-    }
-
-
-    public static class SingleItemAsListDeserializer extends StdDeserializer<SingleCaseSearchRequestAsList> implements ContextualDeserializer {
-
-        protected SingleItemAsListDeserializer() {
-            this(null);
-        }
-
-        protected SingleItemAsListDeserializer(Class<?> vc) {
-            super(vc);
-        }
-
-        @Override
-        public SingleCaseSearchRequestAsList deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, IllegalArgumentException {
-            SingleCaseSearchRequestAsList wrapper;
-
-            ObjectMapper innerDeserializer = new ObjectMapper();
-            JsonNode node = innerDeserializer.readTree(jsonParser);
-            try {
-                CaseSearchRequest request = innerDeserializer.convertValue(node, new TypeReference<CaseSearchRequest>() {});
-                wrapper = new SingleCaseSearchRequestAsList(request);
-            } catch (IllegalArgumentException singleItemException) {
-                try {
-                    List<CaseSearchRequest> requests = innerDeserializer.convertValue(node, new TypeReference<List<CaseSearchRequest>>() {});
-                    wrapper = new SingleCaseSearchRequestAsList(requests);
-                } catch (IllegalArgumentException arrayException) {
-                    if(node.isArray())
-                        throw new IllegalArgumentException("Single item as list deserialization failed. List deserialization exception: " + arrayException.getMessage(), arrayException);
-                    else
-                        throw new IllegalArgumentException("Single item as list deserialization failed. Single item deserialization exception: " + singleItemException.getMessage(), singleItemException);
-                }
-            }
-
-            return wrapper;
-        }
-
-        @Override
-        public JsonDeserializer<?> createContextual(DeserializationContext deserializationContext, BeanProperty beanProperty) throws JsonMappingException {
-            return null;
-        }
-    }
-}
+@JsonDeserialize(using = SingleItemAsListDeserializer.class, contentAs = CaseSearchRequest.class)
+public class SingleCaseSearchRequestAsList extends SingleItemAsList<CaseSearchRequest> {}
