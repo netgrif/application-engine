29eadfd6b2450de068a8048ce077638e62db086d
Juraj Mažári <mazari@netgrif.com>
2019-12-03 16:32:11 +0100

    NAE-335 - Variable arc definition rework
    - add reference class
    - rework references

--- src/main/java/com/netgrif/workflow/petrinet/domain/arcs/reference/Reference.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/arcs/reference/Reference.java
@@ -1,6 +1,10 @@
 package com.netgrif.workflow.petrinet.domain.arcs.reference;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Transient;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +14,12 @@ public class Reference {
     private String reference;
 
     private Type type;
+
+    @Transient
+    private Referencable referencable;
+
+    public Reference(String reference, Type type) {
+        this.reference = reference;
+        this.type = type;
+    }
 }
