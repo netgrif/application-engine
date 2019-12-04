c5c5960288f75d2b1b8147629568a4b481b28e3c
Martin Miklovič <miklovic@netgrif.com>
2019-12-04 23:07:16 +0100

    NAE-301 - Add key param to data-fields of type enumeration and multi-choice - Implementation map in enumeration and multi-choice fields

--- src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedEnumerationField.java
+++ src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedEnumerationField.java
@@ -4,24 +4,21 @@ import com.netgrif.workflow.petrinet.domain.I18nString;
 import com.netgrif.workflow.petrinet.domain.dataset.EnumerationField;
 import lombok.Data;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 
 @Data
 public class LocalisedEnumerationField extends LocalisedField {
 
-    private List<String> choices;
+    private Map<String, String> choices;
 
     private Object defaultValue;
 
     public LocalisedEnumerationField(EnumerationField field, Locale locale) {
         super(field, locale);
-        this.choices = new LinkedList<>();
-        Set<I18nString> choices = field.getChoices();
-        for (I18nString choice : choices) {
-            this.choices.add(choice.getTranslation(locale));
+        this.choices = new LinkedHashMap<>();
+        Map<String, I18nString> choices = field.getChoices();
+        for (Map.Entry<String, I18nString> choice : choices.entrySet()) {
+            this.choices.put(choice.getKey(), choice.getValue().getTranslation(locale));
         }
 
         I18nString defaultI18n = field.getDefaultValue();
