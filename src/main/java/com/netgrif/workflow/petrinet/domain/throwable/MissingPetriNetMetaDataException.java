10da4d9fe30a843e5eab97722619c38e29114ea4
Daniel Vaník <vanik@netgrif.com>
2019-12-11 09:44:49 +0100

    NAE-212 - Model upload refactor - change net upload - move metadata(id, title, initials) to net

---
+++ src/main/java/com/netgrif/workflow/petrinet/domain/throwable/MissingPetriNetMetaDataException.java
@@ -0,0 +1,8 @@
+package com.netgrif.workflow.petrinet.domain.throwable;
+
+public class MissingPetriNetMetaDataException extends Exception {
+
+    public MissingPetriNetMetaDataException(String message) {
+        super(message);
+    }
+}
