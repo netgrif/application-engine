2d5013ba02ee27da22bfcd1e9ac6e7cec632b680
Jakub Kovář <kovar@netgrif.com>
2019-12-21 10:56:46 +0100

    NAE-500 - Make sure new filters can be created if they contain merged filters - simplification of the implementation after further insight into the problem - reverted a lot of the code to it's original state

--- src/main/java/com/netgrif/workflow/workflow/web/requestbodies/CreateCaseFilterBody.java
+++
@@ -1,9 +0,0 @@
-package com.netgrif.workflow.workflow.web.requestbodies;
-
-import com.netgrif.workflow.elastic.web.requestbodies.singleaslist.SingleCaseSearchRequestAsList;
-
-public class CreateCaseFilterBody extends CreateFilterBody<SingleCaseSearchRequestAsList> {
-    public CreateCaseFilterBody(String title, int visibility, String description, String type, SingleCaseSearchRequestAsList query) {
-        super(title, visibility, description, type, query);
-    }
-}
