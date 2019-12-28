f4c99d466e9622d697ca80ffc50dfca5fb30b2c7
Jakub Kovář <kovar@netgrif.com>
2019-12-28 12:10:37 +0100

    NAE-412 - Process and Case loading refactor - moved elastic mapping classes into their own package

---
+++ src/main/java/com/netgrif/workflow/elastic/domain/mapping/DataField.java
@@ -0,0 +1,13 @@
+package com.netgrif.workflow.elastic.domain.mapping;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataField {
+
+    public String value;
+}
