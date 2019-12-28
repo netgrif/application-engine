51bfb0d0320e07ef9dcff6a1d73db4e8ea012e7b
Jakub Kovář <kovar@netgrif.com>
2019-12-28 14:49:05 +0100

    NAE-412 - Process and Case loading refactor - first experiments with nested mapping

--- src/main/java/com/netgrif/workflow/elastic/domain/mapping/TextField.java
+++ src/main/java/com/netgrif/workflow/elastic/domain/mapping/TextField.java
@@ -3,13 +3,25 @@ package com.netgrif.workflow.elastic.domain.mapping;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Keyword;
+import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class TextField extends DataField {
 
+    @Field(type = Text)
+    public String searchable;
+
+    @Field(type = Keyword)
+    public String sortable;
+
     public TextField(String value) {
         super(value);
+        this.searchable = value;
+        this.sortable = value;
     }
 }
