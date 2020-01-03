ba07281b2ffb76e48f65c1bc51525d0a6d62a9e3
Jakub Kovář <kovar@netgrif.com>
2020-01-03 08:51:23 +0100

    NAE-412 - Process and Case loading refactor - changed the mapping runner to use json configuration instead of spring data annotations - json mapping is the same as in NAE-419 branch

--- src/main/groovy/com/netgrif/workflow/startup/ElasticsearchRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/ElasticsearchRunner.groovy
@@ -1,7 +1,6 @@
 package com.netgrif.workflow.startup
 
-import com.netgrif.workflow.elastic.domain.ElasticCase
-import com.netgrif.workflow.elastic.domain.ElasticTask
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -28,9 +27,146 @@ class ElasticsearchRunner extends AbstractOrderedCommandLineRunner {
 
     @Value('${spring.data.elasticsearch.index.case}')
     private String caseIndex
+    private static final String CASE_TYPE = "case"
+    private static final String CASE_MAPPING = '''
+    {
+       "properties":{
+          "author":{
+             "type":"long"
+          },
+          "authorEmail":{
+             "type":"keyword"
+          },
+          "authorName":{
+             "type":"text",
+             "fields":{
+                "keyword":{
+                   "type":"keyword",
+                   "ignore_above":256
+                }
+             }
+          },
+          "creationDate":{
+             "type":"long"
+          },
+          "creationDateSortable": {
+              "type": "long"
+          },
+          "enabledRoles":{
+             "type":"keyword"
+          },
+          "id":{
+             "type":"text",
+             "fields":{
+                "keyword":{
+                   "type":"keyword",
+                   "ignore_above":256
+                }
+             }
+          },
+          "processIdentifier":{
+             "type":"keyword"
+          },
+          "processId":{
+             "type":"keyword"
+          },
+          "stringId":{
+             "type":"keyword"
+          },
+          "taskIds":{
+             "type":"keyword"
+          },
+          "taskMongoIds":{
+             "type":"keyword"
+          },
+          "title":{
+             "type":"text",
+             "fields":{
+                "keyword":{
+                   "type":"keyword",
+                   "ignore_above":256
+                }
+             }
+          },
+          "titleSortable":{
+             "type":"keyword"
+          },
+          "visualId":{
+             "type":"keyword"
+          }
+       }
+    }
+    '''
 
     @Value('${spring.data.elasticsearch.index.task}')
     private String taskIndex
+    private static final String TASK_TYPE = "task"
+    private static final String TASK_MAPPING = '''
+    {
+        "properties": {
+            "assignPolicy": {
+                "type": "keyword"
+            },
+            "caseColor": {
+                "type": "keyword"
+            },
+            "caseId": {
+                "type": "keyword"
+            },
+            "caseTitle": {
+                "type": "text",
+                "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 256
+                    }
+                }
+            },
+            "caseTitleSortable": {
+                "type": "keyword"
+            },
+            "dataFocusPolicy": {
+                "type": "keyword"
+            },
+            "finishPolicy": {
+                "type": "keyword"
+            },
+            "icon": {
+                "type": "keyword"
+            },
+            "priority": {
+                "type": "long"
+            },
+            "processId": {
+                "type": "keyword"
+            },
+            "roles": {
+                "type": "keyword"
+            },
+            "stringId": {
+                "type": "keyword"
+            },
+            "title": {
+                "type": "text",
+                "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 256
+                    }
+                }
+            },
+            "titleSortable": {
+                "type": "keyword"
+            },
+            "transactionId": {
+                "type": "keyword"
+            },
+            "transitionId": {
+                "type": "keyword"
+            }
+        }
+    }
+    '''
 
     @Autowired
     private ElasticsearchTemplate template
@@ -39,14 +175,14 @@ class ElasticsearchRunner extends AbstractOrderedCommandLineRunner {
     void run(String... args) throws Exception {
         if (drop) {
             log.info("Dropping Elasticsearch database [${url}:${port}/${clusterName}]")
-            template.deleteIndex(ElasticCase.class)
-            template.createIndex(ElasticCase.class)
-            template.deleteIndex(ElasticTask.class)
-            template.createIndex(ElasticTask.class)
+            template.deleteIndex(caseIndex)
+            template.createIndex(caseIndex)
+            template.deleteIndex(taskIndex)
+            template.createIndex(taskIndex)
         }
         log.info("Updating Elasticsearch case mapping [${caseIndex}]")
-        template.putMapping(ElasticCase.class)
+        template.putMapping(caseIndex, CASE_TYPE, CASE_MAPPING)
         log.info("Updating Elasticsearch task mapping [${taskIndex}]")
-        template.putMapping(ElasticTask.class)
+        template.putMapping(taskIndex, TASK_TYPE, TASK_MAPPING)
     }
 }
