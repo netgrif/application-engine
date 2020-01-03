73a166162c7835a80672f2028bf0b27219f45c69
Jakub Kovář <kovar@netgrif.com>
2020-01-03 08:39:15 +0100

    NAE-412 - Process and Case loading refactor - files reverted to a state similar to the original one

--- src/main/java/com/netgrif/workflow/elastic/domain/mapping/DataField.java
+++ src/main/java/com/netgrif/workflow/elastic/domain/mapping/DataField.java
@@ -4,26 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 
 import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
-@Document(indexName = "#{@elasticCaseIndex}", type = "data_fulltext")
+@AllArgsConstructor
 public class DataField {
-
-    @Id
-    private String id;
 
     @Field(type = Text)
     public String fulltext;
 
-    public DataField(String fulltext) {
-        this.fulltext = fulltext;
-    }
 }
