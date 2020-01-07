5bec78c5826a68be4975c35f75e0901ead745597
Jakub Kovář <kovar@netgrif.com>
2020-01-07 09:30:34 +0100

    Revert "NAE-412 - Process and Case loading refactor"
    Reverted to a state before experiments with join mapping (commit 9b9c2255)

--- src/main/java/com/netgrif/workflow/elastic/domain/mapping/DataField.java
+++ src/main/java/com/netgrif/workflow/elastic/domain/mapping/DataField.java
@@ -1,12 +1,9 @@
 package com.netgrif.workflow.elastic.domain.mapping;
 
-import com.netgrif.workflow.elastic.domain.ElasticCase;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Version;
 import org.springframework.data.elasticsearch.annotations.Field;
 
 import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
@@ -15,26 +12,9 @@ import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
 @NoArgsConstructor
 @EqualsAndHashCode
 @AllArgsConstructor
-public abstract class DataField {
-
-    @Id
-    private String id;
-
-    @Version
-    private Long version;
+public class DataField {
 
     @Field(type = Text)
     public String fulltext;
 
-    private JoinField dataSetJoin;
-
-    public DataField(String fulltextValue) {
-        this.fulltext = fulltextValue;
-    }
-
-    public void createParentLink(ElasticCase parent) {
-        this.dataSetJoin = new JoinField("data", parent.getId());
-    }
-
-    public abstract String getIndex();
 }
