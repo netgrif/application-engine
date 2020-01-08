9f0608fd5c10564c66838609a6fb3d6e4321adae
Jakub Kovář <kovar@netgrif.com>
2020-01-08 09:54:30 +0100

    NAE-412 - Process and Case loading refactor - UserField rework

--- src/main/java/com/netgrif/workflow/elastic/domain/UserField.java
+++ src/main/java/com/netgrif/workflow/elastic/domain/UserField.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
 
@@ -21,10 +22,14 @@ public class UserField extends DataField {
     @Field(type = Text)
     private String fullName;
 
-    public UserField(long value, String email, String fullName) {
-        super("" + value);
+    @Field(type = FieldType.Long)
+    private Long userId;
+
+    public UserField(long userId, String email, String fullName) {
+        super(String.format("%s %s", fullName, email));
         this.email = email;
         this.fullName = fullName;
+        this.userId = userId;
     }
 }
 
