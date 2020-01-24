e1cb98d0c0bc3222cf5376396fec6f5f4c0f218b
Jakub Kovář <kovar@netgrif.com>
2020-01-24 08:57:39 +0100

    NAE-559 - Fix saved filter selection - bugfix from eUlohy null data were not sent to frontend

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/ChangedFieldContainer.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/ChangedFieldContainer.groovy
@@ -1,15 +1,19 @@
 package com.netgrif.workflow.petrinet.domain.dataset.logic
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class ChangedFieldContainer {
 
-    private Map<String,Map<String,Object>> changedFields
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, Map<String, Object>> changedFields
 
     ChangedFieldContainer() {
         changedFields = new HashMap<>()
     }
 
-    void putAll(Map<String, ChangedField> changed){
-        changed.each { key, field -> changedFields.put(field.id,field.attributes)}
+    void putAll(Map<String, ChangedField> changed) {
+        changed.each { key, field -> changedFields.put(field.id, field.attributes) }
     }
 
     Map<String, Map<String, Object>> getChangedFields() {
