4da3bab03494c735fd4ab056ad0d60325cc343d9
Tomáš Kováčik <kovacik@netgrif.com>
2020-04-17 13:35:40 +0200

    NAE-849 - Boolean validation

--- src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedBooleanField.java
+++ src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedBooleanField.java
@@ -1,8 +1,12 @@
 package com.netgrif.workflow.workflow.web.responsebodies;
 
 import com.netgrif.workflow.petrinet.domain.dataset.BooleanField;
+import com.netgrif.workflow.petrinet.domain.dataset.logic.validation.LocalizedValidation;
+import com.netgrif.workflow.petrinet.domain.dataset.logic.validation.Validation;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @Data
@@ -10,8 +14,17 @@ public class LocalisedBooleanField extends LocalisedField {
 
     private Boolean defaultValue;
 
+    private List<LocalizedValidation> validations;
+
     public LocalisedBooleanField(BooleanField field, Locale locale) {
         super(field, locale);
+        List<LocalizedValidation> locVal = new ArrayList<LocalizedValidation>();
+        if (field.getValidations() != null) {
+            for(Validation val:field.getValidations()){
+                locVal.add(val.getLocalizedValidation(locale));
+            }
+        }
+        this.validations = locVal;
         this.defaultValue = field.getDefaultValue();
     }
 }
