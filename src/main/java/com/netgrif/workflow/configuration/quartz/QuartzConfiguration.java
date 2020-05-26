b56cadd5bce6e50e705c9991d75a164debccae36
Timotej Bezák <bezak@netgrif.com>
2020-05-26 23:04:34 +0200

    NAE-852 - Rule engine - make template.drl a classpathresource - minor refactor in RuleEvaluationScheduleService - clean up imports, annotations - start writing tests for rule engine

--- src/main/java/com/netgrif/workflow/configuration/quartz/QuartzConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/quartz/QuartzConfiguration.java
@@ -1,23 +1,14 @@
 package com.netgrif.workflow.configuration.quartz;
 
 import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
@@ -26,8 +17,6 @@ import java.util.Properties;
 
 @Configuration
 public class QuartzConfiguration {
-
-    private static final Logger log = LoggerFactory.getLogger(QuartzConfiguration.class);
 
     @Value("${quartz.jdbc-url}")
     private String jdbcUrl;
@@ -38,17 +27,11 @@ public class QuartzConfiguration {
     @Value("${spring.datasource.password}")
     private String jdbcPass;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String ddlAuto;
-
     @Autowired
     private ApplicationContext applicationContext;
 
     @Autowired
     private QuartzProperties quartzProperties;
-
-    @Autowired
-    private DataSource dataSource;
 
     @QuartzDataSource
     public DataSource quartzDataSource() {
