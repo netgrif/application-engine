e61ee7702aa1d356e11513a3426ca34826a78dd8
Kranec <kranec@netgrif.com>
2020-06-23 13:25:16 +0200

    NAE-880 - Configurable storage path - refactor after pull request comments

--- src/main/groovy/com/netgrif/workflow/startup/StorageRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/StorageRunner.groovy
@@ -1,8 +1,9 @@
 package com.netgrif.workflow.startup
 
+import com.netgrif.workflow.workflow.domain.FileStorageConfiguration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
@@ -12,21 +13,18 @@ class StorageRunner extends AbstractOrderedCommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(StorageRunner)
 
-    @Value('${storage.clean}')
-    private boolean cleanStorage
-
-    @Value('${storage.path}')
-    private String storagePath
+    @Autowired
+    private FileStorageConfiguration fileStorageConfiguration
 
     @Override
     void run(String... strings) throws Exception {
         log.info("Creating storage folder")
-        File storage = new File("${storagePath}/uploadedModels/model.txt")
+        File storage = new File("${fileStorageConfiguration.getStoragePath()}/uploadedModels/model.txt")
         storage.getParentFile().mkdirs()
 
         if (cleanStorage) {
             log.info("Removing files from storage folder and it's sub-folders")
-            purgeDirectory(new File(storagePath))
+            purgeDirectory(new File(fileStorageConfiguration.getStoragePath()))
         }
 
         log.info("Creating log folder")
