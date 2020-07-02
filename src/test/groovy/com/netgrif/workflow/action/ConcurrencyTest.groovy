f665f829b9159164cefea0d3a5d910a8ab8ec451
Jakub Kovář <kovar@netgrif.com>
2020-07-02 18:03:23 +0200

    NAE-897 - Action Delegate Context Leak - better concurrency performance - test

---
+++ src/test/groovy/com/netgrif/workflow/action/ConcurrencyTest.groovy
@@ -0,0 +1,52 @@
+package com.netgrif.workflow.action
+
+import com.netgrif.workflow.importer.service.Config
+import com.netgrif.workflow.importer.service.Importer
+import com.netgrif.workflow.startup.ImportHelper
+import com.netgrif.workflow.workflow.domain.Case
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles(["test"])
+@SpringBootTest
+class ConcurrencyTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ConcurrencyTest)
+
+    @Autowired
+    private Importer importer
+
+    @Autowired
+    private ImportHelper importHelper
+
+    @Test
+    void test() {
+        def mainNet = importer.importPetriNet(new File("src/test/resources/action_delegate_concurrency_test.xml"), "action_delegate_concurrency_test", "TST", new Config())
+        assert mainNet.isPresent()
+
+        List<Case> cases = []
+        10.times {
+            cases << importHelper.createCase("Case 1", mainNet.get())
+        }
+
+        def threads = []
+        cases.each { it ->
+            String caseId = it.stringId
+            threads << new Thread({
+                log.info("Running case $caseId")
+                importHelper.assignTaskToSuper("task", caseId)
+            })
+        }
+
+        threads.each {
+            it.start()
+        }
+    }
+}
