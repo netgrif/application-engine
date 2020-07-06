464d8c96d333cdc4a498854f15f6b2bf8ebee406
Bezák <bezak@netgrif.com>
2020-07-06 23:01:23 +0200

    NAE-852 - Rule engine - add stress test - add more logging

--- src/main/java/com/netgrif/workflow/configuration/drools/RefreshableKieBase.java
+++ src/main/java/com/netgrif/workflow/configuration/drools/RefreshableKieBase.java
@@ -4,7 +4,10 @@ import com.netgrif.workflow.configuration.drools.interfaces.IKnowledgeBaseInitia
 import com.netgrif.workflow.configuration.drools.interfaces.IRefreshableKieBase;
 import com.netgrif.workflow.rules.domain.RuleRepository;
 import org.kie.api.KieBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,11 +15,16 @@ import java.time.LocalDateTime;
 @Component
 public class RefreshableKieBase implements IRefreshableKieBase {
 
+    public static final Logger log = LoggerFactory.getLogger(RefreshableKieBase.class);
+
+    @Value("${drools.knowBase.auto-refresh}")
+    private boolean autoRefresh;
+
     private LocalDateTime lastRefresh;
     private KieBase kieBase;
 
-    private RuleRepository ruleRepository;
-    private IKnowledgeBaseInitializer knowledgeBaseInitializer;
+    private final RuleRepository ruleRepository;
+    private final IKnowledgeBaseInitializer knowledgeBaseInitializer;
 
 
     public RefreshableKieBase(@Autowired RuleRepository ruleRepository, @Autowired IKnowledgeBaseInitializer knowledgeBaseInitializer) {
@@ -29,11 +37,13 @@ public class RefreshableKieBase implements IRefreshableKieBase {
     }
 
     public boolean shouldRefresh() {
-        return ruleRepository.existsByLastUpdateAfter(lastRefresh);
+        return autoRefresh && ruleRepository.existsByLastUpdateAfter(lastRefresh);
     }
 
     public void refresh() {
         this.lastRefresh = LocalDateTime.now();
+        log.info("Refreshing kieBase");
         this.kieBase = knowledgeBaseInitializer.constructKieBase();
+        log.info("KieBase refreshed");
     }
 }
