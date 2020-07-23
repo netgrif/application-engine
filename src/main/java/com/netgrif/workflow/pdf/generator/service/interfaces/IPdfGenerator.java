0b0303392c738ca21f3f8e973d11abfb6a2e3761
Štefan Renczés <renczes@netgrif.com>
2020-07-23 09:57:33 +0200

    MAE-396 - PDF generator - export vsetkych typov fieldov do PDF

--- src/main/java/com/netgrif/workflow/pdf/generator/service/interfaces/IPdfGenerator.java
+++ src/main/java/com/netgrif/workflow/pdf/generator/service/interfaces/IPdfGenerator.java
@@ -6,5 +6,12 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 public interface IPdfGenerator {
+    /**
+     * Function is called when a PDF needs to be generated from the transition. This generate PDF from transition
+     * data by calling corresponding functions.
+     * @param formCase
+     * @param transitionId
+     * @throws IOException
+     */
     void convertCaseForm(Case formCase, String transitionId) throws IOException;
 }
