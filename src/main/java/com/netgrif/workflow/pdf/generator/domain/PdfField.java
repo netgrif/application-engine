5302e85f00ebaa6fba10f77a5e41ea0d3eba0d1a
Štefan Renczés <renczes@netgrif.com>
2020-07-23 09:57:33 +0200

    MAE-396 - PDF generator - export vsetkych typov fieldov do PDF

--- src/main/java/com/netgrif/workflow/pdf/generator/domain/PdfField.java
+++ src/main/java/com/netgrif/workflow/pdf/generator/domain/PdfField.java
@@ -8,6 +8,9 @@ import com.netgrif.workflow.petrinet.domain.dataset.FieldType;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Class that holds information about fields that will be exported to PDF
  * */
@@ -23,7 +26,7 @@ public class PdfField extends PdfProperties implements Comparable<PdfField>{
 
     @Getter
     @Setter
-    private FieldType type;
+    private FieldType type = null;
 
     @Getter
     @Setter
@@ -31,7 +34,11 @@ public class PdfField extends PdfProperties implements Comparable<PdfField>{
 
     @Getter
     @Setter
-    private String value = null;
+    private List<String> values = null;
+
+    @Getter
+    @Setter
+    private List<String> choices = null;
 
     @Getter
     @Setter
@@ -81,45 +88,45 @@ public class PdfField extends PdfProperties implements Comparable<PdfField>{
     @Setter
     private boolean dgField;
 
-
-    public PdfField(String fieldId, DataGroup dataGroup, int layoutX, int layoutY, int width, int originalHeight,
-                    FieldType type, String label, String value){
+    public PdfField(String fieldId, DataGroup dataGroup, FieldType type, String label, List<String> values, List<String> choices){
         this.fieldId = fieldId;
         this.dataGroup = dataGroup;
-        this.layoutX = layoutX;
-        this.layoutY = layoutY;
-        this.width = width;
-        this.height = originalHeight;
         this.type = type;
         this.label = label;
-        this.value = value;
+        this.values = values;
+        this.choices = choices;
         this.dgField = false;
     }
 
-    public PdfField(String fieldId, int layoutX, int layoutY, int width, int originalHeight, String label){
+    public PdfField(String fieldId, int layoutX, int layoutY, int width, int originalHeight,
+                    String label, boolean dgField){
         this.fieldId = fieldId;
         this.layoutX = layoutX;
         this.layoutY = layoutY;
         this.width = width;
         this.height = originalHeight;
         this.label = label;
-        this.dgField = true;
+        this.dgField = dgField;
     }
 
     /**
      * Counts and changes fields default height in case there are multiple lines and text will not fit into default size
+     * @param fontSize size of font
      */
     public void countMultiLineHeight(int fontSize){
         int maxLabelLineLength = PdfDrawer.getMaxLabelLineSize(this.width, fontSize);
-        int maxValueLineLength = PdfDrawer.getMaxValueLineSize(this.width);
+        int maxValueLineLength = PdfDrawer.getMaxValueLineSize(this.width - 3 * PADDING);
         int multiLineHeight = 0;
 
-        String[] splitLabel = DataConverter.generateMultiLineText(this.label, maxLabelLineLength);
-        multiLineHeight += splitLabel.length * LINE_HEIGHT + PADDING;
+        List<String> splitLabel = DataConverter.generateMultiLineText(Collections.singletonList(this.label), maxLabelLineLength);
+        multiLineHeight += splitLabel.size() * LINE_HEIGHT + PADDING;
 
-        if(this.value != null){
-            String[] splitText = DataConverter.generateMultiLineText(this.value, maxValueLineLength);
-            multiLineHeight += splitText.length * LINE_HEIGHT + PADDING;
+        if(this.choices != null){
+            List<String> splitText = DataConverter.generateMultiLineText(this.choices, maxValueLineLength);
+            multiLineHeight += splitText.size() * LINE_HEIGHT + PADDING;
+        }else if(this.values != null){
+            List<String> splitText = DataConverter.generateMultiLineText(this.values, maxValueLineLength);
+            multiLineHeight += splitText.size() * LINE_HEIGHT + PADDING;
         }
 
         this.changedSize = changeHeight(multiLineHeight);
--- src/main/java/com/netgrif/workflow/pdf/generator/service/interfaces/IPdfDrawer.java
+++ src/main/java/com/netgrif/workflow/pdf/generator/service/interfaces/IPdfDrawer.java
@@ -1,16 +1,18 @@
 package com.netgrif.workflow.pdf.generator.service.interfaces;
 
+import com.netgrif.workflow.petrinet.domain.dataset.FieldType;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface IPdfDrawer {
     void setupDrawer(PDDocument pdf);
     void newPage() throws IOException;
     void drawTitle(String title, int fieldX, int fieldY, int fieldWidth) throws IOException;
-    void drawTextField(String label, String value, int fieldX, int fieldY, int fieldWidth, int fieldHeight) throws IOException;
-    int drawFieldLabel(String text, int fieldX, int fieldY, int fieldWidth, int fieldHeight, PDType0Font font, int fontSize) throws IOException;
     void closeContentStream() throws IOException;
-    void drawTextValue(String text, int fieldX, int fieldY, int fieldWidth, int fieldHeight, int lineCounter) throws IOException;
+    void drawTextField(String label, List<String> value, int fieldX, int fieldY, int fieldWidth, int fieldHeight) throws IOException;
+    void drawSelectionField(String label, List<String> choices, List<String> values, int fieldX, int fieldY, int fieldWidth, int fieldHeight, FieldType type) throws IOException;
+    int drawLabel(String text, int fieldX, int fieldY, int fieldWidth, int fieldHeight, PDType0Font font, int fontSize) throws IOException;
 }
