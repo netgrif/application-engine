1ed7c92f1962c3bf46b48f980a8d3d98c517613a
Jakub Kovář <kovar@netgrif.com>
2020-08-05 08:06:16 +0200

    NAE-804 - Overridable endpoint authorisation
    - This reverts commit c5f42edc

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IDataService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IDataService.java
@@ -6,11 +6,13 @@ import com.netgrif.workflow.petrinet.domain.Transition;
 import com.netgrif.workflow.petrinet.domain.dataset.Field;
 import com.netgrif.workflow.petrinet.domain.dataset.FileField;
 import com.netgrif.workflow.petrinet.domain.dataset.logic.ChangedField;
+import com.netgrif.workflow.petrinet.domain.dataset.logic.ChangedFieldByFileFieldContainer;
 import com.netgrif.workflow.petrinet.domain.dataset.logic.ChangedFieldContainer;
 import com.netgrif.workflow.petrinet.domain.dataset.logic.action.Action;
 import com.netgrif.workflow.workflow.domain.Case;
 import com.netgrif.workflow.workflow.domain.Task;
 import com.netgrif.workflow.workflow.service.FileFieldInputStream;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,7 +37,7 @@ public interface IDataService {
 
     InputStream download(String url) throws IOException;
 
-    boolean saveFile(String taskId, String fieldId, MultipartFile multipartFile);
+    ChangedFieldByFileFieldContainer saveFile(String taskId, String fieldId, MultipartFile multipartFile);
 
     List<DataGroup> getDataGroups(String taskId);
 
