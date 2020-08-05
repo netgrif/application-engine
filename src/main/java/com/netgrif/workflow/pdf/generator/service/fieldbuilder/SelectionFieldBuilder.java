5a586c29d8d56e26fdb98021823a4b6662da7baf
Štefan Renczés <renczes@netgrif.com>
2020-08-05 13:18:58 +0200

    MAE-396 - PDF generator - dodatocna uprava k prelozeniu choices v pripade enumu a multichoice

--- src/main/java/com/netgrif/workflow/pdf/generator/service/fieldbuilder/SelectionFieldBuilder.java
+++ src/main/java/com/netgrif/workflow/pdf/generator/service/fieldbuilder/SelectionFieldBuilder.java
@@ -2,12 +2,20 @@ package com.netgrif.workflow.pdf.generator.service.fieldbuilder;
 
 import com.netgrif.workflow.pdf.generator.config.PdfResource;
 import com.netgrif.workflow.pdf.generator.domain.PdfSelectionField;
+import com.netgrif.workflow.petrinet.domain.I18nString;
+
+import java.util.List;
+import java.util.Set;
 
 public abstract class SelectionFieldBuilder extends FieldBuilder {
 
     public SelectionFieldBuilder(PdfResource resource) {
         super(resource);
     }
+
+    protected abstract List<String> getTranslatedSet(Set<I18nString> choices);
+
+    protected abstract String getTranslatedString(Set<I18nString> choices, String value);
 
     protected void setFieldPositions(PdfSelectionField pdfField, int fontSize) {
         pdfField.setX(countPosX(pdfField));
@@ -17,4 +25,5 @@ public abstract class SelectionFieldBuilder extends FieldBuilder {
         pdfField.setBottomY(countBottomPosY(pdfField,resource));
         pdfField.countMultiLineHeight(fontSize, resource);
     }
+
 }
