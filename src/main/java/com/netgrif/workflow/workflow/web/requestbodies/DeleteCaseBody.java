c98d69f3f3ab1849771da1c8dc057662b3bb91b0
Jakub Kovář <kovar@netgrif.com>
2020-08-07 07:58:35 +0200

    NAE-876 - Tree Case View
    - subtree removal support from delete case endpoint -> temporal solution until CRUD is implemented

---
+++ src/main/java/com/netgrif/workflow/workflow/web/requestbodies/DeleteCaseBody.java
@@ -0,0 +1,8 @@
+package com.netgrif.workflow.workflow.web.requestbodies;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DeleteCaseBody {
+    public boolean deleteSubtree;
+}
