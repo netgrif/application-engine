8255d2338d004dde0aec2bf69f09a564cb706a5b
Timotej Bezák <bezak@netgrif.com>
2020-08-09 21:08:08 +0200

    NAE-852 - Rule engine
    - put template into resource file
    - add default properties where possible

--- src/main/groovy/com/netgrif/workflow/startup/RuleEngineRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/RuleEngineRunner.groovy
@@ -1,6 +1,5 @@
 package com.netgrif.workflow.startup
 
-
 import com.netgrif.workflow.configuration.drools.interfaces.IRefreshableKieBase
 import com.netgrif.workflow.configuration.drools.interfaces.IRuleEngineGlobalsProvider
 import groovy.text.SimpleTemplateEngine
@@ -8,6 +7,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
 
 @Component
@@ -21,60 +21,20 @@ class RuleEngineRunner extends AbstractOrderedCommandLineRunner {
     @Autowired
     private IRuleEngineGlobalsProvider sessionInitializer
 
-    @Value('${drools.template.generate}')
+    @Value('${drools.template.generate:#{true}}')
     private boolean generate
 
-    @Value('${drools.template.path}')
-    private String templatePath
+    @Value('${drools.template.path:#{"rules/templates/template.drl"}}')
+    private String generatedTemplatePath
 
-    private static final String TEMPLATE = '''
-        template header
-        ruleId
-        salienceVal
-        ruleEnabled
-        dateEffective
-        dateExpires
-        whenCondition
-        thenAction
-        
-        import java.util.HashMap;
-        import java.util.ArrayList;
-        import org.slf4j.Logger;
-        import java.time.LocalDate;
-        import java.time.LocalDateTime;
-        import com.netgrif.workflow.utils.DateUtils;
-       
-        import com.netgrif.workflow.workflow.domain.*
-        import com.netgrif.workflow.petrinet.domain.*
-        import com.netgrif.workflow.rules.domain.facts.*
-        
-        import org.quartz.*
-        
-<%= imports %>
-        
-<%= globals %>
-        
-        template "standard_template"
-        
-        rule "@{ruleId}" salience @{salienceVal}
-          dialect "mvel"
-          enabled @{ruleEnabled}
-          @{dateEffective}
-          @{dateExpires}
-          when
-            @{whenCondition}
-          then
-            @{thenAction}
-        end
-        end template
-    '''
-
+    @Value('${drools.template-resource.classpath:#{"rules/templates/template.drl"}}')
+    private String templateResource
 
     @Override
     void run(String... strings) throws Exception {
         log.info("Rule engine runner starting")
         if (generate) {
-            log.info("Generating template to " + templatePath)
+            log.info("Generating template to " + generatedTemplatePath)
             generateTemplate()
         }
 
@@ -86,12 +46,13 @@ class RuleEngineRunner extends AbstractOrderedCommandLineRunner {
     void generateTemplate() {
         def engine = new SimpleTemplateEngine()
         def binding = [
-                imports : sessionInitializer.imports().collect { "        $it" }.join(""),
-                globals : sessionInitializer.globals().collect { "        ${it.toString()}" }.join(""),
+                imports : sessionInitializer.imports().collect { "$it" }.join(""),
+                globals : sessionInitializer.globals().collect { "${it.toString()}" }.join(""),
         ]
-        String template = engine.createTemplate(TEMPLATE).make(binding)
 
-        File templateFile = new File(templatePath)
+        String template = engine.createTemplate(new ClassPathResource(templateResource).getFile().getText()).make(binding)
+
+        File templateFile = new File(generatedTemplatePath)
         templateFile.getParentFile().mkdirs()
         boolean deleted = templateFile.delete()
         if (!deleted) {
