26c0882efadd64ae17b91c3471a0ca867bb8014f
Jakub Kovář <kovar@netgrif.com>
2020-08-14 16:39:59 +0200

    NAE-804 - Overridable endpoint authorisation
    - fixed naming of classes

--- src/main/java/com/netgrif/workflow/workflow/service/FilterAuthorizationService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/FilterAuthorizationService.java
@@ -2,11 +2,11 @@ package com.netgrif.workflow.workflow.service;
 
 import com.netgrif.workflow.auth.domain.LoggedUser;
 import com.netgrif.workflow.workflow.domain.Filter;
-import com.netgrif.workflow.workflow.service.interfaces.IFilterAuthenticationService;
+import com.netgrif.workflow.workflow.service.interfaces.IFilterAuthorizationService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FilterAuthenticationService implements IFilterAuthenticationService {
+public class FilterAuthorizationService implements IFilterAuthorizationService {
 
     @Override
     public boolean canCallDelete(LoggedUser user, Filter filter) {
