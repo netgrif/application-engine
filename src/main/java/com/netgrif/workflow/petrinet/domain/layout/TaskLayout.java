78ad70776bda0fafeb3a8b607c566d9744718839
Jozef Daxner <daxner@netgrif.com>
2020-08-17 10:33:29 +0200

    NAE-874 Customisable data field offset
    - refactor

--- src/main/java/com/netgrif/workflow/petrinet/domain/layout/TaskLayout.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/layout/TaskLayout.java
@@ -1,5 +1,6 @@
 package com.netgrif.workflow.petrinet.domain.layout;
 
+import com.netgrif.workflow.importer.model.Transition;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,11 @@ public class TaskLayout extends Layout {
     public TaskLayout(Integer rows, Integer cols, Integer offset) {
         super(rows, cols);
         this.offset = offset;
+    }
+
+    public TaskLayout(Transition data) {
+        super(data.getLayout().getRows(), data.getLayout().getCols());
+        this.offset = data.getLayout().getOffset();
     }
 
     public TaskLayout() {
