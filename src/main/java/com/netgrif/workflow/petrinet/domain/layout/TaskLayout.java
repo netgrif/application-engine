3ed744a36f128a95edb4a08d82abce9f1cc48ae9
Jakub Kovář <kovar@netgrif.com>
2020-09-10 13:18:51 +0200

    NAE-965 - Grid layout container
    - reverted removal of layout type attribute from transition

--- src/main/java/com/netgrif/workflow/petrinet/domain/layout/TaskLayout.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/layout/TaskLayout.java
@@ -9,11 +9,13 @@ import lombok.*;
 public class TaskLayout extends Layout {
 
     private Integer offset;
+    private String type;
     private String fieldAlignment;
 
     public TaskLayout(Transition data) {
         super(data.getLayout().getRows(), data.getLayout().getCols());
         this.offset = data.getLayout().getOffset();
+        this.type = data.getLayout().getType() != null ? data.getLayout().getType().value() : null;
         this.fieldAlignment = data.getLayout().getFieldAlignment() != null ? data.getLayout().getFieldAlignment().value() : null;
     }
 }
