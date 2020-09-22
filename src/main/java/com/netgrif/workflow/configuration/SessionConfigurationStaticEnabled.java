852a5cfa88eaa62f06e61a60b1e9f7425529475c
Jozef Machac <machac@netgrif.com>
2020-09-22 09:06:40 +0200

    NAE-997 - Engine docker image
    - Docker Finish

--- src/main/java/com/netgrif/workflow/configuration/SessionConfigurationStaticEnabled.java
+++ src/main/java/com/netgrif/workflow/configuration/SessionConfigurationStaticEnabled.java
@@ -1,8 +1,10 @@
 package com.netgrif.workflow.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -14,9 +16,16 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 )
 public class SessionConfigurationStaticEnabled {
 
+    @Value("${spring.session.redis.host}")
+    private String hostName;
+
+    @Value("${spring.session.redis.port}")
+    private Integer port;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostName, port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
 }
