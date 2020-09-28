6932bf3876181a355039155bb59d2960bda2e424
Štefan Renczés <renczes@netgrif.com>
2020-09-28 11:44:15 +0200

    MAE-994 - Organization groups - dodatočné úpravy podľa meetingu

--- src/main/java/com/netgrif/workflow/orgstructure/groups/interfaces/INextGroupService.java
+++ src/main/java/com/netgrif/workflow/orgstructure/groups/interfaces/INextGroupService.java
@@ -11,17 +11,27 @@ import java.util.Map;
 
 public interface INextGroupService {
 
+    Case createGroup(User author);
+
+    Case createGroup(String title, User author);
+
     Case findGroup(String groupID);
 
     List<Case> findAllGroups();
 
+    Case findDefaultGroup();
+
     List<Case> findByPredicate(Predicate predicate);
+
+    Map<String, I18nString> inviteUser(String email, Map<String, I18nString> existingUsers, Case groupCase);
+
+    void addUser(User user, Case groupCase);
 
     Map<String, I18nString> addUser(User user, Map<String, I18nString> existingUsers);
 
-    Map<String, I18nString> removeUser(HashSet<String> usersToRemove, Map<String, I18nString> existingUsers);
+    void removeUser(User user, Case groupCase);
+
+    Map<String, I18nString> removeUser(HashSet<String> usersToRemove, Map<String, I18nString> existingUsers, Case groupCase);
 
     List<User> getMembers(Case groupCase);
-
-    Map<String, I18nString> listToMap(List<Case> cases);
 }
