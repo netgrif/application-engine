ed37bef7949d5211a0776925ab3723c6fa5f7603
Jakub Kovář <kovar@netgrif.com>
2020-09-29 14:09:28 +0200

    NAE-1010 - 500 error po zavolaní getData v taskRefe
    - data group layout is deserialized correctly

--- src/main/java/com/netgrif/workflow/importer/service/Importer.java
+++ src/main/java/com/netgrif/workflow/importer/service/Importer.java
@@ -408,14 +408,7 @@ public class Importer {
 
         String dataGroupLayout = importDataGroup.getLayout() != null ? importDataGroup.getLayout().value() : null;
 
-        if (importDataGroup.getCols() != null || importDataGroup.getRows() != null) {
-            dataGroup.setLayout(new DataGroupLayout(importDataGroup.getRows(), importDataGroup.getCols(), dataGroupLayout));
-        }
-        if (importDataGroup.getCols() == null && (transition.getLayout() != null && transition.getLayout().getCols() != null)) {
-            dataGroup.setLayout(dataGroup.getLayout() != null ?
-                    new DataGroupLayout(dataGroup.getLayout().getRows(), transition.getLayout().getCols(), dataGroupLayout) :
-                    new DataGroupLayout(null, transition.getLayout().getCols(), dataGroupLayout));
-        }
+        dataGroup.setLayout(new DataGroupLayout(importDataGroup.getRows(), importDataGroup.getCols(), dataGroupLayout));
 
         dataGroup.setTitle(toI18NString(importDataGroup.getTitle()));
         dataGroup.setAlignment(alignment);
