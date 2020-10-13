a208247bcc3b168991e7ac6ef1df6a404ca87597
Štefan Renczés <renczes@netgrif.com>
2020-10-13 09:25:13 +0200

    NAE-994 - Group management
    - commiting test

--- src/main/groovy/com/netgrif/workflow/startup/GroupRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/GroupRunner.groovy
@@ -36,17 +36,23 @@ public class GroupRunner extends AbstractOrderedCommandLineRunner {
 
     @Override
     void run(String... args) throws Exception {
-        if(petriNetService.getNewestVersionByIdentifier(GROUP_PETRINET_IDENTIFIER) != null){
+        createDefaultGroup()
+    }
+
+    Optional<PetriNet> createDefaultGroup(){
+        PetriNet group
+        if((group = petriNetService.getNewestVersionByIdentifier(GROUP_PETRINET_IDENTIFIER)) != null){
             log.info("Petri net for groups has already been imported.")
-            return
+            return new Optional<>(group)
         }
 
         Optional<PetriNet> groupNet =  helper.createNet(GROUP_FILE_NAME, "major", systemCreator.loggedSystem)
 
         if(!groupNet.present){
             log.error("Import of petri net for groups failed!")
-            return
+            return groupNet
         }
         nextGroupService.createDefaultSystemGroup(userService.getLoggedOrSystem())
+        return groupNet;
     }
 }
