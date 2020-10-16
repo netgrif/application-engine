ce89a2aa971c400869d68865fe7595c06755bd24
Štefan Renczés <renczes@netgrif.com>
2020-10-16 11:04:37 +0200

    NAE-1027 - Event phases for data fields events
    - repairing PR

--- src/main/java/com/netgrif/workflow/petrinet/domain/DataEvent.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/DataEvent.java
@@ -3,6 +3,8 @@ package com.netgrif.workflow.petrinet.domain;
 import com.netgrif.workflow.petrinet.domain.dataset.logic.action.Action;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,14 +15,37 @@ public class DataEvent {
 
     private Action.ActionTrigger trigger;
 
-    private Map<String, List<Action>> actions;
+    private DataEventPhase defaultPhase;
+
+    private Map<DataEventPhase, List<Action>> actions;
 
     public DataEvent(){
-
+        this.actions = new HashMap<>();
     }
 
     public DataEvent(String id, String type) {
         this.id = id;
         this.trigger = Action.ActionTrigger.fromString(type);
+        initActions();
+    }
+
+    public DataEvent(String type) {
+        this.trigger = Action.ActionTrigger.fromString(type);
+        initActions();
+    }
+
+    public void resolveDefaultPhase(){
+        if (trigger.equals(Action.ActionTrigger.GET))
+            this.defaultPhase = DataEventPhase.PRE;
+        else if (trigger.equals(Action.ActionTrigger.SET))
+            this.defaultPhase = DataEventPhase.POST;
+        else
+            this.defaultPhase = null;
+    }
+
+    private void initActions(){
+        this.actions = new HashMap<>();
+        this.actions.put(DataEventPhase.PRE, new ArrayList<>());
+        this.actions.put(DataEventPhase.POST, new ArrayList<>());
     }
 }
