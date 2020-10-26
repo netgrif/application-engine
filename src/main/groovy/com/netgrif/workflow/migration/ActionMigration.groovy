3cf858c83ec3a57e22f5a858b467e5eabf4dad05
Štefan Renczés <renczes@netgrif.com>
2020-10-26 12:43:59 +0100

    NAE-1027 - Event phases for datafield actions
    - implemented reimport of petri nets and replace old actions with new ones

--- src/main/groovy/com/netgrif/workflow/migration/ActionMigration.groovy
+++ src/main/groovy/com/netgrif/workflow/migration/ActionMigration.groovy
@@ -1,19 +1,63 @@
 package com.netgrif.workflow.migration
 
-import com.netgrif.workflow.petrinet.domain.PetriNet;
-import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import com.netgrif.workflow.auth.service.interfaces.IUserService
+import com.netgrif.workflow.petrinet.domain.PetriNet
+import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Component
+
+import java.util.stream.Collectors
 
 @Component
-class ActionMigration extends MigrationOrderedCommandLineRunner {
+@Slf4j
+class ActionMigration {
 
     @Autowired
     private IPetriNetService petriNetService
 
+    @Autowired
+    private IUserService userService;
 
-    @Override
-    void migrate() {
-        List<PetriNet> allNets = petriNetService.getAll()
+    void migrateActions(String petriNetPath){
+        InputStream netStream = new ClassPathResource(petriNetPath).inputStream
+        Optional<PetriNet> newPetriNet = petriNetService.importPetriNet(netStream, "major", userService.loggedOrSystem.transformToLoggedUser())
+        List<PetriNet> oldPetriNets = new ArrayList<>()
+
+        if(newPetriNet.isPresent())
+            oldPetriNets = petriNetService.getByIdentifier(newPetriNet.get().importId)
+                    .stream().filter({ net -> (net.version != newPetriNet.get().version)})
+                    .collect(Collectors.toList())
+        else
+            log.error("Petri net from file [" + petriNetPath + "] was not imported.")
+
+        if(oldPetriNets.size() > 0)
+            oldPetriNets.each {net ->
+                migrateDataSetActions(newPetriNet.get(), net)
+                migrateDataRefActions(newPetriNet.get(), net)
+                petriNetService.save(net)
+            }
+        else
+            log.error("Petri net from file [" + petriNetPath + "] was not imported.")
     }
+
+    private void migrateDataSetActions(PetriNet newPetriNet, PetriNet oldPetriNet){
+            newPetriNet.dataSet.each {key, data ->
+                if (data.events != null && data.events.size() > 0){
+                    oldPetriNet.dataSet[key].events = data.events
+                }
+            }
+    }
+
+    private void migrateDataRefActions(PetriNet newPetriNet, PetriNet oldPetriNet){
+        newPetriNet.transitions.each {transKey, trans ->
+           trans.dataSet.each {dataKey, data ->
+               if(data.events != null && data.events.size() > 0){
+                   oldPetriNet.transitions[transKey].dataSet[dataKey].events = data.events
+               }
+           }
+        }
+    }
+
 }
