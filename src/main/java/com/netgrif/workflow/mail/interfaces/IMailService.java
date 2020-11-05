9dfaa31dc69176a6e1b5ad9cd26ade0ace884e73
Štefan Renczés <renczes@netgrif.com>
2020-11-05 07:59:44 +0100

    NAE-1086 - Mail Action API
    - implemented methods in MailService
    - implemented methods in ActionDelegate

--- src/main/java/com/netgrif/workflow/mail/interfaces/IMailService.java
+++ src/main/java/com/netgrif/workflow/mail/interfaces/IMailService.java
@@ -1,11 +1,15 @@
 package com.netgrif.workflow.mail.interfaces;
 
 import com.netgrif.workflow.auth.domain.User;
+import com.netgrif.workflow.mail.EmailType;
 import freemarker.template.TemplateException;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public interface IMailService {
@@ -15,4 +19,8 @@ public interface IMailService {
     void sendPasswordResetEmail(User user) throws MessagingException, IOException, TemplateException;
 
     void testConnection();
+
+    void sendMail(List<String> recipients, EmailType type, Map<String, Object> model, Map<String, File> attachments) throws MessagingException, IOException, TemplateException;
+
+    void sendMail(List<String> recipients, String subject, String text, boolean isHtml, Map<String, File> attachments) throws MessagingException, IOException, TemplateException;
 }
