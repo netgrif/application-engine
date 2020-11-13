4ccd989fc11ff2cff7b7a33499a245f0fcc32b60
Štefan Renczés <renczes@netgrif.com>
2020-11-13 15:21:01 +0100

    NAE-1086 - Mail Action API
    - refactored functions in IMailService

--- src/main/java/com/netgrif/workflow/mail/domain/TypedMailDraft.java
+++ src/main/java/com/netgrif/workflow/mail/domain/TypedMailDraft.java
@@ -2,6 +2,7 @@ package com.netgrif.workflow.mail.domain;
 
 import com.netgrif.workflow.mail.EmailType;
 import lombok.Builder;
+import lombok.Data;
 
 import java.io.File;
 import java.util.HashMap;
@@ -9,14 +10,15 @@ import java.util.List;
 import java.util.Map;
 
 @Builder(builderMethodName = "typedMailDraftBuilder")
+@Data
 public class TypedMailDraft {
-    List<String> recipients;
+    private List<String> recipients;
 
-    EmailType type;
+    private EmailType type;
 
-    Map<String, Object> model;
+    private Map<String, Object> model;
 
-    Map<String, File> attachments;
+    private Map<String, File> attachments;
 
     public static TypedMailDraftBuilder builder(List<String> recipients){
         return typedMailDraftBuilder().recipients(recipients).model(new HashMap<>()).attachments(new HashMap<>());
