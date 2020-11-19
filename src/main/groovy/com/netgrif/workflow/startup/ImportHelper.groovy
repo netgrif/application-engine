fcb63b0a92bd59a58afa2dff5f30da656e47ae35
Jozef Machac <machac@netgrif.com>
2020-11-19 13:39:16 +0100

    NAE-1107 - ImportHelper & superCreator
    - ConditionalOnProperty

--- src/main/groovy/com/netgrif/workflow/startup/ImportHelper.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/ImportHelper.groovy
@@ -30,10 +30,12 @@ import groovy.json.JsonOutput
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
 
+@ConditionalOnProperty(value = "admin.create-super", matchIfMissing = true)
 @Component
 class ImportHelper {
 
