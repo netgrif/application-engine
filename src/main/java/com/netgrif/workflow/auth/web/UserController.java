ecf73aceb01604e6963b11146f5e422ca180c430
Jakub Kovář <kovar@netgrif.com>
2020-11-27 12:02:05 +0100

    NAE-1136 - Delegate endpoint doesn't work
    - moved Long parsing responsibility from Spring to our code
    - added exception handling in Advice classes

--- src/main/java/com/netgrif/workflow/auth/web/UserController.java
+++ src/main/java/com/netgrif/workflow/auth/web/UserController.java
@@ -189,8 +189,9 @@ public class UserController {
             @ApiResponse(code = 200, message = "OK", response = MessageResource.class),
             @ApiResponse(code = 403, message = "Caller doesn't fulfill the authorisation requirements"),
     })
-    public MessageResource assignAuthorityToUser(@PathVariable("id") Long userId, @RequestBody Long authorityId) {
-        userService.assignAuthority(userId, authorityId);
+    public MessageResource assignAuthorityToUser(@PathVariable("id") Long userId, @RequestBody String authorityId) {
+        Long authority = authorityId != null ? Long.parseLong(authorityId) : null;
+        userService.assignAuthority(userId, authority);
         return MessageResource.successMessage("Authority " + authorityId + " assigned to user " + userId);
     }
 
