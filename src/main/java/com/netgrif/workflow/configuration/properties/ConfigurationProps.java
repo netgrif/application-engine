4bf2f3527c6998e4a269fcfd801030e2adbffd5f
Jakub Kovář <kovar@netgrif.com>
2020-12-01 12:48:52 +0100

    NAE-1142 - Whitelist URLs trough application properties
    - removed configuration properties file in favor of application properties

--- src/main/java/com/netgrif/workflow/configuration/properties/ConfigurationProps.java
+++ src/main/java/com/netgrif/workflow/configuration/properties/ConfigurationProps.java
@@ -1,26 +1,21 @@
 package com.netgrif.workflow.configuration.properties;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.concurrent.TimeUnit;
 
+@Data
 @Configuration
-@PropertySource("classpath:configuration.properties")
 @ConfigurationProperties(prefix = "spring.max")
 public class ConfigurationProps {
-    @Getter @Setter
+
     private int loginAttempts = 10;
 
-    @Getter @Setter
     private int emailSendsAttempts = 2;
 
-    @Getter @Setter
     private int emailBlockDuration = 1;
 
-    @Getter @Setter
     private TimeUnit emailBlockTimeType = TimeUnit.DAYS;
 }
