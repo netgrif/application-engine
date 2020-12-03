b8205f3b183c1ca7ed2473ab88fa435f63bafc84
Štefan Renczés <renczes@netgrif.com>
2020-12-03 10:15:33 +0100

    NAE-1115 - User list on task instead of roles
    - implemented user filling of userrefs on task

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/ITaskService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/ITaskService.java
@@ -83,6 +83,8 @@ public interface ITaskService {
 
     EventOutcome delegateTask(LoggedUser loggedUser, Long delegatedId, String taskId) throws TransitionNotExecutableException;
 
+    void resolveUserRef(Task task, Case useCase);
+
     void delete(Iterable<? extends Task> tasks, Case useCase);
 
     void delete(Iterable<? extends Task> tasks, String caseId);
