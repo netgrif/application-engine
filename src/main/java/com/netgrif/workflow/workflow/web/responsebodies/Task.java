04923c497b003d38516ae9b28f214bd67112a1c3
Jakub Kovář <kovar@netgrif.com>
2020-12-11 15:44:55 +0100

    NAE-1147 - Auto-generated navigation
    - fixed null pointer exception on tasks with no user

--- src/main/java/com/netgrif/workflow/workflow/web/responsebodies/Task.java
+++ src/main/java/com/netgrif/workflow/workflow/web/responsebodies/Task.java
@@ -78,7 +78,7 @@ public class Task {
         this.caseColor = task.getCaseColor();
         this.caseTitle = task.getCaseTitle();
         this.priority = task.getPriority();
-        this.user = new User(task.getUser(), true);
+        this.user = task.getUser() != null ? new User(task.getUser(), true) : null;
         this.roles = task.getRoles();
         this.startDate = task.getStartDate();
         this.finishDate = task.getFinishDate();
