d884c959c49be146bea1023f106218193eef313e
Jakub Kovář <kovar@netgrif.com>
2020-12-11 13:38:28 +0100

    NAE-1147 - Auto-generated navigation
    - User response object refactor

---
+++ src/main/java/com/netgrif/workflow/auth/web/responsebodies/ProcessRoleFactory.java
@@ -0,0 +1,30 @@
+package com.netgrif.workflow.auth.web.responsebodies;
+
+import com.netgrif.workflow.auth.domain.UserProcessRole;
+import com.netgrif.workflow.petrinet.domain.PetriNet;
+import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class ProcessRoleFactory implements IProcessRoleFactory {
+
+    @Autowired
+    private IPetriNetService petriNetService;
+
+    @Override
+    public ProcessRole getProcessRole(com.netgrif.workflow.petrinet.domain.roles.ProcessRole role, UserProcessRole userProcessRole, Locale locale) {
+        if (!role.getStringId().equals(userProcessRole.getRoleId())) {
+            throw new IllegalArgumentException("ProcessRole StringId and UserProcessRole roleId must match!");
+        }
+        ProcessRole result = new ProcessRole(role, locale);
+        PetriNet net = petriNetService.getPetriNet(userProcessRole.getNetId());
+        result.setNetStringId(net.getStringId());
+        result.setNetImportId(net.getImportId());
+        result.setNetVersion(net.getVersion().toString());
+        return result;
+    }
+
+}
