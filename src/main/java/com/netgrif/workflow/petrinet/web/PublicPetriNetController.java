cfce7c2d4ae6f78893fe795d06656d931d5861b3
Tomáš Kováčik <kovacik@netgrif.com>
2020-12-14 10:29:15 +0100

    NAE-1054 - Public view
    - add search endpoint

--- src/main/java/com/netgrif/workflow/petrinet/web/PublicPetriNetController.java
+++ src/main/java/com/netgrif/workflow/petrinet/web/PublicPetriNetController.java
@@ -1,8 +1,21 @@
 package com.netgrif.workflow.petrinet.web;
 
+import com.netgrif.workflow.auth.domain.LoggedUser;
 import com.netgrif.workflow.auth.service.interfaces.IUserService;
 import com.netgrif.workflow.petrinet.domain.version.StringToVersionConverter;
+import com.netgrif.workflow.petrinet.web.responsebodies.PetriNetReference;
+import com.netgrif.workflow.petrinet.web.responsebodies.PetriNetReferenceResourceAssembler;
 import com.netgrif.workflow.workflow.web.PublicAbstractController;
+import io.swagger.annotations.Authorization;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +28,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping({"/api/public"})
@@ -47,6 +63,17 @@ public class PublicPetriNetController extends PublicAbstractController {
     @ResponseBody
     public PetriNetReferenceResource getOne(@PathVariable("identifier") String identifier, @PathVariable("version") String version, Locale locale) {
         return new PetriNetReferenceResource(this.service.getReference(identifier, this.converter.convert(version), getAnonym(), locale));
+    }
+
+    @ApiOperation(value = "Search processes")
+    @RequestMapping(value = "/petrinet/search", method = POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaTypes.HAL_JSON_VALUE)
+    public PagedResources<PetriNetReferenceResource> searchPetriNets(@RequestBody Map<String, Object> criteria, Pageable pageable, PagedResourcesAssembler<PetriNetReference> assembler, Locale locale) {
+        Page<PetriNetReference> nets = service.search(criteria, getAnonym(), pageable, locale);
+        Link selfLink = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(PublicPetriNetController.class)
+                .searchPetriNets(criteria, pageable, assembler, locale)).withRel("search");
+        PagedResources<PetriNetReferenceResource> resources = assembler.toResource(nets, new PetriNetReferenceResourceAssembler(), selfLink);
+        PetriNetReferenceResourceAssembler.buildLinks(resources);
+        return resources;
     }
 
 
