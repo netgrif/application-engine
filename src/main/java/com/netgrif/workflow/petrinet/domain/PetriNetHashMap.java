94bfe4eb95c57361cf7b365e0136aa1ec3bb882e
Tomáš Kováčik <kovacik@netgrif.com>
2020-12-15 14:09:00 +0100

    NAE-1149 - Immediate Task Data
    - change according to PR

--- src/main/java/com/netgrif/workflow/petrinet/domain/PetriNetHashMap.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/PetriNetHashMap.java
@@ -4,22 +4,18 @@ import com.netgrif.workflow.petrinet.domain.dataset.Field;
 import org.bson.types.ObjectId;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class PetriNetHashMap extends HashMap<org.bson.types.ObjectId, com.netgrif.workflow.petrinet.domain.PetriNet> {
 
     @Override
     public PetriNet put(ObjectId key, PetriNet value) {
-        Map<String, Field> taskImmediate = new HashMap<>();
-
-        value.getTransitions().values().forEach(trans ->
-                trans.getImmediateData().forEach(fieldId -> {
-                    if (!taskImmediate.containsKey(fieldId)) {
-                        taskImmediate.put(fieldId, value.getDataSet().get(fieldId));
-                    }
-                }));
-
-        value.setTaskImmediateData(taskImmediate);
+         value.setTaskImmediateData(value.getTransitions().values().stream().map(Transition::getImmediateData)
+                .flatMap(List::stream).sorted().distinct()
+                .collect(Collectors.toMap(Function.identity(), field -> value.getDataSet().get(field))));
         return super.put(key, value);
     }
 }
