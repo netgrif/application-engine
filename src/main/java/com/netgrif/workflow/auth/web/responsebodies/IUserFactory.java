e1489df60ca8e4ad961c123be71a5afa6cefab06
Jakub Kovář <kovar@netgrif.com>
2020-12-17 07:18:10 +0100

    NAE-1147 - Auto-generated navigation
    - implemented some more PR feedback

--- src/main/java/com/netgrif/workflow/auth/web/responsebodies/IUserFactory.java
+++ src/main/java/com/netgrif/workflow/auth/web/responsebodies/IUserFactory.java
@@ -4,9 +4,15 @@ import java.util.Locale;
 
 public interface IUserFactory {
     /**
-     * Equivalent to calling getUser(user, locale, false)
+     * @param user the domain User object we want to send to frontend
+     * @param locale the locale for translations
+     * @return a full version of the user response object, that has all of its attributes set
      */
     User getUser(com.netgrif.workflow.auth.domain.User user, Locale locale);
 
-    User getUser(com.netgrif.workflow.auth.domain.User user, Locale locale, boolean small);
+    /**
+     * @param user the domain User object we want to send to frontend
+     * @return a small version of the user response object, that has its large attributes (roles, groups, authorities...) cleared
+     */
+    User getSmallUser(com.netgrif.workflow.auth.domain.User user);
 }
