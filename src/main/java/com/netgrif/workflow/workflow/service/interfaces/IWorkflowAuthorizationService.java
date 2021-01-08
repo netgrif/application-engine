6e024e5fff8de8f71191173626097f4c4e1f20e8
Tomáš Kováčik <kovacik@netgrif.com>
2021-01-08 09:11:36 +0100

    NAE-1119 - Constructor and destructor as process meta-data
    - add process role permisions, and autorizations on endpoints, also finish running actions

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IWorkflowAuthorizationService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IWorkflowAuthorizationService.java
@@ -1,10 +1,16 @@
 package com.netgrif.workflow.workflow.service.interfaces;
 
 import com.netgrif.workflow.auth.domain.LoggedUser;
+import com.netgrif.workflow.auth.domain.User;
+import com.netgrif.workflow.petrinet.domain.PetriNet;
+import com.netgrif.workflow.petrinet.domain.roles.ProcessRolePermission;
 import com.netgrif.workflow.workflow.domain.Case;
 
 public interface IWorkflowAuthorizationService {
 
-    boolean canCallDelete(LoggedUser user, Case aCase);
+    boolean canCallDelete(LoggedUser user, String caseId);
 
+    boolean canCallCreate(LoggedUser user, String netId);
+
+    boolean userHasAtLeastOneRolePermission(User user, PetriNet net, ProcessRolePermission... permissions);
 }
