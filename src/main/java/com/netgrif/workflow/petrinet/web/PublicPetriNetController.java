88c2e29a0f273f6b0cb7d9ef92492157fd82cb72
Milan Mladoniczky <mladoniczky@netgrif.com>
2021-01-25 17:19:30 +0100

    NAE-1054 - Public view
    - format endpoint mapping

--- src/main/java/com/netgrif/workflow/petrinet/web/PublicPetriNetController.java
+++ src/main/java/com/netgrif/workflow/petrinet/web/PublicPetriNetController.java
@@ -1,12 +1,14 @@
 package com.netgrif.workflow.petrinet.web;
 
-import com.netgrif.workflow.auth.domain.LoggedUser;
 import com.netgrif.workflow.auth.service.interfaces.IUserService;
 import com.netgrif.workflow.petrinet.domain.version.StringToVersionConverter;
+import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService;
 import com.netgrif.workflow.petrinet.web.responsebodies.PetriNetReference;
+import com.netgrif.workflow.petrinet.web.responsebodies.PetriNetReferenceResource;
 import com.netgrif.workflow.petrinet.web.responsebodies.PetriNetReferenceResourceAssembler;
 import com.netgrif.workflow.workflow.web.PublicAbstractController;
-import io.swagger.annotations.Authorization;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
@@ -15,13 +17,6 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import lombok.extern.slf4j.Slf4j;
-import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService;
-import com.netgrif.workflow.petrinet.web.responsebodies.PetriNetReferenceResource;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -53,13 +48,8 @@ public class PublicPetriNetController extends PublicAbstractController {
         return new PetriNetReferenceResource(IPetriNetService.transformToReference(this.service.getPetriNet(decodeUrl(id)), locale));
     }
 
-    @ApiOperation(
-            value = "Get process by identifier and version"
-    )
-    @GetMapping(
-            value = {"/petrinet/{identifier}/{version}"},
-            produces = {MediaTypes.HAL_JSON_VALUE}
-    )
+    @ApiOperation(value = "Get process by identifier and version")
+    @GetMapping(value = "/petrinet/{identifier}/{version}", produces = MediaTypes.HAL_JSON_VALUE)
     @ResponseBody
     public PetriNetReferenceResource getOne(@PathVariable("identifier") String identifier, @PathVariable("version") String version, Locale locale) {
         return new PetriNetReferenceResource(this.service.getReference(identifier, this.converter.convert(version), getAnonym(), locale));
