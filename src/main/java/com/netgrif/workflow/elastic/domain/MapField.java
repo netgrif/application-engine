caa68c2874dedb76a6f09e5e17ca65c5c77bd663
Jakub Kovář <kovar@netgrif.com>
2021-02-15 09:54:26 +0100

    NAE-1211 - Search in several data fields does not work as intended
    - reworked multichoice indexation to use array of values instead of concatenated content
    - added indexation for multichoice and enumeration map fields

---
+++ src/main/java/com/netgrif/workflow/elastic/domain/MapField.java
@@ -0,0 +1,37 @@
+package com.netgrif.workflow.elastic.domain;
+
+import com.netgrif.workflow.petrinet.domain.I18nString;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class MapField extends TextField {
+
+    @Field(type = FieldType.Keyword)
+    public String[] keyValue;
+
+    public MapField(Map.Entry<String, I18nString> valuePair) {
+        super(valuePair.getValue().toString());
+        this.keyValue = new String[1];
+        this.keyValue[0] = valuePair.getKey();
+    }
+
+    public MapField(List<Map.Entry<String, I18nString>> valuePairs) {
+        super(new String[0]);
+        this.keyValue = new String[valuePairs.size()];
+        String[] values = new String[valuePairs.size()];
+        for (int i = 0; i < valuePairs.size(); i++) {
+            keyValue[i] = valuePairs.get(i).getKey();
+            values[i] = valuePairs.get(i).getValue().toString();
+        }
+        this.textValue = values;
+    }
+}
