b76987def086d821ccda085960994dbcaf3dfb2f
Štefan Renczés <renczes@netgrif.com>
2021-02-16 11:31:06 +0100

    NAE-1236 - Anonym user persistance
    - deleted anonymous attribute from user
    - refactored PublicAuthenticationFilter

--- src/main/java/com/netgrif/workflow/auth/domain/AnonymousUser.java
+++ src/main/java/com/netgrif/workflow/auth/domain/AnonymousUser.java
@@ -24,4 +24,17 @@ public class AnonymousUser extends User{
     public AnonymousUser(String email, String password, String name, String surname) {
         super(email, password, name, surname);
     }
+
+    @Override
+    public LoggedUser transformToLoggedUser() {
+        LoggedUser loggedUser = new LoggedUser(this.getId(), this.getEmail(), this.getPassword(), this.getAuthorities());
+        loggedUser.setFullName(this.getFullName());
+        loggedUser.setAnonymous(true);
+        if (!this.getUserProcessRoles().isEmpty())
+            loggedUser.parseProcessRoles(this.getUserProcessRoles());
+        if (!this.getGroups().isEmpty())
+            loggedUser.parseGroups(this.getGroups());
+
+        return loggedUser;
+    }
 }
