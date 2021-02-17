3564f550d69db83fd64bdd7e7a1bc567a75ab47f
Jakub Kovář <kovar@netgrif.com>
2021-02-17 10:03:46 +0100

    NAE-1211 - Search in several data fields does not work as intended
    - translations indexation for map fields

--- src/main/java/com/netgrif/workflow/elastic/domain/MapField.java
+++ src/main/java/com/netgrif/workflow/elastic/domain/MapField.java
@@ -1,12 +1,13 @@
 package com.netgrif.workflow.elastic.domain;
 
-import com.netgrif.workflow.petrinet.domain.I18nString;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -18,19 +19,21 @@ public class MapField extends TextField {
     @Field(type = FieldType.Keyword)
     public String[] keyValue;
 
-    public MapField(Map.Entry<String, I18nString> valuePair) {
-        super(valuePair.getValue().toString());
+    public MapField(Map.Entry<String, Collection<String>> valuePair) {
+        super(valuePair.getValue().toArray(new String[0]));
         this.keyValue = new String[1];
         this.keyValue[0] = valuePair.getKey();
     }
 
-    public MapField(List<Map.Entry<String, I18nString>> valuePairs) {
-        super(new String[valuePairs.size()]);
+    public MapField(List<Map.Entry<String, Collection<String>>> valuePairs) {
+        super(new String[0]);
         this.keyValue = new String[valuePairs.size()];
+        List<String> values = new ArrayList<>();
         for (int i = 0; i < valuePairs.size(); i++) {
             keyValue[i] = valuePairs.get(i).getKey();
-            super.textValue[i] = valuePairs.get(i).getValue().toString();
-            super.fulltextValue[i] = valuePairs.get(i).getValue().toString();
+            values.addAll(valuePairs.get(i).getValue());
         }
+        this.textValue = values.toArray(new String[0]);
+        this.fulltextValue = values.toArray(new String[0]);
     }
 }
