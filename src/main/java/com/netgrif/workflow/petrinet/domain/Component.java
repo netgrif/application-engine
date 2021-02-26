c7179d79b7fbe5d68877b12bc023929b81755657
Tomáš Kováčik <kovacik@netgrif.com>
2021-02-26 08:45:47 +0100

    NAE-1248 - Icon enum
    -add new tag properties into component tag, and also add option_icons into this new tag for parsing icons for icon enum
    -add test net

--- src/main/java/com/netgrif/workflow/petrinet/domain/Component.java
+++ src/main/java/com/netgrif/workflow/petrinet/domain/Component.java
@@ -3,7 +3,9 @@ package com.netgrif.workflow.petrinet.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Component {
@@ -15,15 +17,25 @@ public class Component {
     @Setter
     private Map<String, String> properties;
 
+    @Getter
+    @Setter
+    private List<Icon> optionIcons;
+
     public Component(){}
 
     public Component(String name){
         this.name = name;
         this.properties = new HashMap<>();
+        this.optionIcons = new ArrayList<>();
     }
 
     public Component(String name, Map<String, String> properties){
         this(name);
         this.properties = properties;
+    }
+
+    public Component(String name, List<Icon> optionIcons){
+        this(name);
+        this.optionIcons = optionIcons;
     }
 }
