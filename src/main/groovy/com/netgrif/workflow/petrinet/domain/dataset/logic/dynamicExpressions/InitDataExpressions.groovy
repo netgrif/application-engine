24a2bd4dc5c05c9e7f9a7e8b3691a16cf3e653db
Bezak <bezak@netgrif.com>
2021-04-15 17:07:03 +0200

    NAE-58 - Dynamic Init values
    - refactor from regex to element attribute

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/dynamicExpressions/InitDataExpressions.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/dynamicExpressions/InitDataExpressions.groovy
@@ -3,27 +3,11 @@ package com.netgrif.workflow.petrinet.domain.dataset.logic.dynamicExpressions
 import com.netgrif.workflow.workflow.domain.Case
 import org.springframework.stereotype.Component
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-
 @Component
 class InitDataExpressions extends DataExpressions {
 
-    protected static final Pattern EXPRESSION_REGEX = Pattern.compile("\\\$\\{((.|\n)*)}")
-
     def compile(Case useCase, String expression) {
-        Matcher matcher = EXPRESSION_REGEX.matcher(expression)
-        matcher.find()
-        return compileClean(useCase, matcher.group(1))
-    }
-
-    def compileClean(Case useCase, String expression) {
         return compileClosure(useCase, expression)
-    }
-
-    static boolean containsDynamicExpression(String expression) {
-        Matcher matcher = EXPRESSION_REGEX.matcher(expression)
-        return matcher.find()
     }
 
 }
