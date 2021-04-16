7828563cee2b52614e7a9d659069d58bc0e66e8d
Bezak <bezak@netgrif.com>
2021-04-16 10:24:43 +0200

    NAE-58 - Dynamic Init values
    - refactor validations to groovy string instead of regex parsing

--- src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/dynamicExpressions/DataExpressions.groovy
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/dynamicExpressions/DataExpressions.groovy
@@ -4,41 +4,14 @@ import com.netgrif.workflow.workflow.domain.Case
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-
 @Component
 class DataExpressions {
-
-    /**
-     * pattern for dynamic evaluation is ${code}
-     */
-    public static final Pattern EXPRESSION_REGEX = Pattern.compile("\\\$\\{((.|\n)*?)}")
 
     @Autowired
     protected CaseFieldsExpressionRunner expressionRunner
 
     String compile(Case useCase, String expression) {
-        Matcher matcher = EXPRESSION_REGEX.matcher(expression)
-        while (matcher.find()) {
-            String result = compileClosure(useCase, matcher.group(1))
-            expression = expression.replace(matcher.group(), result)
-        }
-        return expression
-    }
-
-    static boolean containsDynamicExpression(String expression) {
-        Matcher matcher = EXPRESSION_REGEX.matcher(expression)
-        return matcher.find()
-    }
-
-    protected String compileClosure(Case useCase, String expression) {
-        return expressionRunner.run(useCase, expression)?.toString()
-    }
-
-    protected static String extractDynamicExpression(String expression) {
-        Matcher matcher = EXPRESSION_REGEX.matcher(expression)
-        return matcher.group()
+        return expressionRunner.run(useCase, "\"$expression\"" as String)?.toString()
     }
 
 }
