0ea1895835a7a55a47fad025eee4dc79d4f4fd3e
Bezak <bezak@netgrif.com>
2021-04-26 13:23:40 +0200

    NAE-58 - Dynamic Init values
    - dynamic choices and options

--- src/main/java/com/netgrif/workflow/workflow/service/InitValueExpressionEvaluator.java
+++ src/main/java/com/netgrif/workflow/workflow/service/InitValueExpressionEvaluator.java
@@ -1,11 +1,20 @@
 package com.netgrif.workflow.workflow.service;
 
+import com.netgrif.workflow.petrinet.domain.I18nString;
+import com.netgrif.workflow.petrinet.domain.dataset.ChoiceField;
 import com.netgrif.workflow.petrinet.domain.dataset.Field;
+import com.netgrif.workflow.petrinet.domain.dataset.MapOptionsField;
 import com.netgrif.workflow.petrinet.domain.dataset.logic.action.runner.CaseFieldsExpressionRunner;
 import com.netgrif.workflow.workflow.domain.Case;
 import com.netgrif.workflow.workflow.service.interfaces.IInitValueExpressionEvaluator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class InitValueExpressionEvaluator implements IInitValueExpressionEvaluator {
@@ -14,7 +23,36 @@ public class InitValueExpressionEvaluator implements IInitValueExpressionEvaluat
     private CaseFieldsExpressionRunner runner;
 
     @Override
-    public Object evaluate(Case useCase, Field defaultField) {
-        return runner.run(useCase, defaultField.getInitExpression());
+    public <T> T evaluate(Case useCase, Field<T> defaultField) {
+        return (T) evaluate(useCase, defaultField.getInitExpression());
+    }
+
+    @Override
+    public Map<String, I18nString> evaluateOptions(Case useCase, MapOptionsField<I18nString, ?> field) {
+        Object result = evaluate(useCase, field.getExpression());
+        if (!(result instanceof Map)) {
+            throw new IllegalArgumentException("[" + useCase.getStringId() + "] Dynamic options not an instance of Map: " + field.getImportId());
+        }
+        Map<String, Object> map = (Map) result;
+        if (map.values().stream().anyMatch(it -> !(it instanceof I18nString))) {
+            return map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, it -> new I18nString(it.getValue().toString())));
+        } else {
+            return (Map<String, I18nString>) result;
+        }
+    }
+
+    @Override
+    public Set<I18nString> evaluateChoices(Case useCase, ChoiceField<?> field) {
+        Object result = evaluate(useCase, field.getExpression());
+        if (!(result instanceof Collection)) {
+            throw new IllegalArgumentException("[" + useCase.getStringId() + "] Dynamic choices not an instance of Collection: " + field.getImportId());
+        }
+        Collection<Object> collection = (Collection) result;
+        return collection.stream().map(it -> (it instanceof I18nString) ? (I18nString) it : new I18nString(it.toString())).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Object evaluate(Case useCase, String expression) {
+        return runner.run(useCase, expression);
     }
 }
---
+++ src/test/groovy/com/netgrif/workflow/petrinet/domain/dataset/DynamicChoicesTest.groovy
@@ -0,0 +1,64 @@
+package com.netgrif.workflow.petrinet.domain.dataset
+
+import com.netgrif.workflow.TestHelper
+import com.netgrif.workflow.petrinet.domain.PetriNet
+import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService
+import com.netgrif.workflow.startup.ImportHelper
+import com.netgrif.workflow.startup.SuperCreator
+import com.netgrif.workflow.workflow.domain.repositories.CaseRepository
+import com.netgrif.workflow.workflow.service.interfaces.IDataService
+import com.netgrif.workflow.workflow.service.interfaces.ITaskService
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+
+@SpringBootTest
+@ActiveProfiles(["test"])
+@RunWith(SpringRunner.class)
+class DynamicChoicesTest {
+
+    @Autowired
+    private TestHelper testHelper;
+
+    @Autowired
+    private ImportHelper importHelper
+
+    @Autowired
+    private IPetriNetService petriNetService;
+
+    @Autowired
+    private SuperCreator superCreator;
+
+    @Autowired
+    private IDataService dataService;
+
+    @Autowired
+    private ITaskService taskService;
+
+    @Autowired
+    private CaseRepository caseRepository;
+
+    @Before
+    public void before() {
+        testHelper.truncateDbs();
+    }
+
+    @Test
+    void testDynamicEnum() {
+        Optional<PetriNet> optNet = petriNetService.importPetriNet(new FileInputStream("src/test/resources/petriNets/dynamic_choices.xml"), "major", superCreator.getLoggedSuper());
+
+        assert optNet.isPresent();
+        def net = optNet.get()
+
+        def aCase = importHelper.createCase("Case", net)
+
+        assert !aCase.dataSet["enumeration"].choices.empty
+        assert !aCase.dataSet["enumeration_map"].options.isEmpty()
+        assert !aCase.dataSet["multichoice"].choices.empty
+        assert !aCase.dataSet["multichoice_map"].options.isEmpty()
+    }
+}
