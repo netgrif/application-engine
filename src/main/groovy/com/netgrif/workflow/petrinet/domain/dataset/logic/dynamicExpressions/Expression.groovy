286924a62b64103198678a300ec88c436e661f1f
Bezak <bezak@netgrif.com>
2021-04-27 12:26:13 +0200

    NAE-1251 - Dynamic validations
    - refactor expressions into a new class
    - add caching

---
+++ src/main/groovy/com/netgrif/workflow/petrinet/domain/dataset/logic/dynamicExpressions/Expression.groovy
@@ -0,0 +1,40 @@
+package com.netgrif.workflow.petrinet.domain.dataset.logic.dynamicExpressions
+
+import com.querydsl.core.annotations.PropertyType
+import com.querydsl.core.annotations.QueryType
+import org.bson.types.ObjectId
+
+class Expression {
+
+    protected ObjectId _id
+
+    protected String definition
+
+    Expression() {
+        this._id = new ObjectId()
+    }
+
+    Expression(String definition) {
+        this()
+        this.definition = definition
+    }
+
+    String getStringId() {
+        return _id.toString()
+    }
+
+    String getDefinition() {
+        return definition
+    }
+
+    @Override
+    String toString() {
+        return "[$stringId] $definition"
+    }
+
+    @Override
+    @QueryType(PropertyType.NONE)
+    MetaClass getMetaClass() {
+        return this.metaClass
+    }
+}
