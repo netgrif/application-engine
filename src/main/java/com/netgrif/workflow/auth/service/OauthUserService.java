cf779fa206752a7e4f1eb94da38102721370c57c
Machac <machac@netgrif.com>
2021-05-06 17:52:30 +0200

    NAE-1302 - OpenID IDM integration
    - debug

--- src/main/java/com/netgrif/workflow/auth/service/OauthUserService.java
+++ src/main/java/com/netgrif/workflow/auth/service/OauthUserService.java
@@ -1,17 +1,17 @@
-package com.netgrif.workflow.auth.service;
-
-import com.netgrif.workflow.auth.domain.OauthUser;
-import com.netgrif.workflow.auth.domain.User;
-import com.netgrif.workflow.auth.service.interfaces.IOauthUserService;
-import com.netgrif.workflow.event.events.user.UserRegistrationEvent;
-
-public class OauthUserService extends UserService implements IOauthUserService {
-
-    @Override
-    public OauthUser saveNewOAuth(OauthUser user) {
-        addDefaultRole(user);
-        addDefaultAuthorities(user);
-        return userRepository.save(user);
-    }
-
-}
+//package com.netgrif.workflow.auth.service;
+//
+//import com.netgrif.workflow.auth.domain.OauthUser;
+//import com.netgrif.workflow.auth.domain.User;
+//import com.netgrif.workflow.auth.service.interfaces.IOauthUserService;
+//import com.netgrif.workflow.event.events.user.UserRegistrationEvent;
+//
+//public class OauthUserService extends UserService implements IOauthUserService {
+//
+//    @Override
+//    public OauthUser saveNewOAuth(OauthUser user) {
+//        addDefaultRole(user);
+//        addDefaultAuthorities(user);
+//        return userRepository.save(user);
+//    }
+//
+//}
