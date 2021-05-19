5383d19e6da7e46272a8e435905e938b4eb282ab
Bezak <bezak@netgrif.com>
2021-05-19 10:27:00 +0200

    NAE-1302 - OpenID IDM integration
    - fix oauth logged user
    - update configuration class

--- src/main/java/com/netgrif/workflow/configuration/OAuthUserServiceConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/OAuthUserServiceConfiguration.java
@@ -2,19 +2,28 @@ package com.netgrif.workflow.configuration;
 
 import com.netgrif.workflow.auth.service.interfaces.IUserService;
 import com.netgrif.workflow.oauth.domain.RemoteUserResource;
+import com.netgrif.workflow.oauth.service.OAuthUserMapper;
 import com.netgrif.workflow.oauth.service.OAuthUserService;
+import com.netgrif.workflow.oauth.service.interfaces.IOauthUserMapper;
 import com.netgrif.workflow.oauth.service.interfaces.IRemoteUserResourceService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnExpression("${nae.oauth.enabled}")
 public class OAuthUserServiceConfiguration {
 
     @Bean
-    @ConditionalOnExpression("${nae.oauth.enabled}")
     public IUserService userService(IRemoteUserResourceService<RemoteUserResource> remoteUserResourceService) {
         return new OAuthUserService(remoteUserResourceService);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public IOauthUserMapper oauthUserMapper() {
+        return new OAuthUserMapper();
     }
 
 }
