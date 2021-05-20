edb18f157bc52363d722523655055982495fafe6
Bezak <bezak@netgrif.com>
2021-05-20 11:08:47 +0200

    NAE-1302 - OpenID IDM integration
    - group loading based on "small" parameter
    - custom resource class

--- src/main/java/com/netgrif/workflow/configuration/UserServiceConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/UserServiceConfiguration.java
@@ -4,6 +4,8 @@ import com.netgrif.workflow.auth.service.RegistrationService;
 import com.netgrif.workflow.auth.service.UserService;
 import com.netgrif.workflow.auth.service.interfaces.IRegistrationService;
 import com.netgrif.workflow.auth.service.interfaces.IUserService;
+import com.netgrif.workflow.auth.web.responsebodies.IUserFactory;
+import com.netgrif.workflow.auth.web.responsebodies.UserFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,12 @@ public class UserServiceConfiguration {
     @ConditionalOnMissingBean
     public IRegistrationService registrationService() {
         return new RegistrationService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public IUserFactory userFactory() {
+        return new UserFactory();
     }
 
 }
