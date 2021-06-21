397fecefdb8398bf0e3033ad3b6af60b57233fd1
palajsamuel <palaj@netgrif.com>
2021-06-21 13:19:19 +0200

    NAE-1296 - Frontend actions
    - added message parsing to event outcomes
    - refactor of DataEvent
    - refactor of Importer, Field and Transition related to DataEvent refactor
    - petriflow_schema update

--- src/main/java/com/netgrif/workflow/workflow/domain/eventoutcomes/dataoutcomes/SetDataEventOutcome.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/eventoutcomes/dataoutcomes/SetDataEventOutcome.java
@@ -2,7 +2,6 @@ package com.netgrif.workflow.workflow.domain.eventoutcomes.dataoutcomes;
 
 import com.netgrif.workflow.petrinet.domain.dataset.logic.ChangedFieldsTree;
 import com.netgrif.workflow.workflow.domain.eventoutcomes.EventOutcome;
-import com.netgrif.workflow.workflow.domain.eventoutcomes.LocalisedEventOutcome;
 import com.netgrif.workflow.workflow.domain.eventoutcomes.dataoutcomes.localised.LocalisedSetDataEventOutcome;
 import lombok.Data;
 
