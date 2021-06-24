a06fc59d02dc37f267b4afcc7383ad666d94a6a2
Machac <machac@netgrif.com>
2021-06-24 09:55:54 +0200

    - PSC - Invite user - Fix

--- src/main/java/com/netgrif/workflow/orgstructure/web/responsebodies/GroupsResource.java
+++ src/main/java/com/netgrif/workflow/orgstructure/web/responsebodies/GroupsResource.java
@@ -1,20 +1,19 @@
-//package com.netgrif.workflow.orgstructure.web.responsebodies;
-//
-//import com.netgrif.workflow.orgstructure.domain.Group;
-//import com.netgrif.workflow.orgstructure.web.GroupController;
-//import org.springframework.hateoas.CollectionModel;
-//import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-//
-//
-//public class GroupsResource extends CollectionModel<Group> {
-//
-//    public GroupsResource(Iterable<Group> content) {
-//        super(content);
-//        buildLinks();
-//    }
-//
-//    private void buildLinks(){
-//        add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(GroupController.class)
-//                .getAllGroups()).withSelfRel());
-//    }
-//}
+package com.netgrif.workflow.orgstructure.web.responsebodies;
+
+import com.netgrif.workflow.orgstructure.web.GroupController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+
+public class GroupsResource extends CollectionModel<Group> {
+
+    public GroupsResource(Iterable<Group> content) {
+        super(content);
+        buildLinks();
+    }
+
+    private void buildLinks(){
+        add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(GroupController.class)
+                .getAllGroups()).withSelfRel());
+    }
+}
