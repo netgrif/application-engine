a61cf135aa14fa9d982663134a9868263b054655
Jakub Kovář <kovar@netgrif.com>
2021-07-14 11:04:22 +0200

    NAE-1392 - Implement GroovyShell Factory
    - implemented GroovyShellFactory
    - replaced all GroovyShell constructions with factory

---
+++ src/main/java/com/netgrif/workflow/configuration/groovy/GroovyShellFactory.java
@@ -0,0 +1,18 @@
+package com.netgrif.workflow.configuration.groovy;
+
+import groovy.lang.GroovyShell;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroovyShellFactory implements IGroovyShellFactory {
+
+    @Autowired
+    CompilerConfiguration configuration;
+
+    @Override
+    public GroovyShell getGroovyShell() {
+        return new GroovyShell(this.getClass().getClassLoader(), this.configuration);
+    }
+}
---
+++ src/main/java/com/netgrif/workflow/configuration/groovy/IGroovyShellFactory.java
@@ -0,0 +1,7 @@
+package com.netgrif.workflow.configuration.groovy;
+
+import groovy.lang.GroovyShell;
+
+public interface IGroovyShellFactory {
+    GroovyShell getGroovyShell();
+}
