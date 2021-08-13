980f495ec4d815b757428db585afcae9454e2e20
kozak <kozak@netgrif.com>
2021-08-13 14:39:38 +0200

    NAE-1407 Static functions
    - properties and test refactoring

--- src/main/java/com/netgrif/workflow/workflow/service/FieldActionsCacheService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/FieldActionsCacheService.java
@@ -1,5 +1,6 @@
 package com.netgrif.workflow.workflow.service;
 
+import com.netgrif.workflow.configuration.properties.FieldActionsCacheProperties;
 import com.netgrif.workflow.elastic.service.executors.MaxSizeHashMap;
 import com.netgrif.workflow.event.IGroovyShellFactory;
 import com.netgrif.workflow.petrinet.domain.Function;
@@ -13,7 +14,6 @@ import groovy.lang.Closure;
 import groovy.lang.GroovyShell;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
@@ -27,9 +27,9 @@ import java.util.stream.Collectors;
 @Slf4j
 public class FieldActionsCacheService implements IFieldActionsCacheService {
 
-    private final Long actionCacheMaxSize;
-    private final Long functionCacheMaxSize;
-    private final Long namespaceFunctionCacheMaxSize;
+    @Autowired
+    private FieldActionsCacheProperties properties;
+
     private IPetriNetService petriNetService;
 
     private Map<String, Closure> actionsCache;
@@ -37,16 +37,10 @@ public class FieldActionsCacheService implements IFieldActionsCacheService {
     private Map<String, CachedFunction> functionsCache;
     private final GroovyShell shell;
 
-    public FieldActionsCacheService(@Value("${nae.field-runner.actions.cache-size}") Long actionCacheMaxSize,
-                                    @Value("${nae.field-runner.functions.namespace.cache-size}") Long namespaceFunctionCacheMaxSize,
-                                    @Value("${nae.field-runner.functions.cache-size}") Long functionCacheMaxSize,
-                                    IGroovyShellFactory shellFactory) {
-        this.actionCacheMaxSize = actionCacheMaxSize;
-        this.functionCacheMaxSize = functionCacheMaxSize;
-        this.namespaceFunctionCacheMaxSize = namespaceFunctionCacheMaxSize;
-        this.actionsCache = new MaxSizeHashMap<>(actionCacheMaxSize);
-        this.functionsCache = new MaxSizeHashMap<>(functionCacheMaxSize);
-        this.namespaceFunctionsCache = new MaxSizeHashMap<>(namespaceFunctionCacheMaxSize);
+    public FieldActionsCacheService(IGroovyShellFactory shellFactory) {
+        this.actionsCache = new MaxSizeHashMap<>(properties.getActions());
+        this.functionsCache = new MaxSizeHashMap<>(properties.getFunctions());
+        this.namespaceFunctionsCache = new MaxSizeHashMap<>(properties.getNamespaceFunctions());
         this.shell = shellFactory.getGroovyShell();
     }
 
@@ -145,16 +139,16 @@ public class FieldActionsCacheService implements IFieldActionsCacheService {
 
     @Override
     public void clearActionCache() {
-        this.actionsCache = new MaxSizeHashMap<>(actionCacheMaxSize);
+        this.actionsCache = new MaxSizeHashMap<>(properties.getActions());
     }
 
     @Override
     public void clearNamespaceFunctionCache() {
-        this.namespaceFunctionsCache = new MaxSizeHashMap<>(namespaceFunctionCacheMaxSize);
+        this.namespaceFunctionsCache = new MaxSizeHashMap<>(properties.getNamespaceFunctions());
     }
 
     @Override
     public void clearFunctionCache() {
-        this.functionsCache = new MaxSizeHashMap<>(functionCacheMaxSize);
+        this.functionsCache = new MaxSizeHashMap<>(properties.getFunctions());
     }
 }
