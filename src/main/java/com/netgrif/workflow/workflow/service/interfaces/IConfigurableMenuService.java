8ed4ae6e1e1c7c0a2f0c585f20106afb95d8c7dc
Samuel Spisak <spisak@netgrif.com>
2021-08-16 16:56:31 +0200

    NAE-1402 - Configurable group navigation with role constraints
    - added functionality for banning specific roles from view

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IConfigurableMenuService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IConfigurableMenuService.java
@@ -1,18 +1,15 @@
 package com.netgrif.workflow.workflow.service.interfaces;
 
 import com.netgrif.workflow.petrinet.domain.I18nString;
-import com.netgrif.workflow.petrinet.domain.PetriNet;
 import com.netgrif.workflow.petrinet.domain.dataset.EnumerationMapField;
 import com.netgrif.workflow.petrinet.domain.dataset.MultichoiceMapField;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 
 public interface IConfigurableMenuService {
 
     Map<String, I18nString> getNetsByAuthor(Long authorId);
-    Map<String, I18nString> getNetRoles (EnumerationMapField field, String value,MultichoiceMapField addedRoleIds);
+    Map<String, I18nString> getAvailableRolesFromNet (EnumerationMapField processField,MultichoiceMapField permittedRoles, MultichoiceMapField bannedRoles);
     Map<String, I18nString> addSelectedRoles(MultichoiceMapField selected, EnumerationMapField netField, MultichoiceMapField roles);
     Map<String, I18nString> removeSelectedRoles(MultichoiceMapField addedRoles);
 
