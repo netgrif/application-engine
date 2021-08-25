054e0f79c120f7c697ce42853d04d043f8ebbc62
Machac <machac@netgrif.com>
2021-08-25 09:42:02 +0200

    NAE-1401 - Configurable LDAP integration
    - Configuration

--- src/main/java/com/netgrif/workflow/ldap/domain/LdapUserRef.java
+++ src/main/java/com/netgrif/workflow/ldap/domain/LdapUserRef.java
@@ -2,6 +2,7 @@ package com.netgrif.workflow.ldap.domain;
 
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ldap.odm.annotations.Attribute;
 import org.springframework.ldap.odm.annotations.Entry;
 import org.springframework.ldap.odm.annotations.Id;
@@ -11,7 +12,6 @@ import java.util.List;
 
 
 @Entry(
-        base = "ou=people",
         objectClasses = {"inetOrgPerson", "person"}
 )
 
@@ -22,44 +22,44 @@ public class LdapUserRef {
     @Id
     private Name dn;
 
-
-    @Attribute(name = "cn")
+    @Value("${nae.ldap.mapCn}")
+//    @Attribute()
     private String cn;
 
 
-    @Attribute(name = "uid")
+    @Attribute(name = "${nae.ldap.uid}")
     private String uid;
 
 
-    @Attribute(name = "uid")
+    @Attribute(name = "${nae.ldap.mapMail}")
     private String mail;
 
 
-    @Attribute(name = "givenName")
+    @Attribute(name = "${nae.ldap.mapFirstName}")
     private String firstName;
 
 
-    @Attribute(name = "userPassword")
+    @Attribute(name = "${nae.ldap.mapUserPassword}")
     private byte[] password;
 
 
-    @Attribute(name = "sn")
+    @Attribute(name = "${nae.ldap.mapSurname}")
     private String surname;
 
 
-    @Attribute(name = "displayName")
+    @Attribute(name = "${nae.ldap.mapDisplayName}")
     private String fullName;
 
 
-    @Attribute(name = "homeDirectory")
+    @Attribute(name = "${nae.ldap.mapHomeDirectory}")
     private String homeDirectory;
 
 
-    @Attribute(name = "objectClass")
+    @Attribute(name = "${nae.ldap.mapObjectClass}")
     private List<String> objectClass;
 
 
-    @Attribute(name = "MemberOf")
+    @Attribute(name = "${nae.ldap.mapMemberOf}")
     private List<String> memberOf;
 
 
