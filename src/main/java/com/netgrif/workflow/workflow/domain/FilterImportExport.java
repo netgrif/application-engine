27694a478c9df4a3e5b840f156c442e4568854dd
Jozef Daxner <daxner@netgrif.com>
2021-08-25 12:57:22 +0200

    NAE-1406-Filter import/export
    - finish export of filters

---
+++ src/main/java/com/netgrif/workflow/workflow/domain/FilterImportExport.java
@@ -0,0 +1,23 @@
+package com.netgrif.workflow.workflow.domain;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.netgrif.workflow.petrinet.domain.I18nString;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "filter")
+public class FilterImportExport {
+
+    protected String title;
+    protected I18nString filterName;
+    protected String filterValue;
+    protected Map<String, Object> filterMetadata;
+    protected String visibility;
+    protected String type;
+}
