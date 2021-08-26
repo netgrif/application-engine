763581e174e8e4f8f10fe098dd1f506e1cb99217
Machac <machac@netgrif.com>
2021-08-26 10:23:50 +0200

    NAE-1401 - Configurable LDAP integration
    - Configuration

--- src/main/java/com/netgrif/workflow/ldap/service/LdapUserRefService.java
+++ src/main/java/com/netgrif/workflow/ldap/service/LdapUserRefService.java
@@ -3,32 +3,24 @@ package com.netgrif.workflow.ldap.service;
 
 import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.auth.domain.UserState;
-import com.netgrif.workflow.auth.service.interfaces.IAuthorityService;
 import com.netgrif.workflow.auth.service.interfaces.IUserService;
+import com.netgrif.workflow.configuration.properties.NaeLdapProperties;
 import com.netgrif.workflow.event.events.user.UserRegistrationEvent;
+import com.netgrif.workflow.ldap.domain.LdapConfiguration;
 import com.netgrif.workflow.ldap.domain.LdapUser;
 import com.netgrif.workflow.ldap.domain.LdapUserRef;
 import com.netgrif.workflow.ldap.domain.repository.LdapUserRefRepository;
-import com.netgrif.workflow.ldap.service.interfaces.ILdapGroupRefService;
 import com.netgrif.workflow.ldap.service.interfaces.ILdapUserRefService;
 import com.netgrif.workflow.orgstructure.groups.config.GroupConfigurationProperties;
 import com.netgrif.workflow.orgstructure.groups.interfaces.INextGroupService;
-import com.netgrif.workflow.petrinet.service.interfaces.IPetriNetService;
-import com.netgrif.workflow.startup.ImportHelper;
-import com.netgrif.workflow.workflow.service.interfaces.IWorkflowService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-import java.util.List;
-import java.util.Set;
+import javax.naming.Name;
 
 @Service
 @Slf4j
@@ -52,14 +44,19 @@ public class LdapUserRefService implements ILdapUserRefService {
     @Autowired
     private GroupConfigurationProperties groupProperties;
 
+    @Autowired
+    private NaeLdapProperties ldapProperties;
+
 
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private LdapConfiguration ldapUserConfiguration;
 
     @Override
     public User createUser(LdapUserRef ldapUserRef) {
-        LdapUser ldapUser = new LdapUser(ldapUserRef.getDn().toString(), ldapUserRef.getCn(), ldapUserRef.getUid(), ldapUserRef.getHomeDirectory(), ldapUserRef.getMail(), "password", ldapUserRef.getFirstName(), ldapUserRef.getSurname());
+        LdapUser ldapUser = new LdapUser(ldapUserRef.getDn().toString(), ldapUserRef.getCn(), ldapUserRef.getUid(), ldapUserRef.getHomeDirectory(), ldapUserRef.getMail(), "password", ldapUserRef.getFirstName(), ldapUserRef.getSurname(), ldapUserRef.getTelNumber());
         ldapUser.setState(UserState.ACTIVE);
         User savedUser = ldapUserService.saveNew(ldapUser);
         ldapUser.setNextGroups(this.groupService.getAllGroupsOfUser(savedUser));
@@ -75,6 +72,64 @@ public class LdapUserRefService implements ILdapUserRefService {
         publisher.publishEvent(new UserRegistrationEvent(savedUser));
 
         return ldapUserService.save(ldapUser);
+    }
+
+    @Override
+    public LdapUserRef findById(Name id) {
+        DirContextOperations context
+                = ldapUserConfiguration.ldapTemplate().lookupContext(id);
+        LdapUserRef user = new LdapUserRef();
+        user.setDn(context.getDn());
+        user.setCn(verificationData(context, ldapProperties.getMapCn()));
+        user.setUid(verificationData(context, ldapProperties.getMapUid()));
+        user.setMail(verificationData(context, ldapProperties.getMapMail()));
+        user.setFirstName(verificationData(context, ldapProperties.getMapFirstName()));
+        user.setSurname(verificationData(context, ldapProperties.getMapSurname()));
+        user.setFullName(verificationData(context, ldapProperties.getMapDisplayName()));
+        user.setTelNumber(verificationData(context, ldapProperties.getMapTelNumber()));
+        user.setHomeDirectory(verificationData(context, ldapProperties.getMapHomeDirectory()));
+
+        return user;
+    }
+
+    private String verificationData(DirContextOperations context, String attribute) {
+        if (attribute != null) {
+            if (!attribute.equals("")) {
+                try {
+                    return context.getStringAttribute(attribute);
+                } catch (Exception e) {
+                    log.warn("");
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public User updateById(Name id, User savedUser) {
+        DirContextOperations context = ldapUserConfiguration.ldapTemplate().lookupContext(id);
+        LdapUserRef user = new LdapUserRef();
+        user.setCn(verificationData(context, ldapProperties.getMapCn()));
+        user.setUid(verificationData(context, ldapProperties.getMapUid()));
+        user.setMail(verificationData(context, ldapProperties.getMapMail()));
+        user.setFirstName(verificationData(context, ldapProperties.getMapFirstName()));
+        user.setSurname(verificationData(context, ldapProperties.getMapSurname()));
+        user.setTelNumber(verificationData(context, ldapProperties.getMapTelNumber()));
+        user.setFullName(verificationData(context, ldapProperties.getMapDisplayName()));
+        user.setHomeDirectory(verificationData(context, ldapProperties.getMapHomeDirectory()));
+        LdapUser ldapUser = (LdapUser) savedUser;
+
+
+        ldapUser.setCommonName(user.getCn());
+        ldapUser.setUid(user.getUid());
+        ldapUser.setHomeDirectory(user.getHomeDirectory());
+        ldapUser.setEmail(user.getMail());
+        ldapUser.setTelNumber(user.getTelNumber());
+        ldapUser.setName(user.getFirstName());
+        ldapUser.setSurname(user.getSurname());
+        return ldapUserService.save(savedUser);
     }
 
 
--- src/main/java/com/netgrif/workflow/ldap/service/interfaces/ILdapUserRefService.java
+++ src/main/java/com/netgrif/workflow/ldap/service/interfaces/ILdapUserRefService.java
@@ -4,12 +4,18 @@ package com.netgrif.workflow.ldap.service.interfaces;
 import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.ldap.domain.LdapUserRef;
 
+import javax.naming.Name;
+
 
 public interface ILdapUserRefService {
 
 
     User createUser(LdapUserRef ldapUser);
 
+
+    User updateById(Name id, User savedUser);
+
+    LdapUserRef findById(Name id);
 
     LdapUserRef findUserByDn(String dn);
 
