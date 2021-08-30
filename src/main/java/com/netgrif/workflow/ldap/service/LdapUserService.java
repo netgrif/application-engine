b82859be60ef66bca63ac3295a6e61c34cbe33a9
Jozef Macháč <machac@netgrif.com>
2021-08-30 14:54:30 +0200

    NAE-1424 - LDAP integration security vulnerability
    - fix bad annotation

--- src/main/java/com/netgrif/workflow/ldap/service/LdapUserService.java
+++ src/main/java/com/netgrif/workflow/ldap/service/LdapUserService.java
@@ -7,10 +7,12 @@ import com.netgrif.workflow.ldap.domain.LdapUser;
 import com.netgrif.workflow.ldap.domain.repository.LdapUserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
 import javax.naming.Name;
 
 @Slf4j
+@ConditionalOnExpression("${nae.ldap.enabled}")
 public class LdapUserService extends UserService {
 
     @Autowired
