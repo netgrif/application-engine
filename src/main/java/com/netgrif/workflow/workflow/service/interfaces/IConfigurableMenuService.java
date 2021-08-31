be06d75aba6304a6205989e62198c7ec34198583
Samuel Spisak <spisak@netgrif.com>
2021-08-31 11:36:13 +0200

    NAE-1402 - Configurable group navigation with role constraints
    - fixes based on PR comments

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IConfigurableMenuService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IConfigurableMenuService.java
@@ -1,14 +1,17 @@
 package com.netgrif.workflow.workflow.service.interfaces;
 
+import com.netgrif.workflow.auth.domain.LoggedUser;
+import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.petrinet.domain.I18nString;
 import com.netgrif.workflow.petrinet.domain.dataset.EnumerationMapField;
 import com.netgrif.workflow.petrinet.domain.dataset.MultichoiceMapField;
 
+import java.util.Locale;
 import java.util.Map;
 
 public interface IConfigurableMenuService {
 
-    Map<String, I18nString> getNetsByAuthor(Long authorId);
+    Map<String, I18nString> getNetsByAuthor(User author, Locale locale);
     Map<String, I18nString> getAvailableRolesFromNet (EnumerationMapField processField,MultichoiceMapField permittedRoles, MultichoiceMapField bannedRoles);
     Map<String, I18nString> addSelectedRoles(MultichoiceMapField selected, EnumerationMapField netField, MultichoiceMapField roles);
     Map<String, I18nString> removeSelectedRoles(MultichoiceMapField addedRoles);
