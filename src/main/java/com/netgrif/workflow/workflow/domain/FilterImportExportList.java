0511cea14f4c36b286c9198db1072b438a8a2be2
Jozef Daxner <daxner@netgrif.com>
2021-09-02 12:40:42 +0200

    NAE-1406-Filter import/export
    - create class to serialize filter metadata object

--- src/main/java/com/netgrif/workflow/workflow/domain/FilterImportExportList.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/FilterImportExportList.java
@@ -1,8 +1,8 @@
 package com.netgrif.workflow.workflow.domain;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,8 +15,8 @@ import java.util.List;
 @JacksonXmlRootElement(localName = "filterList")
 public class FilterImportExportList {
 
-    @JsonProperty("filter")
-    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "filter")
+//    @JacksonXmlElementWrapper(useWrapping = false)
     protected List<FilterImportExport> filters;
 
     public FilterImportExportList() {
