6426e9c8e9fa118d418def7f048072709c5991a1
Samuel Spisak <spisak@netgrif.com>
2021-09-03 15:52:47 +0200

    NAE-1417 - Menu import/export
    - export implementation

---
+++ src/main/java/com/netgrif/workflow/workflow/domain/MenuEntry.java
@@ -0,0 +1,36 @@
+package com.netgrif.workflow.workflow.domain;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.netgrif.workflow.importer.model.MenuItemRole;
+import com.netgrif.workflow.petrinet.domain.I18nString;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "entry_name",
+        "filterId",
+        "iconIdentifier",
+        "menuItemRoleList",
+})
+public class MenuEntry {
+
+    @XmlElement(required = true)
+    protected String entry_name;
+    @XmlElement
+    protected String filterId;
+    @XmlElement
+    protected String iconIdentifier;
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "entryRole")
+    protected List<MenuItemRole> menuItemRoleList;
+    @JacksonXmlProperty(isAttribute = true)
+    protected Boolean useIcon;
+}
