d1ed12ddcfab7b18104e6e7959c7bc76fa03e851
Machac <machac@netgrif.com>
2021-09-08 16:32:01 +0200

    incorporated comments

--- src/main/java/com/netgrif/workflow/auth/service/interfaces/IUserService.java
+++ src/main/java/com/netgrif/workflow/auth/service/interfaces/IUserService.java
@@ -14,6 +14,58 @@ import java.util.Set;
 
 public interface IUserService {
 
+    IUser findByAuth(Authentication auth);
 
+    IUser save(IUser user);
+
+    IUser saveNew(IUser user);
+
+    AnonymousUser saveNewAnonymous(AnonymousUser user);
+
+    IUser update(IUser user, UpdateUserRequest updates);
+
+    IUser findById(String id, boolean small);
+
+    IUser resolveById(String id, boolean small);
+
+    IUser findByEmail(String email, boolean small);
+
+    IUser findAnonymousByEmail(String email, boolean small);
+
+    List<IUser> findAll(boolean small);
+
+    Page<IUser> findAllCoMembers(LoggedUser loggedUser, boolean small, Pageable pageable);
+
+    List<IUser> findAllByIds(Set<String> ids, boolean small);
+
+    Page<IUser> findAllActiveByProcessRoles(Set<String> roleIds, boolean small, Pageable pageable);
+
+    void addDefaultRole(IUser user);
+
+    List<IUser> findAllByProcessRoles(Set<String> roleIds, boolean small);
+
+    void addDefaultAuthorities(IUser user);
+
+    void assignAuthority(String userId, String authorityId);
+
+    IUser getLoggedOrSystem();
+
+    IUser getLoggedUser();
+
+    IUser getSystem();
+
+    LoggedUser getAnonymousLogged();
+
+    IUser addRole(IUser user, String roleStringId);
+
+    Page<IUser> searchAllCoMembers(String query, LoggedUser principal, Boolean small, Pageable pageable);
+
+    IUser removeRole(IUser user, String roleStringId);
+
+    void deleteUser(IUser user);
+
+    Page<IUser> searchAllCoMembers(String query, List<ObjectId> roles, List<ObjectId> negateRoleIds, LoggedUser principal, Boolean small, Pageable pageable);
+
+    IUser createSystemUser();
 
 }
