830ad1b4eaca963ade812bb616efb2b9cfc6d9bf
Machac <machac@netgrif.com>
2021-09-08 14:33:39 +0200

    incorporated comments

--- src/main/groovy/com/netgrif/workflow/startup/SystemUserRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/SystemUserRunner.groovy
@@ -2,6 +2,8 @@ package com.netgrif.workflow.startup
 
 import com.netgrif.workflow.auth.domain.IUser
 import com.netgrif.workflow.auth.domain.LoggedUser
+import com.netgrif.workflow.auth.domain.User
+import com.netgrif.workflow.auth.domain.UserState
 import com.netgrif.workflow.auth.service.interfaces.IUserService
 import com.netgrif.workflow.configuration.properties.NaeOAuthProperties
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,29 +26,20 @@ class SystemUserRunner extends AbstractOrderedCommandLineRunner {
 
     private IUser systemUser
 
-//    @Override
-//    void run(String... strings) throws Exception {
-//        this.systemUser = service.findByEmail(SYSTEM_USER_EMAIL, false)
-//        if (this.systemUser == null) {
-//            User system = new User(
-//                    email: SYSTEM_USER_EMAIL,
-//                    name: SYSTEM_USER_NAME,
-//                    surname: SYSTEM_USER_SURNAME,
-//                    password: "as3f4as6d5f465d7s65d74f3tgsd5rts3d5f7gdf65tz7f65tz7f3t5zf5r3z7t",
-//                    state: UserState.ACTIVE
-//            )
-//            service.save(system)
-//            this.systemUser = system
-//        }
-//    }
-//
-//    LoggedUser getLoggedSystem() {
-//        return this.systemUser.transformToLoggedUser()
-//    }
-
     @Override
     void run(String... strings) throws Exception {
-        this.systemUser = createSystemUser()
+        this.systemUser = service.findByEmail(SYSTEM_USER_EMAIL, false)
+        if (this.systemUser == null) {
+            User system = new User(
+                    email: SYSTEM_USER_EMAIL,
+                    name: SYSTEM_USER_NAME,
+                    surname: SYSTEM_USER_SURNAME,
+                    password: "as3f4as6d5f465d7s65d74f3tgsd5rts3d5f7gdf65tz7f65tz7f3t5zf5r3z7t",
+                    state: UserState.ACTIVE
+            )
+            service.save(system)
+            this.systemUser = system
+        }
     }
 
     IUser createSystemUser() {
