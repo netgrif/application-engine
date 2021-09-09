e833c6d2f91ec5b0b6a2b79731fe0b9a9046e8b1
Stefan Renczes <renczes@netgrif.com>
2021-09-09 16:13:30 +0200

    NAE-1420 - Case view permission query with no users
    - refactored attributes

--- src/main/java/com/netgrif/workflow/workflow/service/CaseSearchService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/CaseSearchService.java
@@ -67,9 +67,6 @@ public class CaseSearchService extends MongoSearchService<Case> {
         if (requestQuery.containsKey(FULLTEXT)) {
             builder.and(fullText(requestQuery.getOrDefault(PETRINET, null), (String) requestQuery.get(FULLTEXT)));
         }
-        if (requestQuery.containsKey(ROLE)) {
-            builder.and(role(requestQuery.get(ROLE)));
-        }
         if (requestQuery.containsKey(DATA)) {
             builder.and(data(requestQuery.get(DATA)));
         }
@@ -85,7 +82,7 @@ public class CaseSearchService extends MongoSearchService<Case> {
             }
         }
 
-        BooleanBuilder constraints = new BooleanBuilder(role(new ArrayList<>(user.getProcessRoles())));
+        BooleanBuilder constraints = new BooleanBuilder(role(user.getProcessRoles()));
         constraints.or(buildViewUsersQueryConstraint(user));
 
         BooleanBuilder negativeConstraints = new BooleanBuilder(buildNegativeRolesQueryConstraint(user));
@@ -157,11 +154,8 @@ public class CaseSearchService extends MongoSearchService<Case> {
         return null;
     }
 
-    public Predicate role(Object o) {
-        if (o instanceof ArrayList) {
-            return QCase.case$.enabledRoles.any().in((ArrayList<String>) o);
-        }
-        return null;
+    public Predicate role(Set<String> o) {
+            return QCase.case$.viewRoles.any().in(o);
     }
 
     private static BooleanExpression authorObject(HashMap<String, Object> query) {
