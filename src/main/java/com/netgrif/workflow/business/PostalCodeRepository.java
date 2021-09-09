c477deb4dd9fa280b8ab9905c4f43704470f42f7
Machac <machac@netgrif.com>
2021-09-09 13:57:02 +0200

    incorporated comments

--- src/main/java/com/netgrif/workflow/business/PostalCodeRepository.java
+++ src/main/java/com/netgrif/workflow/business/PostalCodeRepository.java
@@ -1,12 +1,13 @@
 package com.netgrif.workflow.business;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostalCodeRepository extends MongoRepository<PostalCode, String> {
+public interface PostalCodeRepository extends MongoRepository<PostalCode, String>, QuerydslPredicateExecutor<PostalCode> {
 
     List<PostalCode> findAllByCode(String code);
 
