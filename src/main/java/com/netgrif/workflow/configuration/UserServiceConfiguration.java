8dde327d913839ccd8f673510b3a8690017c71d4
Machac <machac@netgrif.com>
2021-09-20 13:27:05 +0200

    merge fix

--- src/main/java/com/netgrif/workflow/configuration/UserServiceConfiguration.java
+++ src/main/java/com/netgrif/workflow/configuration/UserServiceConfiguration.java
@@ -1,17 +1,12 @@
 package com.netgrif.workflow.configuration;
 
 import com.netgrif.workflow.auth.service.RegistrationService;
+import com.netgrif.workflow.auth.service.UserDetailsServiceImpl;
 import com.netgrif.workflow.auth.service.UserService;
 import com.netgrif.workflow.auth.service.interfaces.IRegistrationService;
 import com.netgrif.workflow.auth.service.interfaces.IUserService;
 import com.netgrif.workflow.auth.web.responsebodies.IUserFactory;
 import com.netgrif.workflow.auth.web.responsebodies.UserFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import com.netgrif.workflow.auth.service.UserDetailsServiceImpl;
-import com.netgrif.workflow.auth.service.UserService;
-import com.netgrif.workflow.auth.service.interfaces.IUserService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
