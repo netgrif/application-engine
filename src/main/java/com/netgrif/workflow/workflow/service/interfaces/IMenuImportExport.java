ecd82a6579b0d220a057c19568561fa228605a2e
Samuel Spisak <spisak@netgrif.com>
2021-09-21 21:15:18 +0200

    NAE-1417 -  Menu import/export
    - implementacia importu:
    - vytvaranie novych entries a mazanie starych podla menu Identifier fieldu

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IMenuImportExport.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IMenuImportExport.java
@@ -1,8 +1,11 @@
 package com.netgrif.workflow.workflow.service.interfaces;
 
 import com.netgrif.workflow.auth.domain.User;
+import com.netgrif.workflow.petrinet.domain.dataset.FileField;
 import com.netgrif.workflow.petrinet.domain.dataset.FileFieldValue;
 import com.netgrif.workflow.workflow.domain.Case;
+import com.netgrif.workflow.workflow.domain.MenuEntry;
+import com.netgrif.workflow.workflow.domain.MenuList;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,5 +14,7 @@ public interface IMenuImportExport {
 
     void createMenuImport(User author);
     void createMenuExport(User author);
-    FileFieldValue exportMenu(List<Case> menuItemCaseIds) throws IOException;
+    FileFieldValue exportMenu(List<Case> menuItemCaseIds, String menuIdentifier, String groupId, FileField fileField) throws IOException;
+    List<String> importMenu(List<Case> menuItemCases, FileFieldValue ffv, String groupCaseId) throws IOException;
+    String createMenuItemCase(MenuEntry item,  String menuIdentifier, String resultMessage, String groupCaseId);
 }
