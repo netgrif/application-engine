5afea11101a96dcc6bdb097b8495cf0d95237d2e
Jozef Daxner <daxner@netgrif.com>
2021-09-24 17:38:56 +0200

    NAE-1406-Filter import/export
    - finish filter import and export
    - complete xsd schema for filter export/import
    - change loading of filter list for navigation menu

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IFilterImportExportService.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IFilterImportExportService.java
@@ -2,6 +2,7 @@ package com.netgrif.workflow.workflow.service.interfaces;
 
 import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.petrinet.domain.dataset.FileFieldValue;
+import com.netgrif.workflow.workflow.domain.IllegalFilterFileException;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,7 +12,7 @@ public interface IFilterImportExportService {
 
     FileFieldValue exportFilters(Set<String> filtersToExport) throws IOException;
 
-    List<String> importFilters() throws IOException;
+    List<String> importFilters() throws IOException, IllegalFilterFileException;
 
     void createFilterImport(User author);
 
