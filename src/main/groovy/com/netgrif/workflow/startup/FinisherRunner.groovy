a4d9f980ef4cb19a3cbb96ae6b80ea763c829676
Milan Mladoniczky <mladoniczky@netgrif.com>
2021-09-29 12:26:36 +0200

    - fix merge conflicts

--- src/main/groovy/com/netgrif/workflow/startup/FinisherRunner.groovy
+++ src/main/groovy/com/netgrif/workflow/startup/FinisherRunner.groovy
@@ -1,11 +1,8 @@
 package com.netgrif.workflow.startup
 
-import com.netgrif.workflow.petrinet.service.PetriNetService
-import com.netgrif.workflow.workflow.domain.repositories.CaseRepository
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,7 +12,6 @@ class FinisherRunner extends AbstractOrderedCommandLineRunner {
 
     @Override
     void run(String... strings) throws Exception {
-        superCreator.setAllToSuperUser()
         log.info("+----------------------------+")
         log.info("| Netgrif Application Engine |")
         log.info("+----------------------------+")
