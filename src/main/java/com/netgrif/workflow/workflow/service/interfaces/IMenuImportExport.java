0bd413fcc31b2f533cbfe8a9e813566066938018
Samuel Spisak <spisak@netgrif.com>
2021-10-15 20:44:46 +0200

    NAE-1417 -  Menu import/export
    -implementacia importu menu s filtrami

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IMenuImportExport.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IMenuImportExport.java
@@ -1,20 +1,24 @@
 package com.netgrif.workflow.workflow.service.interfaces;
 
 import com.netgrif.workflow.auth.domain.User;
+import com.netgrif.workflow.petrinet.domain.I18nString;
+import com.netgrif.workflow.petrinet.domain.dataset.EnumerationMapField;
 import com.netgrif.workflow.petrinet.domain.dataset.FileField;
 import com.netgrif.workflow.petrinet.domain.dataset.FileFieldValue;
+import com.netgrif.workflow.petrinet.domain.dataset.MultichoiceMapField;
 import com.netgrif.workflow.workflow.domain.Case;
 import com.netgrif.workflow.workflow.domain.MenuEntry;
 import com.netgrif.workflow.workflow.domain.MenuList;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public interface IMenuImportExport {
 
-    void createMenuImport(User author);
-    void createMenuExport(User author);
-    FileFieldValue exportMenu(List<Case> menuItemCaseIds, String menuIdentifier, String groupId, FileField fileField) throws IOException;
+    Map<String, I18nString>  createMenuExportChoices(MultichoiceMapField availableEntries, List<Case> menuItemCases);
+    Map<String, I18nString>  addSelectedEntriesToExport(MultichoiceMapField availableEntries, EnumerationMapField menusForExport, String menuidentifier);
+    FileFieldValue exportMenu(EnumerationMapField menusForExport, String groupId, FileField fileField) throws IOException;
     List<String> importMenu(List<Case> menuItemCases, FileFieldValue ffv, String groupCaseId) throws IOException;
-    String createMenuItemCase(MenuEntry item,  String menuIdentifier, String groupCaseId);
+    String createMenuItemCase(MenuEntry item,  String menuIdentifier, String groupCaseId, String filterCaseId);
 }
