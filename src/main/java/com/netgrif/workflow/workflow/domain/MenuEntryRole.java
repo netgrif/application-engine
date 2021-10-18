bf93012c1f9b7bcf86f55dcf4a4ef0db6367401a
Samuel Spisak <spisak@netgrif.com>
2021-10-18 20:37:22 +0200

    NAE-1417 -  Menu import/export
    - vytvaranie XSD schemy a jej validacie pri importe

--- src/main/java/com/netgrif/workflow/workflow/domain/MenuEntryRole.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/MenuEntryRole.java
@@ -2,29 +2,28 @@ package com.netgrif.workflow.workflow.domain;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.netgrif.workflow.petrinet.domain.I18nString;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.xml.bind.annotation.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-        "menus",
+        "roleImportId",
+        "netImportId"
 })
+public class MenuEntryRole {
 
-@XmlRootElement(name = "menus")
-public class MenuList {
+    @XmlElement(required = true)
+    protected String roleImportId;
+    @XmlElement(required = true)
+    protected String netImportId;
+    @XmlAttribute(name = "authorizationType", required = true)
+    protected AuthorizationType authorizationType;
 
-    @JacksonXmlElementWrapper(useWrapping = false)
-    @JacksonXmlProperty(localName = "menu")
-    protected List<Menu> menus;
-
-    public MenuList () {
-        this.menus = new ArrayList<>();
-    }
 }
