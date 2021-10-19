7fb0223ca424ed801c11eb82a0b2aec5cf3703fc
Samuel Spisak <spisak@netgrif.com>
2021-10-19 03:05:57 +0200

    NAE-1417 -  Menu import/export
    - vytvaranie XSD schemy a jej validacie pri importe,
    - dorabanie importu a inych mensich funkcionalit
    - dokumentacia

--- src/main/java/com/netgrif/workflow/workflow/service/interfaces/IMenuImportExport.java
+++ src/main/java/com/netgrif/workflow/workflow/service/interfaces/IMenuImportExport.java
@@ -1,6 +1,5 @@
 package com.netgrif.workflow.workflow.service.interfaces;
 
-import com.netgrif.workflow.auth.domain.User;
 import com.netgrif.workflow.petrinet.domain.I18nString;
 import com.netgrif.workflow.petrinet.domain.dataset.EnumerationMapField;
 import com.netgrif.workflow.petrinet.domain.dataset.FileField;
@@ -9,15 +8,19 @@ import com.netgrif.workflow.petrinet.domain.dataset.MultichoiceMapField;
 import com.netgrif.workflow.workflow.domain.Case;
 import com.netgrif.workflow.workflow.domain.IllegalMenuFileException;
 import com.netgrif.workflow.workflow.domain.MenuEntry;
-import com.netgrif.workflow.workflow.domain.MenuList;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+
+/**
+ * Interface which provides methods for menu import and export.
+ */
+
 public interface IMenuImportExport {
 
-    Map<String, I18nString>  createMenuExportChoices(MultichoiceMapField availableEntries, List<Case> menuItemCases);
+    Map<String, I18nString>  createAvailableEntriesChoices(List<Case> menuItemCases);
     Map<String, I18nString>  addSelectedEntriesToExport(MultichoiceMapField availableEntries, EnumerationMapField menusForExport, String menuidentifier);
     FileFieldValue exportMenu(EnumerationMapField menusForExport, String groupId, FileField fileField) throws IOException;
     List<String> importMenu(List<Case> menuItemCases, FileFieldValue ffv, String groupCaseId) throws IOException, IllegalMenuFileException;
