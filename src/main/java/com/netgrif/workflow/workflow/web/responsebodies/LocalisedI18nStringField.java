7337b95b945915944ad264583310e54e4b62888f
Jozef Daxner <daxner@netgrif.com>
2021-10-21 14:49:22 +0200

    NAE-1493-i18n divider Data field
    - add text view for i18n field

--- src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedI18nStringField.java
+++ src/main/java/com/netgrif/workflow/workflow/web/responsebodies/LocalisedI18nStringField.java
@@ -6,9 +6,10 @@ import lombok.Data;
 import java.util.Locale;
 
 @Data
-public class LocalisedI18nField extends LocalisedField {
+public class LocalisedI18nStringField extends LocalisedField {
 
-    public LocalisedI18nField(I18nField field, Locale locale) {
+    public LocalisedI18nStringField(I18nField field, Locale locale) {
         super(field, locale);
+        this.setValue(field.getValue().getTranslation(locale));
     }
 }
