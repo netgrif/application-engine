40fd12c7da54258049e0dbdeea00613c0c91af5f
Jakub Kovář <kovar@netgrif.com>
2021-10-25 15:36:06 +0200

    5.7.0
    - PR remarks

--- src/main/java/com/netgrif/workflow/workflow/domain/menu/MenuList.java
+++ src/main/java/com/netgrif/workflow/workflow/domain/menu/MenuList.java
@@ -1,29 +1,29 @@
-package com.netgrif.workflow.workflow.domain;
+package com.netgrif.workflow.workflow.domain.menu;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-        "menuEntries",
+        "menus",
 })
 
-@XmlRootElement(name = "menu")
-public class Menu {
+@XmlRootElement(name = "menus")
+public class MenuList {
 
     @JacksonXmlElementWrapper(useWrapping = false)
-    @JacksonXmlProperty(localName = "menuEntry")
-    protected List<MenuEntry> menuEntries;
+    @JacksonXmlProperty(localName = "menu")
+    protected List<Menu> menus;
 
-    @JacksonXmlProperty(isAttribute = true, localName = "name")
-    protected String menuIdentifier;
+    public MenuList () {
+        this.menus = new ArrayList<>();
+    }
 }
