d67d04c29497513d0fdfca5d93ea61de61298c18
Jakub Kovář <kovar@netgrif.com>
2021-11-12 16:37:06 +0100

    NAE-1518 - Export of chained filters
    - WIP menu export test fixes

---
+++ src/main/java/com/netgrif/workflow/workflow/domain/filter/FilterImportExportList.java
@@ -0,0 +1,31 @@
+package com.netgrif.workflow.workflow.domain.filter;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that wraps and holds list of filters, which are meant to be exported/imported.
+ * This is the root class of xml file, that is created after exporting of filters.
+ * The root tag of the xml file is: <filters>
+ * <filters> tag is followed by list of <filter> tags.
+ * Whole schema for the xml file is on the path: resources/petriNets/filter_export_schema.xsd
+ */
+@EqualsAndHashCode
+@Getter
+@Setter
+@JacksonXmlRootElement(localName = "filters")
+public class FilterImportExportList {
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "filter")
+    protected List<FilterImportExport> filters;
+
+    public FilterImportExportList() {
+        this.filters = new ArrayList<>();
+    }
+}
