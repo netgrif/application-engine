ec47cf5355a66d39769d1a6629456ecb3e5121d7
Stefan Renczes <renczes@netgrif.com>
2021-11-18 17:28:10 +0100

    NAE-1508 - Clean up petriflow schema
    - implemented check of deprecated usersRef on process
    - refactored function names

--- src/main/java/com/netgrif/workflow/importer/service/IDocumentValidator.java
+++ src/main/java/com/netgrif/workflow/importer/service/IDocumentValidator.java
@@ -4,4 +4,6 @@ import com.netgrif.workflow.importer.model.Document;
 
 public interface IDocumentValidator extends IModelValidator {
     void checkDeprecatedAttributes(Document document);
+
+    void checkBeatingAttributes(Document document, Object attr1, Object attr2, String attr1Name, String attr2Name);
 }
