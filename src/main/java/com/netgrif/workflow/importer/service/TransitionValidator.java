09d685af934beb5e5f416b3ced9e6428aa3d4c58
Stefan Renczes <renczes@netgrif.com>
2021-11-19 07:56:56 +0100

    NAE-1508 - Clean up petriflow schema
    - added check for beating attribute in assign tag

--- src/main/java/com/netgrif/workflow/importer/service/TransitionValidator.java
+++ src/main/java/com/netgrif/workflow/importer/service/TransitionValidator.java
@@ -18,10 +18,12 @@ public class TransitionValidator implements ITransitionValidator {
     @Override
     public void checkBeatingAttributes(Transition transition, Object attr1, Object attr2, String attr1Name, String attr2Name) {
         if ((attr1 instanceof Collection && attr2 instanceof Collection && !((Collection) attr1).isEmpty() && !((Collection) attr2).isEmpty())) {
-            throw new BeatingAttributesException(transition.getId(), "transition", attr1Name, attr2Name);
+            throw new BeatingAttributesException("Attributes \"" + attr1Name + "\" and \"" + attr2Name + "\" cannot be present at the same time" +
+                    " on model \"transition\" with ID \"" + transition.getId() + "\". Consider using only one of them.");
         }
         if ((!(attr1 instanceof Collection) && !(attr2 instanceof Collection) && attr1 != null && attr2 != null)) {
-            throw new BeatingAttributesException(transition.getId(), "transition", attr1Name, attr2Name);
+            throw new BeatingAttributesException("Attributes \"" + attr1Name + "\" and \"" + attr2Name + "\" cannot be present at the same time" +
+                    " on model \"transition\" with ID \"" + transition.getId() + "\". Consider using only one of them.");
         }
     }
 }
