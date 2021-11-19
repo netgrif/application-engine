d86f5b3b59d239f7cb6cf09b2fb3eefb63f933c8
Stefan Renczes <renczes@netgrif.com>
2021-11-19 15:01:27 +0100

    NAE-1508 - Clean up petriflow schema
    - added javadoc for validators checkBeatingAttributes function

--- src/main/java/com/netgrif/workflow/importer/service/DocumentValidator.java
+++ src/main/java/com/netgrif/workflow/importer/service/DocumentValidator.java
@@ -14,7 +14,7 @@ public class DocumentValidator implements IDocumentValidator {
     }
 
     @Override
-    public void checkBeatingAttributes(Document document, Object attr1, Object attr2, String attr1Name, String attr2Name) {
+    public void checkBeatingAttributes(Document document, Object attr1, Object attr2, String attr1Name, String attr2Name) throws BeatingAttributesException {
         if ((attr1 instanceof Collection && attr2 instanceof Collection && !((Collection) attr1).isEmpty() && !((Collection) attr2).isEmpty())) {
             throw new BeatingAttributesException("Attributes \"" + attr1Name + "\" and \"" + attr2Name + "\" cannot be present at the same time" +
                     " on model \"document\" with ID \"" + document.getId() + "\". Consider using only one of them.");
--- src/main/java/com/netgrif/workflow/importer/service/TransitionValidator.java
+++ src/main/java/com/netgrif/workflow/importer/service/TransitionValidator.java
@@ -16,7 +16,7 @@ public class TransitionValidator implements ITransitionValidator {
     }
 
     @Override
-    public void checkBeatingAttributes(Transition transition, Object attr1, Object attr2, String attr1Name, String attr2Name) {
+    public void checkBeatingAttributes(Transition transition, Object attr1, Object attr2, String attr1Name, String attr2Name) throws BeatingAttributesException {
         if ((attr1 instanceof Collection && attr2 instanceof Collection && !((Collection) attr1).isEmpty() && !((Collection) attr2).isEmpty())) {
             throw new BeatingAttributesException("Attributes \"" + attr1Name + "\" and \"" + attr2Name + "\" cannot be present at the same time" +
                     " on model \"transition\" with ID \"" + transition.getId() + "\". Consider using only one of them.");
