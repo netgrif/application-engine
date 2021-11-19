f923d1abdfa9d47350d687c4f514aba7179cbeb0
Stefan Renczes <renczes@netgrif.com>
2021-11-19 07:56:56 +0100

    NAE-1508 - Clean up petriflow schema
    - added check for beating attribute in assign tag

--- src/main/java/com/netgrif/workflow/importer/service/ILogicValidator.java
+++ src/main/java/com/netgrif/workflow/importer/service/ILogicValidator.java
@@ -4,4 +4,6 @@ import com.netgrif.workflow.importer.model.Logic;
 
 public interface ILogicValidator extends IModelValidator {
     void checkDeprecatedAttributes(Logic logic);
+
+    void checkBeatingAttributes(Logic logic, Object attr1, Object attr2, String attr1Name, String attr2Name);
 }
