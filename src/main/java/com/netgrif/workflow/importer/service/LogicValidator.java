14b5a607559cad432def0b2d4bb03e7bac867b7c
Stefan Renczes <renczes@netgrif.com>
2021-12-10 16:27:10 +0100

    NAE-1503 - Permissions refactor phase 1
    - corrected validators and ElasticViewPermissionService according to comments on PR

--- src/main/java/com/netgrif/workflow/importer/service/LogicValidator.java
+++ src/main/java/com/netgrif/workflow/importer/service/LogicValidator.java
@@ -15,7 +15,7 @@ public class LogicValidator implements ILogicValidator {
     }
 
     @Override
-    public void checkBeatingAttributes(Logic logic, Object attr1, Object attr2, String attr1Name, String attr2Name) throws BeatingAttributesException {
+    public void checkConflictingAttributes(Logic logic, Object attr1, Object attr2, String attr1Name, String attr2Name) throws BeatingAttributesException {
         if ((attr1 instanceof Collection && attr2 instanceof Collection && !((Collection) attr1).isEmpty() && !((Collection) attr2).isEmpty())) {
             throw new BeatingAttributesException("Attributes \"" + attr1Name + "\" and \"" + attr2Name + "\" cannot be present at the same time" +
                     " on model \"logic\". Consider using only one of them.");
