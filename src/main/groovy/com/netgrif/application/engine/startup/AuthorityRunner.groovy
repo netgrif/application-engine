a14d450f0257cbd894e7ef161629225727b69449
Stefan Renczes <renczes.stefan@gmail.com>
2022-04-29 10:50:35 +0200

    [NAE-1617] Refactor Authority
    - updating authority runner

--- src/main/groovy/com/netgrif/application/engine/startup/AuthorityRunner.groovy
+++ src/main/groovy/com/netgrif/application/engine/startup/AuthorityRunner.groovy
@@ -1,6 +1,6 @@
 package com.netgrif.application.engine.startup
 
-import com.netgrif.application.engine.auth.domain.Authority
+import com.netgrif.application.engine.auth.domain.AuthorityEnum
 import com.netgrif.application.engine.auth.service.interfaces.IAuthorityService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -13,9 +13,6 @@ class AuthorityRunner extends AbstractOrderedCommandLineRunner {
 
     @Override
     void run(String... strings) throws Exception {
-        service.getOrCreate(Authority.user)
-        service.getOrCreate(Authority.admin)
-        service.getOrCreate(Authority.systemAdmin)
-        service.getOrCreate(Authority.anonymous)
+        AuthorityEnum.values().toList().forEach(authority -> service.getOrCreate(authority.name()))
     }
 }
