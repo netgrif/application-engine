245f98260757a5fa16087b17325cb12d145abf22
Jozef Daxner <daxner@netgrif.com>
2022-05-06 15:30:26 +0200

    [NAE-1468] i18n Data field - add tests for i18n field - add documentation for i18n field

--- src/test/groovy/com/netgrif/application/engine/elastic/DataSearchRequestTest.groovy
+++ src/test/groovy/com/netgrif/application/engine/elastic/DataSearchRequestTest.groovy
@@ -1,6 +1,7 @@
 package com.netgrif.application.engine.elastic
 
 import com.netgrif.application.engine.MockService
+import com.netgrif.application.engine.TestHelper
 import com.netgrif.application.engine.auth.service.interfaces.IUserService
 import com.netgrif.application.engine.elastic.domain.ElasticCase
 import com.netgrif.application.engine.elastic.domain.ElasticCaseRepository
@@ -11,6 +12,7 @@ import com.netgrif.application.engine.elastic.web.requestbodies.CaseSearchReques
 import com.netgrif.application.engine.petrinet.domain.dataset.ChoiceField
 import com.netgrif.application.engine.petrinet.domain.dataset.FileFieldValue
 import com.netgrif.application.engine.petrinet.domain.dataset.FileListFieldValue
+import com.netgrif.application.engine.petrinet.domain.dataset.UserFieldValue
 import com.netgrif.application.engine.petrinet.service.interfaces.IPetriNetService
 import com.netgrif.application.engine.startup.ImportHelper
 import com.netgrif.application.engine.startup.SuperCreator
@@ -21,7 +23,6 @@ import com.netgrif.application.engine.workflow.service.interfaces.IDataService
 import com.netgrif.application.engine.workflow.service.interfaces.ITaskService
 import com.netgrif.application.engine.workflow.service.interfaces.IWorkflowService
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.Logger
@@ -42,7 +43,6 @@ import java.time.LocalTime
 @SpringBootTest()
 @ActiveProfiles(["test"])
 @ExtendWith(SpringExtension.class)
-@Disabled("UnsatisfiedDependency Error creating")
 class DataSearchRequestTest {
 
     private static final Logger log = LoggerFactory.getLogger(DataSearchRequestTest)
@@ -86,15 +86,19 @@ class DataSearchRequestTest {
     @Autowired
     private IDataService dataService
 
+    @Autowired
+    private TestHelper testHelper
+
     private ArrayList<Map.Entry<String, String>> testCases
 
     @BeforeEach
     void before() {
-        template.deleteIndex(ElasticCase.class)
+        testHelper.truncateDbs()
+//        template.deleteIndex(ElasticCase.class)
         template.createIndex(ElasticCase.class)
         template.putMapping(ElasticCase.class)
 
-        template.deleteIndex(ElasticTask.class)
+//        template.deleteIndex(ElasticTask.class)
         template.createIndex(ElasticTask.class)
         template.putMapping(ElasticTask.class)
 
@@ -118,7 +122,7 @@ class DataSearchRequestTest {
         _case.dataSet["number"].value = 7.0 as Double
         _case.dataSet["boolean"].value = true
         _case.dataSet["text"].value = "hello world" as String
-        _case.dataSet["user"].value = testUser1
+        _case.dataSet["user"].value = new UserFieldValue(testUser1.stringId, testUser1.name, testUser1.surname, testUser1.email)
         _case.dataSet["date"].value = date
         _case.dataSet["datetime"].value = date.atTime(13, 37)
         _case.dataSet["enumeration"].value = (_case.petriNet.dataSet["enumeration"] as ChoiceField).choices.find({ it.defaultValue == "Alice" })
@@ -128,6 +132,8 @@ class DataSearchRequestTest {
         _case.dataSet["file"].value = FileFieldValue.fromString("singlefile.txt")
         _case.dataSet["fileList"].value = FileListFieldValue.fromString("multifile1.txt,multifile2.pdf")
         _case.dataSet["userList"].value = [testUser1.id, testUser2.id]
+        _case.dataSet["i18n_text"].value.defaultValue = "Modified i18n text value"
+        _case.dataSet["i18n_divider"].value.defaultValue = "Modified i18n divider value"
         workflowService.save(_case)
 
         Task actionTrigger = taskService.searchOne(QTask.task.caseId.eq(_case.stringId).and(QTask.task.transitionId.eq("2")));
@@ -211,6 +217,8 @@ class DataSearchRequestTest {
                 new AbstractMap.SimpleEntry<String, String>("multichoice_map_changed.textValue.keyword" as String, "Félix" as String),
                 new AbstractMap.SimpleEntry<String, String>("multichoice_map_changed.keyValue" as String, "eve" as String),
                 new AbstractMap.SimpleEntry<String, String>("multichoice_map_changed.keyValue" as String, "felix" as String),
+                new AbstractMap.SimpleEntry<String, String>("i18n_text.textValue.keyword" as String, "Modified i18n text value" as String),
+                new AbstractMap.SimpleEntry<String, String>("i18n_divider.textValue.keyword" as String, "Modified i18n divider value" as String),
         ]
     }
 
