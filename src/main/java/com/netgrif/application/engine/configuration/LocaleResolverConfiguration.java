cea906bc84d9a56ca577a5f886d9094cd5d0b776
Stefan Renczes <renczes.stefan@gmail.com>
2022-09-30 14:01:36 +0200

    [NAE-1544] Time zone in Date and Datetime field
    - added Primary attribute

--- src/main/java/com/netgrif/application/engine/configuration/LocaleResolverConfiguration.java
+++ src/main/java/com/netgrif/application/engine/configuration/LocaleResolverConfiguration.java
@@ -3,13 +3,15 @@ package com.netgrif.application.engine.configuration;
 import com.netgrif.application.engine.configuration.locale.LocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class LocaleResolverConfiguration {
 
-
-    @Bean
+    @Primary
+    @Bean(name = DispatcherServlet.LOCALE_RESOLVER_BEAN_NAME)
     org.springframework.web.servlet.LocaleResolver localeResolver() {
         SessionLocaleResolver l = new SessionLocaleResolver();
         return new LocaleResolver(l);
