f4db2ad5c49d53f13465d0f574595f4f04e8131f
Stefan Renczes <renczes.stefan@gmail.com>
2022-11-30 13:02:12 +0100

    [NAE-1798] UserRefy sa resolveuju po kazdom update Case nie len pri update fieldu ktory sa ich tyka
    - removed old code, when user refs was resolved
    - implemented new helper function to resolve user ref permission in workflowService

--- src/main/java/com/netgrif/application/engine/workflow/service/TaskService.java
+++ src/main/java/com/netgrif/application/engine/workflow/service/TaskService.java
@@ -435,7 +435,7 @@ public class TaskService implements ITaskService {
                 executeTransition(task, workflowService.findOne(useCase.getStringId()));
                 return;
             }
-            resolveUserRef(task, useCase);
+            //resolveUserRef(task, useCase);
         }
     }
 
