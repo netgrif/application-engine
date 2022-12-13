8bb022579cda8ba18e151e5a2b2941674871c424
Stefan Renczes <renczes.stefan@gmail.com>
2022-12-13 17:08:12 +0100

    [NAE-1798] UserRefs resolves everytime when case is saved
    - corrected according to PR

--- src/main/java/com/netgrif/application/engine/workflow/service/TaskService.java
+++ src/main/java/com/netgrif/application/engine/workflow/service/TaskService.java
@@ -434,7 +434,6 @@ public class TaskService implements ITaskService {
                 executeTransition(task, workflowService.findOne(useCase.getStringId()));
                 return;
             }
-            //resolveUserRef(task, useCase);
         }
     }
 
