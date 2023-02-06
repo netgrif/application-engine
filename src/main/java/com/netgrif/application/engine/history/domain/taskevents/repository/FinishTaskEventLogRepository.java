e4f79d4cd2a7f223fe16db2dfb0706fba2844af3
Machac <machac@netgrif.com>
2023-02-06 10:48:18 +0100

    [NAE-1723] EventLog classes are not resolved correctly after search in MongoDB collection
    - fix

--- src/main/java/com/netgrif/application/engine/history/domain/taskevents/repository/FinishTaskEventLogRepository.java
+++ src/main/java/com/netgrif/application/engine/history/domain/taskevents/repository/FinishTaskEventLogRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Deprecated(since = "6.2.2")
+@Deprecated(since = "6.3.0")
 public interface FinishTaskEventLogRepository extends MongoRepository<FinishTaskEventLog, ObjectId> {
 
     List<FinishTaskEventLog> findAllByTaskId(ObjectId taskId);
