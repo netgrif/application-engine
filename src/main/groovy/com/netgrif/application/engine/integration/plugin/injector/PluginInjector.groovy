3619c4e34998db39b1f6964eda0838c2a26fc9f0
Stefan Renczes <renczes.stefan@gmail.com>
2023-11-08 08:15:14 +0100

    [NAE-1546] Plugin library
    - modified injector

--- src/main/groovy/com/netgrif/application/engine/integration/plugin/injector/PluginInjector.groovy
+++ src/main/groovy/com/netgrif/application/engine/integration/plugin/injector/PluginInjector.groovy
@@ -4,26 +4,22 @@ import com.netgrif.application.engine.configuration.ApplicationContextProvider
 import com.netgrif.application.engine.integration.plugins.domain.Plugin
 import com.netgrif.application.engine.integration.plugins.service.PluginService
 import com.netgrif.application.engine.petrinet.domain.dataset.logic.action.ActionDelegate
-import groovy.util.logging.Slf4j
-import org.springframework.stereotype.Component
 
-@Component
-@Slf4j
+
 class PluginInjector {
 
-    void inject(Plugin plugin) {
+    static void inject(Plugin plugin) {
         MetaClass actionDelegateMeta = ActionDelegate.metaClass
-        PluginMeta pluginMeta = new PluginMeta()
+        MetaClass pluginMetaClass = PluginMeta.metaClass
         plugin.entryPoints.each {ep ->
-            EntryPointMeta entryPointMeta = new EntryPointMeta()
+            MetaClass entryPointMetaClass = EntryPointMeta.metaClass
             ep.value.methods.each { m ->
-                entryPointMeta.metaClass[m.value.name] << { Serializable... args ->
-                    PluginService pluginService = ApplicationContextProvider.getBean("pluginService")
+                entryPointMetaClass[m.value.name] << { Serializable... args ->
+                    PluginService pluginService = ApplicationContextProvider.getBean("pluginService") as PluginService
                     return pluginService.call(plugin.identifier, ep.value.identifier, m.value.name, args)}
             }
-            pluginMeta.metaClass[ep.value.identifier] = entryPointMeta
+            pluginMetaClass[ep.value.identifier] = new EntryPointMeta()
         }
-        actionDelegateMeta[plugin.identifier] = pluginMeta
-        log.info("Injected plugin into ActionDelegate")
+        actionDelegateMeta[plugin.identifier] = new PluginMeta()
     }
 }
