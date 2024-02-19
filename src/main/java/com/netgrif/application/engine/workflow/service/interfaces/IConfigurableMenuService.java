dc3fcdd5a6835634820e66095966e05b6157dd88
Machac <machac@netgrif.com>
2024-02-19 13:26:01 +0100

    [NAE-1948] Bugs after merge to 6.4.0
    - fix type User

--- src/main/java/com/netgrif/application/engine/workflow/service/interfaces/IConfigurableMenuService.java
+++ src/main/java/com/netgrif/application/engine/workflow/service/interfaces/IConfigurableMenuService.java
@@ -1,7 +1,6 @@
 package com.netgrif.application.engine.workflow.service.interfaces;
 
-import com.netgrif.application.engine.auth.domain.LoggedUser;
-import com.netgrif.application.engine.auth.domain.User;
+import com.netgrif.application.engine.auth.domain.IUser;
 import com.netgrif.application.engine.petrinet.domain.I18nString;
 import com.netgrif.application.engine.petrinet.domain.dataset.EnumerationMapField;
 import com.netgrif.application.engine.petrinet.domain.dataset.MultichoiceMapField;
@@ -11,7 +10,7 @@ import java.util.Map;
 
 public interface IConfigurableMenuService {
 
-    Map<String, I18nString> getNetsByAuthorAsMapOptions(User author, Locale locale);
+    Map<String, I18nString> getNetsByAuthorAsMapOptions(IUser author, Locale locale);
     Map<String, I18nString> getAvailableRolesFromNet (EnumerationMapField processField,MultichoiceMapField permittedRoles, MultichoiceMapField bannedRoles);
     Map<String, I18nString> addSelectedRoles(MultichoiceMapField selected, EnumerationMapField netField, MultichoiceMapField roles);
     Map<String, I18nString> removeSelectedRoles(MultichoiceMapField addedRoles);
