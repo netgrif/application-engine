192e00537ecbbcc27c21f22e38e4c67cdbaeecee
martinkranec <kranec@netgrif.com>
2024-03-22 09:46:24 +0100

    [NAE-1946] Remote file connector to S3 - added new type of storage - added autowiring by setter

--- src/main/java/com/netgrif/application/engine/files/StorageResolverService.java
+++ src/main/java/com/netgrif/application/engine/files/StorageResolverService.java
@@ -7,24 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class StorageResolverService {
 
-    @Autowired(required = false)
     private List<IStorageService> storageServices;
 
-    public IStorageService resolve(String type) {
+    @Autowired
+    private void setStorageServices(List<IStorageService> storageServices) {
+        this.storageServices = storageServices;
+    }
+
+    public IStorageService resolve(StorageType type) {
         if (storageServices == null) {
             log.error("Storage services with interface IStorageService not found.");
             throw new StorageNotFoundException("Remote Storage not available.");
         }
-        IStorageService storageService = storageServices.stream().filter(service -> service.getType().equals(type)).collect(Collectors.toList()).get(0);
-        if (storageService == null) {
-            throw new StorageNotFoundException("Storage Service with type: " + type + " not available.");
+        Optional<IStorageService> storageService = storageServices.stream().filter(service -> service.getType().equals(type)).collect(Collectors.toList()).stream().findFirst();
+        if (storageService.isPresent()) {
+            return storageService.get();
         }
-        return storageService;
+        throw new StorageNotFoundException("Storage Service with type: " + type + " not available.");
+
     }
 }
