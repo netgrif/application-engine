5cdf8d83a008d6774c625b0b1a7970249f7d15ce
martinkranec <kranec@netgrif.com>
2024-03-27 09:50:57 +0100

    [NAE-1946] Remote file connector to S3 - fix new init test

--- src/test/groovy/com/netgrif/application/engine/workflow/NewInitTest.groovy
+++ src/test/groovy/com/netgrif/application/engine/workflow/NewInitTest.groovy
@@ -16,10 +16,12 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles(["test"])
+@TestPropertySource(properties = "nae.storage.minio.enabled=true")
 @SpringBootTest
 class NewInitTest {
 
