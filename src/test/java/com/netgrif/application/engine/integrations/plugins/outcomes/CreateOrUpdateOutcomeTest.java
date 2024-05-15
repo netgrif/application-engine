908eac165975115766d3ce870e134b0c8a8cb1a5
chvostek <chvostek@netgrif.com>
2024-05-15 12:09:24 +0200

    [NAE-1546] Plugin library - implement CreateOrUpdateOutcomeTest

---
+++ src/test/java/com/netgrif/application/engine/integrations/plugins/outcomes/CreateOrUpdateOutcomeTest.java
@@ -0,0 +1,21 @@
+package com.netgrif.application.engine.integrations.plugins.outcomes;
+
+import com.netgrif.application.engine.integration.plugins.outcomes.CreateOrUpdateOutcome;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"test"})
+public class CreateOrUpdateOutcomeTest {
+
+    @Test
+    public void testAddCreatedAndSubjectCaseId() {
+        CreateOrUpdateOutcome outcome = new CreateOrUpdateOutcome();
+
+        outcome.addCreatedAndSubjectCaseId("test-case-id");
+
+        assert outcome.getCreatedCaseIds().contains("test-case-id") : "Collection createdCaseIds has missing id";
+        assert outcome.getSubjectCaseIds().contains("test-case-id") : "Collection subjectCaseIds has missing id";
+    }
+}
