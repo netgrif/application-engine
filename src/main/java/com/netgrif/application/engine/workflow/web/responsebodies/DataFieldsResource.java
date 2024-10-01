0c753ddc75dd738858df7460071cf7e9d44b783b
Stefan Renczes <renczes@netgrif.com>
2024-10-01 15:04:44 +0200

    [NAE-2008] Serialisation of classes
    - serialisation of LocalisedField and LocalizedValidation
    - added content serialisation for CollectionModel

--- src/main/java/com/netgrif/application/engine/workflow/web/responsebodies/DataFieldsResource.java
+++ src/main/java/com/netgrif/application/engine/workflow/web/responsebodies/DataFieldsResource.java
@@ -1,18 +1,27 @@
 package com.netgrif.application.engine.workflow.web.responsebodies;
 
 import com.netgrif.application.engine.petrinet.domain.dataset.Field;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.hateoas.CollectionModel;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
-public class DataFieldsResource extends CollectionModel<LocalisedField> {
+@Setter
+@Getter
+public class DataFieldsResource extends CollectionModel<LocalisedField> implements Serializable {
+
+    private static final long serialVersionUID = 73213276016133399L;
+
+    private Collection<LocalisedField> content;
 
     public DataFieldsResource(Collection<Field> content, Locale locale) {
-        super(content.stream()
+        super();
+        this.content = content.stream()
                 .map(f -> LocalisedFieldFactory.from(f, locale))
-                .collect(Collectors.toList()), new ArrayList<>(), null);
+                .collect(Collectors.toList());
     }
 }
