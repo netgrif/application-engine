1fa70e9449fd1a4a322f11ea395102994bab762e
chvostek <chvostek@netgrif.com>
2025-02-17 13:15:37 +0100

    [NAE-2054] Release 6.5.0 CE - update FilterRunner after merge

--- src/main/groovy/com/netgrif/application/engine/startup/FilterRunner.groovy
+++ src/main/groovy/com/netgrif/application/engine/startup/FilterRunner.groovy
@@ -2,7 +2,6 @@ package com.netgrif.application.engine.startup
 
 import com.netgrif.application.engine.menu.domain.MenuItemView
 import com.netgrif.application.engine.petrinet.domain.PetriNet
-import com.netgrif.application.engine.petrinet.domain.VersionType
 import com.netgrif.application.engine.petrinet.service.interfaces.IPetriNetService
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
@@ -35,50 +34,18 @@ class FilterRunner extends AbstractOrderedCommandLineRunner {
 
     @Override
     void run(String... args) throws Exception {
-        createFilterNet()
+        helper.importProcess("Petri net for filters", FILTER_PETRI_NET_IDENTIFIER, FILTER_FILE_NAME)
         createConfigurationNets()
-        createMenuItemNet()
-        createImportFiltersNet()
-        createExportFiltersNet()
+        helper.importProcess("Petri net for filter preferences", MENU_NET_IDENTIFIER, MENU_ITEM_FILE_NAME)
+        helper.importProcess("Petri net for importing filters", IMPORT_NET_IDENTIFIER, IMPORT_FILTER_FILE_NAME)
+        helper.importProcess("Petri net for exporting filters", EXPORT_NET_IDENTIFIER, EXPORT_FILTER_FILE_NAME)
     }
 
-    Optional<PetriNet> createFilterNet() {
-        importProcess("Petri net for filters", FILTER_PETRI_NET_IDENTIFIER, FILTER_FILE_NAME)
-    }
-
-    Optional<PetriNet> createMenuItemNet() {
-        importProcess("Petri net for filter preferences", MENU_NET_IDENTIFIER, MENU_ITEM_FILE_NAME)
-    }
-
-    List<PetriNet> createConfigurationNets() {
+    private List<PetriNet> createConfigurationNets() {
         return MenuItemView.values().each { view ->
             String processIdentifier = view.getIdentifier() + "_configuration"
             String filePath = String.format("engine-processes/menu/%s.xml", processIdentifier)
-            importProcess(String.format("Petri net for %s", processIdentifier), processIdentifier, filePath)
+            helper.importProcess(String.format("Petri net for %s", processIdentifier), processIdentifier, filePath)
         }.collect()
-    }
-
-    Optional<PetriNet> createImportFiltersNet() {
-        importProcess("Petri net for importing filters", IMPORT_NET_IDENTIFIER, IMPORT_FILTER_FILE_NAME)
-    }
-
-    Optional<PetriNet> createExportFiltersNet() {
-        importProcess("Petri net for exporting filters", EXPORT_NET_IDENTIFIER, EXPORT_FILTER_FILE_NAME)
-    }
-
-    Optional<PetriNet> importProcess(String message, String netIdentifier, String netFileName) {
-        PetriNet filter = petriNetService.getNewestVersionByIdentifier(netIdentifier)
-        if (filter != null) {
-            log.info("${message} has already been imported.")
-            return Optional.of(filter)
-        }
-
-        Optional<PetriNet> filterNet = helper.createNet(netFileName, VersionType.MAJOR, systemCreator.loggedSystem)
-
-        if (!filterNet.isPresent()) {
-            log.error("Import of ${message} failed!")
-        }
-
-        return filterNet
     }
 }
