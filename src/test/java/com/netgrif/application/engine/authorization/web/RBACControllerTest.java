45e72d01e40b96cb17b9648140db00c97d833d31
chvostek <chvostek@netgrif.com>
2025-03-07 08:48:18 +0100

    [NAE-2058] Role refactor - add RBACControllerTest with todo

---
+++ src/test/java/com/netgrif/application/engine/authorization/web/RBACControllerTest.java
@@ -0,0 +1,13 @@
+package com.netgrif.application.engine.authorization.web;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ActiveProfiles({"test"})
+@ExtendWith(SpringExtension.class)
+public class RBACControllerTest {
+    // todo 2058
+}
