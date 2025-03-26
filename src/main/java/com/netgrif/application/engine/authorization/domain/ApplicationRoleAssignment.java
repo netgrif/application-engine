004b1a02bce481827d642da0313c88c97d3f198a
chvostek <chvostek@netgrif.com>
2025-03-26 15:47:04 +0100

    [NAE-2058] Role refactor - implement session authority - implement ApplicationRoleAssignment.toSessionAuthority

--- src/main/java/com/netgrif/application/engine/authorization/domain/ApplicationRoleAssignment.java
+++ src/main/java/com/netgrif/application/engine/authorization/domain/ApplicationRoleAssignment.java
@@ -1,5 +1,6 @@
 package com.netgrif.application.engine.authorization.domain;
 
+import com.netgrif.application.engine.authentication.domain.Authority;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -11,4 +12,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ApplicationRoleAssignment extends RoleAssignment {
     @Indexed
     private String applicationId;
+
+    /**
+     * todo javadoc
+     */
+    public Authority toSessionAuthority() {
+        return new Authority(getStringId(), roleId);
+    }
 }
