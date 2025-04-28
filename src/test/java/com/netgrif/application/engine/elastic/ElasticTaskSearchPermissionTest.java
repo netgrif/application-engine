fa7e6608faabeda6717a66880f4a3a9147846e13
chvostek <chvostek@netgrif.com>
2025-04-28 12:37:56 +0200

    [NAE-2058] Role refactor - implement ElasticTaskSearchPermissionTest - optimize ElasticCaseSearchPermissionTest

--- src/test/java/com/netgrif/application/engine/elastic/ElasticTaskSearchPermissionTest.java
+++ src/test/java/com/netgrif/application/engine/elastic/ElasticTaskSearchPermissionTest.java
@@ -7,8 +7,9 @@ import com.netgrif.application.engine.authorization.domain.*;
 import com.netgrif.application.engine.authorization.domain.repositories.CaseRoleRepository;
 import com.netgrif.application.engine.authorization.domain.repositories.ProcessRoleRepository;
 import com.netgrif.application.engine.authorization.domain.repositories.RoleAssignmentRepository;
-import com.netgrif.application.engine.elastic.service.interfaces.IElasticCaseService;
-import com.netgrif.application.engine.elastic.web.requestbodies.CaseSearchRequest;
+import com.netgrif.application.engine.elastic.service.interfaces.IElasticTaskService;
+import com.netgrif.application.engine.elastic.web.requestbodies.ElasticTaskSearchRequest;
+import com.netgrif.application.engine.petrinet.domain.Process;
 import com.netgrif.application.engine.petrinet.domain.VersionType;
 import com.netgrif.application.engine.petrinet.domain.dataset.TextField;
 import com.netgrif.application.engine.petrinet.domain.throwable.MissingPetriNetMetaDataException;
@@ -17,6 +18,7 @@ import com.netgrif.application.engine.startup.ApplicationRoleRunner;
 import com.netgrif.application.engine.startup.ImportHelper;
 import com.netgrif.application.engine.startup.SuperCreator;
 import com.netgrif.application.engine.workflow.domain.Case;
+import com.netgrif.application.engine.workflow.domain.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,22 +28,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import com.netgrif.application.engine.petrinet.domain.Process;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 @SpringBootTest
 @ActiveProfiles({"test"})
 @ExtendWith(SpringExtension.class)
-public class ElasticCaseSearchPermissionTest {
+public class ElasticTaskSearchPermissionTest {
 
     @Autowired
-    private IElasticCaseService elasticCaseService;
+    private IElasticTaskService elasticTaskService;
+
+    @Autowired
+    private IPetriNetService petriNetService;
 
     @Autowired
     private RoleAssignmentRepository roleAssignmentRepository;
@@ -56,59 +59,24 @@ public class ElasticCaseSearchPermissionTest {
     private ApplicationRoleRunner applicationRoleRunner;
 
     @Autowired
-    private IPetriNetService petriNetService;
-
-    @Autowired
-    private ImportHelper importHelper;
+    private SuperCreator superCreator;
 
     @Autowired
     private TestHelper testHelper;
 
     @Autowired
-    private SuperCreator superCreator;
+    private ImportHelper importHelper;
 
     private Identity testIdentity;
 
     private Case testCase;
 
-    private CaseSearchRequest request;
+    private ElasticTaskSearchRequest request;
 
     @BeforeEach
     public void before() {
         testHelper.truncateDbs();
         createIdentityWithActor();
-    }
-
-    /**
-     * todo javadoc
-     * */
-    @Test
-    public void testViewPermissionsOrdered() throws InterruptedException, IOException, MissingPetriNetMetaDataException {
-        createTestCase("case_authorization_test");
-        buildSearchRequest();
-        Thread.sleep(2000);
-
-        assertWithoutRole();
-        assertWithAddedPosProcessRole();
-        assertWithAddedNegProcessRole();
-        assertWithAddedPosCaseRole();
-        assertWithAddedNegCaseRole();
-        assertWithAddedAdminAppRole();
-    }
-
-    /**
-     * todo javadoc
-     * */
-    @Test
-    public void testViewWithDefaultPermission() throws IOException, MissingPetriNetMetaDataException, InterruptedException {
-        createTestCase("case_authorization_default_test");
-        buildSearchRequest();
-        Thread.sleep(2000);
-
-        Page<Case> pagedResult = doSearch();
-
-        assert pagedResult.hasContent();
-        assert pagedResult.getTotalElements() == 1;
     }
 
     private void createIdentityWithActor() {
@@ -120,23 +88,45 @@ public class ElasticCaseSearchPermissionTest {
                 .build(), new ArrayList<>());
     }
 
-    private void createTestCase(String identifier) throws IOException, MissingPetriNetMetaDataException {
-        Process process = petriNetService.importPetriNet(new FileInputStream(String.format("src/test/resources/petriNets/%s.xml", identifier)),
-                VersionType.MAJOR, superCreator.getLoggedSuper().getActiveActorId()).getNet();
-        testHelper.login(superCreator.getSuperIdentity());
-        testCase = importHelper.createCase("Case permissions", process);
-        testHelper.logout();
+    @Test
+    public void testViewPermissionsOrdered() throws InterruptedException, IOException, MissingPetriNetMetaDataException {
+        testCase = createTestCase("task_authorization_test");
+        buildSearchRequest(testCase, "t_getdata");
+        Thread.sleep(2000);
+
+        assertWithoutRole();
+        assertWithAddedPosProcessRole();
+        assertWithAddedNegProcessRole();
+        assertWithAddedPosCaseRole();
+        assertWithAddedNegCaseRole();
+        assertWithAddedAdminAppRole();
     }
 
-    private void buildSearchRequest() {
-        request = new CaseSearchRequest(Map.of("stringId", List.of(testCase.getStringId())));
+    @Test
+    public void testViewWithDefaultPermission() throws IOException, MissingPetriNetMetaDataException, InterruptedException {
+        Case testCaseWithDefault = createTestCase("task_authorization_default_test");
+        Thread.sleep(2000);
+
+        String transId = "t_getdata";
+        buildSearchRequest(testCaseWithDefault, transId);
+        Page<Task> pagedResult = doSearch();
+        assert pagedResult.hasContent();
+        assert pagedResult.getTotalElements() == 1;
+        assert pagedResult.getContent().get(0).getTransitionId().equals(transId);
+
+        transId = "t_getdata_disabled";
+        buildSearchRequest(testCaseWithDefault, transId);
+        pagedResult = doSearch();
+        assert pagedResult.hasContent();
+        assert pagedResult.getTotalElements() == 1;
+        assert pagedResult.getContent().get(0).getTransitionId().equals(transId);
     }
 
     /**
      * todo javadoc
      * */
     private void assertWithoutRole() {
-        Page<Case> pagedResult = doSearch();
+        Page<Task> pagedResult = doSearch();
         assert !pagedResult.hasContent();
     }
 
@@ -146,7 +136,7 @@ public class ElasticCaseSearchPermissionTest {
     private void assertWithAddedPosProcessRole() {
         assignProcessRole(processRoleRepository.findByImportId("pos_process_role"));
 
-        Page<Case> pagedResult = doSearch();
+        Page<Task> pagedResult = doSearch();
         assert pagedResult.hasContent();
         assert pagedResult.getTotalElements() == 1;
     }
@@ -157,7 +147,7 @@ public class ElasticCaseSearchPermissionTest {
     private void assertWithAddedNegProcessRole() {
         assignProcessRole(processRoleRepository.findByImportId("neg_process_role"));
 
-        Page<Case> pagedResult = doSearch();
+        Page<Task> pagedResult = doSearch();
         assert !pagedResult.hasContent();
     }
 
@@ -167,7 +157,7 @@ public class ElasticCaseSearchPermissionTest {
     private void assertWithAddedPosCaseRole() {
         assignCaseRole(caseRoleRepository.findByCaseIdAndImportId(testCase.getStringId(), "pos_case_role"));
 
-        Page<Case> pagedResult = doSearch();
+        Page<Task> pagedResult = doSearch();
         assert pagedResult.hasContent();
         assert pagedResult.getTotalElements() == 1;
     }
@@ -178,7 +168,7 @@ public class ElasticCaseSearchPermissionTest {
     private void assertWithAddedNegCaseRole() {
         assignCaseRole(caseRoleRepository.findByCaseIdAndImportId(testCase.getStringId(), "neg_case_role"));
 
-        Page<Case> pagedResult = doSearch();
+        Page<Task> pagedResult = doSearch();
         assert !pagedResult.hasContent();
     }
 
@@ -188,13 +178,25 @@ public class ElasticCaseSearchPermissionTest {
     private void assertWithAddedAdminAppRole() {
         assignAppRole(applicationRoleRunner.getAppRole(ApplicationRoleRunner.ADMIN_APP_ROLE));
 
-        Page<Case> pagedResult = doSearch();
+        Page<Task> pagedResult = doSearch();
         assert pagedResult.hasContent();
         assert pagedResult.getTotalElements() == 1;
     }
 
-    private Page<Case> doSearch() {
-        return elasticCaseService.search(List.of(request), testIdentity.toSession(), PageRequest.of(0, 2),
+    private Case createTestCase(String identifier) throws IOException, MissingPetriNetMetaDataException {
+        Process process = petriNetService.importPetriNet(new FileInputStream(String.format("src/test/resources/petriNets/%s.xml", identifier)),
+                VersionType.MAJOR, superCreator.getLoggedSuper().getActiveActorId()).getNet();
+        testHelper.login(superCreator.getSuperIdentity());
+        Case testCase = importHelper.createCase("Task permissions", process);
+        testHelper.logout();
+        return testCase;
+    }
+    private void buildSearchRequest(Case testCase, String transitionId) {
+        request = new ElasticTaskSearchRequest(String.format("stringId:%s", testCase.getTaskStringId(transitionId)));
+    }
+
+    private Page<Task> doSearch() {
+        return elasticTaskService.search(List.of(request), testIdentity.toSession(), PageRequest.of(0, 2),
                 Locale.getDefault(), false);
     }
 
@@ -219,6 +221,4 @@ public class ElasticCaseSearchPermissionTest {
         assignment.setRoleId(role.getStringId());
         roleAssignmentRepository.save(assignment);
     }
-
-
 }
