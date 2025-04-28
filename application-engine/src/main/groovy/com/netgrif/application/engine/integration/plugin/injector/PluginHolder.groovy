20fe016bd8d979b22da470a6db56027a492663eb
Milan Mladoniczky <6153201+tuplle@users.noreply.github.com>
2025-04-28 23:26:56 +0200

    [NAE-2093] Remove dependency on plugin-core NAE
    Remove unused plugin-related classes and configurations
    - Deleted plugin injector, meta classes, and associated configurations.
    - Removed exception classes, utility, service, and security classes tied to plugins.
    - Cleared plugin runner logic and redundant method/documentation references.

--- application-engine/src/main/groovy/com/netgrif/application/engine/integration/plugin/injector/PluginHolder.groovy
+++
@@ -1,7 +0,0 @@
-package com.netgrif.application.engine.integration.plugin.injector;
-
-/**
- * A class where the plugins are injected. The plugins are injected into metaclasses
- * */
-class PluginHolder {
-}
