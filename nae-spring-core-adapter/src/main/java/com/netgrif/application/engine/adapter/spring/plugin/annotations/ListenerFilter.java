53de537d861d5d4160d2421865f4cc8489fcf8dd
Milan Mladoniczky <6153201+tuplle@users.noreply.github.com>
2025-06-21 23:34:54 +0200

    Add annotations for ListenerFilter
    - Introduced @Target and @Retention annotations for ListenerFilter definition.
    - Ensured compatibility with method-level usage and set retention policy at runtime.

--- nae-spring-core-adapter/src/main/java/com/netgrif/application/engine/adapter/spring/plugin/annotations/ListenerFilter.java
+++ nae-spring-core-adapter/src/main/java/com/netgrif/application/engine/adapter/spring/plugin/annotations/ListenerFilter.java
@@ -2,8 +2,14 @@ package com.netgrif.application.engine.adapter.spring.plugin.annotations;
 
 import com.netgrif.application.engine.objects.event.dispatchers.common.AbstractDispatcher;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.EventObject;
 
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ListenerFilter {
     Class<? extends EventObject> eventType();
     AbstractDispatcher.DispatchMethod dispatchMethod();
