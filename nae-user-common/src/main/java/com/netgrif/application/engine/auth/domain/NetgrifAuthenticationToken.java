6719ae65f5c5bf8ac313d12e01e3f1611466ed39
chvostek <chvostek@netgrif.com>
2025-06-24 12:48:50 +0200

    [NAE-2127] Implement DelegatingContextFilter for Multi-Realm Support - add realm attribute in NetgrifAuthenticationToken

--- nae-user-common/src/main/java/com/netgrif/application/engine/auth/domain/NetgrifAuthenticationToken.java
+++ nae-user-common/src/main/java/com/netgrif/application/engine/auth/domain/NetgrifAuthenticationToken.java
@@ -1,6 +1,7 @@
 package com.netgrif.application.engine.auth.domain;
 
 
+import com.netgrif.application.engine.objects.auth.domain.Realm;
 import lombok.Getter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
@@ -8,10 +9,18 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 public class NetgrifAuthenticationToken extends UsernamePasswordAuthenticationToken {
 
     private final String realmId;
+    private final Realm realm;
 
     public NetgrifAuthenticationToken(Object principal, Object credentials, String realmId) {
         super(principal, credentials);
         this.realmId = realmId;
+        this.realm = null;
+    }
+
+    public NetgrifAuthenticationToken(Object principal, Object credentials, Realm realm) {
+        super(principal, credentials);
+        this.realm = realm;
+        this.realmId = realm != null ? realm.getId() : null;
     }
 
 }
