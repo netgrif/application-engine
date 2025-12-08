cd27b364fe65a93dce04ccae311724bb3f3c37c6
renczesstefan <renczes.stefan@gmail.com>
2025-12-08 19:42:02 +0100

    Add support for handling TaskField in ElasticCaseMapping
    Introduced the TaskField class to handle task references in elastic mappings. Updated ElasticCaseMappingService to transform TaskField objects and integrated this functionality into the application engine. This enhancement ensures proper indexing and retrieval of task-related data in Elasticsearch.

---
+++ nae-object-library/src/main/java/com/netgrif/application/engine/objects/elastic/domain/TaskField.java
@@ -0,0 +1,26 @@
+package com.netgrif.application.engine.objects.elastic.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public abstract class TaskField extends DataField {
+
+    public String[] taskRefValue;
+
+    public TaskField(String[] values) {
+        super(values);
+        this.taskRefValue = values;
+    }
+
+    @Override
+    public Object getValue() {
+        return new ArrayList<>(List.of(taskRefValue));
+    }
+}
