a18f14f50865d3feb911c1e59a93a722d3bf4e13
renczesstefan <renczes.stefan@gmail.com>
2025-12-09 08:55:08 +0100

    - added inherited to annotation

--- nae-object-library/src/main/java/com/netgrif/application/engine/objects/annotations/Indexable.java
+++ nae-object-library/src/main/java/com/netgrif/application/engine/objects/annotations/Indexable.java
@@ -1,10 +1,8 @@
 package com.netgrif.application.engine.objects.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface Indexable {
