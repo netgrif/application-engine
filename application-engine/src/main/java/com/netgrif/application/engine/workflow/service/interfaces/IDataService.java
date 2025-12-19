7860465d9ec0dc65eea13ded75cd9bc7b9facf7c
chvostek <chvostek@netgrif.com>
2025-12-19 09:46:34 +0100

    [NAE-2303] TaskRef Security Improvements - rename parameter

--- application-engine/src/main/java/com/netgrif/application/engine/workflow/service/interfaces/IDataService.java
+++ application-engine/src/main/java/com/netgrif/application/engine/workflow/service/interfaces/IDataService.java
@@ -41,7 +41,7 @@ public interface IDataService {
 
     SetDataEventOutcome setData(Task task, ObjectNode values, Map<String, String> params);
 
-    SetDataEventOutcome setData(Task task, ObjectNode values, Map<String, String> params, boolean runSafe);
+    SetDataEventOutcome setData(Task task, ObjectNode values, Map<String, String> params, boolean runStrict);
 
     FileFieldInputStream getFile(Case useCase, Task task, FileField field, boolean forPreview) throws FileNotFoundException;
 
