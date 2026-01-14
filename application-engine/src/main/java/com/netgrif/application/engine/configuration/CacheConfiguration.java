07aa67f58e5b8dba363bc0b87ba9f846919d3645
renczesstefan <renczes.stefan@gmail.com>
2026-01-14 10:18:16 +0100

    Remove unused import from CacheConfiguration.java
    The import for `ElementType` was unnecessary and has been removed to clean up the code. This change improves maintainability by keeping the imports list concise and relevant.

--- application-engine/src/main/java/com/netgrif/application/engine/configuration/CacheConfiguration.java
+++ application-engine/src/main/java/com/netgrif/application/engine/configuration/CacheConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 
-import java.lang.annotation.ElementType;
 import java.util.*;
 import java.util.stream.Collectors;
 
