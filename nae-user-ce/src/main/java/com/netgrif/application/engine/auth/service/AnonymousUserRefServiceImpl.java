8d3923f9c7314d5541b41ca44870e00c9d920c12
renczesstefan <renczes.stefan@gmail.com>
2026-02-02 15:45:25 +0100

    Add AnonymousUserRefService bean to AuthBeansConfiguration
    The AnonymousUserRefServiceImpl class was decoupled from the @Service annotation and is now registered as a bean in AuthBeansConfiguration. This change also ensures the bean is only created if a UserFactory bean is missing, improving configurability and flexibility.

--- nae-user-ce/src/main/java/com/netgrif/application/engine/auth/service/AnonymousUserRefServiceImpl.java
+++ nae-user-ce/src/main/java/com/netgrif/application/engine/auth/service/AnonymousUserRefServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.Set;
 
-@Service
 public class AnonymousUserRefServiceImpl implements AnonymousUserRefService {
 
     @Autowired
