9f19f9536754ca381c56d4a332850844b44df7d6
renczesstefan <renczes.stefan@gmail.com>
2026-02-04 11:40:52 +0100

    [NAE-2369] Remove global functions of cache
    Replaced `reloadCachedGlobalFunctions` with a new method `removeCachedPetriNetFunctions` in `PetriNetService` and adjusted its usage to improve cache management. Updated the `FieldActionsCacheService` to evict specific cache entries more explicitly, ensuring proper cleanup.

--- application-engine/src/main/java/com/netgrif/application/engine/petrinet/service/PetriNetService.java
+++ application-engine/src/main/java/com/netgrif/application/engine/petrinet/service/PetriNetService.java
@@ -653,7 +653,7 @@ public class PetriNetService implements IPetriNetService {
         publisher.publishEvent(new ProcessDeleteEvent(petriNet, EventPhase.PRE));
         repository.deleteBy_id(petriNet.getObjectId());
         evictCache(petriNet);
-        functionCacheService.reloadCachedGlobalFunctions(petriNet);
+        functionCacheService.removeCachedPetriNetFunctions(petriNet.getIdentifier());
         if (petriNet.isDefaultVersion()) {
             PetriNet processToMakeDefault = self.getLatestVersionByIdentifier(petriNet.getIdentifier());
             if (processToMakeDefault != null) {
