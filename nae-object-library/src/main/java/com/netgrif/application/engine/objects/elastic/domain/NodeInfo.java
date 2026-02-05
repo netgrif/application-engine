bc581fa7646cc8a278dd61791952323313a7be8b
Tomáš Kováčik <kovacik@netgrif.com>
2026-02-05 12:21:45 +0100

    [NAE-2261] SEARCH Node - fetch cases from mongo
    - add NoArgsConstructor

--- nae-object-library/src/main/java/com/netgrif/application/engine/objects/elastic/domain/NodeInfo.java
+++ nae-object-library/src/main/java/com/netgrif/application/engine/objects/elastic/domain/NodeInfo.java
@@ -2,12 +2,14 @@ package com.netgrif.application.engine.objects.elastic.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NodeInfo implements Serializable {
 
     @Serial
