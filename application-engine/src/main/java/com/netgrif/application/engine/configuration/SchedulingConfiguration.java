5f22bd34aa6cb9b559fb1ea1e64e644ca011dc7f
chvostek <chvostek@netgrif.com>
2026-02-16 15:47:23 +0100

    [NAE-2072] Workspaces - implement NAE thread interfaces to handle security context propagation - wrap existing task executors in DelegatingSecurityContextTaskScheduler or DelegatingSecurityContextAsyncTaskExecutor - implement WorkspaceAsyncTest - introduce Case.findTaskId

--- application-engine/src/main/java/com/netgrif/application/engine/configuration/SchedulingConfiguration.java
+++ application-engine/src/main/java/com/netgrif/application/engine/configuration/SchedulingConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.security.scheduling.DelegatingSecurityContextTaskScheduler;
 
 @EnableScheduling
 @Configuration
@@ -12,6 +13,6 @@ public class SchedulingConfiguration {
 
     @Bean
     public TaskScheduler taskScheduler() {
-        return new ThreadPoolTaskScheduler();
+        return new DelegatingSecurityContextTaskScheduler(new ThreadPoolTaskScheduler());
     }
 }
