07e1b60d43caec3a49d7c6532f74e7c0a595cca7
renczesstefan <renczes.stefan@gmail.com>
2026-04-17 12:27:28 +0200

    [NAE-2409] Implement Plugin save/get file over gRPC for worker
    Integrated new debug and trace logging to monitor core API methods such as data retrieval, task and case management, and file operations. This enhances traceability and simplifies troubleshooting by capturing detailed runtime information for key API calls.

--- application-engine/src/main/java/com/netgrif/application/engine/actions/ActionApiImpl.java
+++ application-engine/src/main/java/com/netgrif/application/engine/actions/ActionApiImpl.java
@@ -92,42 +92,51 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public GetDataEventOutcome getData(String taskId, Map<String, String> params) {
+        log.debug("Getting data for task [{}] with params: [{}]", taskId, params == null ? "null" : params.toString());
         return dataService.getData(taskId, params);
     }
 
     @Override
     public SetDataEventOutcome setData(String taskId, Map<String, Map<String, String>> dataSet, Map<String, String> params) throws JsonProcessingException {
+        log.debug("Setting data for task [{}] with params: [{}]", taskId, params == null ? "null" : params.toString());
         ObjectMapper mapper = new ObjectMapper(); 
         String json = mapper.writeValueAsString(dataSet);
         ObjectNode values = (ObjectNode) mapper.readTree(json);
+        log.trace("Setting data for task [{}] with params: [{}], values: [{}]", taskId, params == null ? "null" : params.toString(), values.toString());
         return dataService.setData(taskId, values, params);
     }
 
     @Override
     public Page<Case> searchCases(String processIdentifier, Predicate predicate, Pageable pageable) {
+        log.debug("Searching cases for process identifier [{}] with predicate [{}], pageable [{}]", processIdentifier, predicate, pageable);
         return workflowService.search(predicate, pageable);
     }
 
     @Override
     public Page<Case> searchCases(List<String> elasticStringQueries, AuthPrincipalDto authPrincipalDto, Pageable pageable, Boolean isIntersection) {
+        log.debug("Searching cases for elastic queries [{}] with auth principal [{}], pageable [{}], intersect [{}]", elasticStringQueries, authPrincipalDto, pageable, isIntersection);
         boolean intersect = Boolean.TRUE.equals(isIntersection);
         List<CaseSearchRequest> caseSearchRequests = elasticStringQueries.stream().map(query -> CaseSearchRequest.builder().query(query).build()).toList();
         LoggedUser loggedUser = ActorTransformer.toLoggedUser(resolveAbstractUser(authPrincipalDto));
         Locale locale = LocaleContextHolder.getLocale();
+        log.trace("Searching cases for elastic queries [{}] with auth principal [{}], pageable [{}], intersect [{}], locale [{}]", elasticStringQueries, authPrincipalDto, pageable, isIntersection, locale);
         return elasticCaseService.search(caseSearchRequests, loggedUser, pageable, locale, intersect);
     }
 
     @Override
     public Long countCases(List<String> elasticStringQueries, AuthPrincipalDto authPrincipalDto, Boolean isIntersection) {
+        log.debug("Counting cases for elastic queries [{}] with auth principal [{}], intersect [{}]", elasticStringQueries, authPrincipalDto, isIntersection);
         boolean intersect = Boolean.TRUE.equals(isIntersection);
         List<CaseSearchRequest> caseSearchRequests = elasticStringQueries.stream().map(query -> CaseSearchRequest.builder().query(query).build()).toList();
         LoggedUser loggedUser = ActorTransformer.toLoggedUser(resolveAbstractUser(authPrincipalDto));
         Locale locale = LocaleContextHolder.getLocale();
+        log.trace("Counting cases for elastic queries [{}] with auth principal [{}], intersect [{}], locale [{}]", elasticStringQueries, authPrincipalDto, isIntersection, locale);
         return elasticCaseService.count(caseSearchRequests, loggedUser, locale, intersect);
     }
 
     @Override
     public CreateCaseEventOutcome createCaseByIdentifier(String identifier, String title, String color, AuthPrincipalDto authPrincipalDto, Map<String, String> params) {
+        log.debug("Creating case with identifier [{}] and title [{}] and color [{}] with auth principal [{}] and params [{}]", identifier, title, color, authPrincipalDto, params);
         Locale locale = LocaleContextHolder.getLocale();
         return workflowService.createCase(CreateCaseParams.with()
                 .processIdentifier(identifier)
@@ -141,6 +150,7 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public DeleteCaseEventOutcome deleteCase(String caseId, Map<String, String> params) {
+        log.debug("Deleting case with id [{}] and params [{}]", caseId, params);
         return workflowService.deleteCase(DeleteCaseParams.with()
                 .useCaseId(caseId)
                 .params(params)
@@ -149,20 +159,24 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public Page<Task> searchTasks(String processIdentifier, Predicate predicate, Pageable pageable) {
+        log.debug("Searching tasks for process identifier [{}] with predicate [{}], pageable [{}]", processIdentifier, predicate, pageable);
         return taskService.search(predicate, pageable);
     }
 
     @Override
     public Page<Task> searchTasks(List<String> elasticStringQueries, AuthPrincipalDto authPrincipalDto, Pageable pageable, Boolean isIntersection) {
+        log.debug("Searching tasks for elastic queries [{}] with auth principal [{}], pageable [{}], intersect [{}]", elasticStringQueries, authPrincipalDto, pageable, isIntersection);
         boolean intersect = Boolean.TRUE.equals(isIntersection);
         List<ElasticTaskSearchRequest> taskSearchRequests = elasticStringQueries.stream().map(query -> ElasticTaskSearchRequest.builder().query(query).build()).toList();
         LoggedUser loggedUser = ActorTransformer.toLoggedUser(resolveAbstractUser(authPrincipalDto));
         Locale locale = LocaleContextHolder.getLocale();
+        log.trace("Searching tasks for elastic queries [{}] with auth principal [{}], pageable [{}], intersect [{}], locale [{}]", elasticStringQueries, authPrincipalDto, pageable, isIntersection, locale);
         return elasticTaskService.search(taskSearchRequests, loggedUser, pageable, locale, intersect);
     }
 
     @Override
     public AssignTaskEventOutcome assignTask(String taskId, AuthPrincipalDto authPrincipalDto, Map<String, String> params) throws TransitionNotExecutableException {
+        log.debug("Assigning task [{}] with auth principal [{}] and params [{}]", taskId, authPrincipalDto, params);
         Task task = taskService.findOne(taskId);
         AbstractUser user = resolveAbstractUser(authPrincipalDto);
         return taskService.assignTask(TaskParams.with()
@@ -174,6 +188,7 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public CancelTaskEventOutcome cancelTask(String taskId, AuthPrincipalDto authPrincipalDto, Map<String, String> params) {
+        log.debug("Canceling task [{}] with auth principal [{}] and params [{}]", taskId, authPrincipalDto, params);
         Task task = taskService.findOne(taskId);
         AbstractUser user = resolveAbstractUser(authPrincipalDto);
         return taskService.cancelTask(TaskParams.with()
@@ -185,6 +200,7 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public FinishTaskEventOutcome finishTask(String taskId, AuthPrincipalDto authPrincipalDto, Map<String, String> params) throws TransitionNotExecutableException {
+        log.debug("Finishing task [{}] with auth principal [{}] and params [{}]", taskId, authPrincipalDto, params);
         Task task = taskService.findOne(taskId);
         AbstractUser user = resolveAbstractUser(authPrincipalDto);
         return taskService.finishTask(TaskParams.with()
@@ -196,16 +212,19 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public Case findCase(String caseId) {
+        log.debug("Finding case with id [{}]", caseId);
         return workflowService.findOne(caseId);
     }
 
     @Override
     public Task findTask(String taskId) {
+        log.debug("Finding task with id [{}]", taskId);
         return taskService.findOne(taskId);
     }
 
     @Override
     public Page<User> searchUsers(Predicate predicate, Pageable pageable, String realmId) {
+        log.debug("Searching users with predicate [{}] and pageable [{}] and realm ID [{}]", predicate, pageable, realmId);
         return userService.search(predicate, pageable, realmId);
     }
 
@@ -216,31 +235,39 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public SetDataEventOutcome saveFile(String taskId, String fieldId, ActionFileHolder file, Map<String, String> params) {
+        log.debug("Saving file [{}] for task [{}] and field [{}] with params [{}]", file.getFileName(), taskId, fieldId, params);
         MultipartFile multipartFile = new MockMultipartFile(file.getFileName(), file.getFileContent());
+        log.trace("Saving file [{}] for task [{}] and field [{}] with params [{}]", multipartFile.getOriginalFilename(), taskId, fieldId, params);
         return dataService.saveFile(taskId, fieldId, multipartFile, params);
     }
 
     @Override
     public SetDataEventOutcome saveFiles(String taskId, String fieldId, ActionFileHolder[] files, Map<String, String> params) {
+        log.debug("Saving files [{}] for task [{}] and field [{}] with params [{}]", files.length, taskId, fieldId, params);
         MultipartFile[] multipartFiles = new MultipartFile[files.length];
         for (int i = 0; i < files.length; i++) {
-            multipartFiles[i] = new MockMultipartFile(files[i].getFileName(), files[i].getFileContent());
+            multipartFiles[i] = new MockMultipartFile(files[i].getFileName(), files[i].getFileName(), null, files[i].getFileContent());
+            log.trace("Saving file [{}] for task [{}] and field [{}] with params [{}]", multipartFiles[i].getOriginalFilename(), taskId, fieldId, params);
         }
+        log.trace("Saving files [{}] for task [{}] and field [{}] with params [{}]", multipartFiles.length, taskId, fieldId, params);
         return dataService.saveFiles(taskId, fieldId, multipartFiles, params);
     }
 
     @Override
     public SetDataEventOutcome deleteFile(String taskId, String fieldId, Map<String, String> params) {
+        log.debug("Deleting file for task [{}] and field [{}] with params [{}]", taskId, fieldId, params);
         return dataService.deleteFile(taskId, fieldId, params);
     }
 
     @Override
     public SetDataEventOutcome deleteFileByName(String taskId, String fieldId, String name, Map<String, String> params) {
+        log.debug("Deleting file [{}] for task [{}] and field [{}] with params [{}]", name, taskId, fieldId, params);
         return dataService.deleteFileByName(taskId, fieldId, name, params);
     }
 
     @Override
     public ActionFileHolder getFile(String caseId, String fieldId, boolean forPreview, Map<String, String> params) throws IOException {
+        log.debug("Getting file for case [{}] and field [{}] with preview [{}] and params [{}]", caseId, fieldId, forPreview, params);
         FileFieldInputStream fileFieldInputStream = dataService.getFile(caseId, fieldId, forPreview, params);
         return ActionFileHolder.builder()
                 .fileName(fileFieldInputStream.getFileName())
@@ -250,6 +277,7 @@ public class ActionApiImpl implements ActionApi {
 
     @Override
     public ActionFileHolder getFileByCaseAndName(String caseId, String fieldId, String name, Map<String, String> params) throws IOException {
+        log.debug("Getting file [{}] for case [{}] and field [{}] with params [{}]", name, caseId, fieldId, params);
         FileFieldInputStream fileFieldInputStream = dataService.getFileByCaseAndName(caseId, fieldId, name, params);
         return ActionFileHolder.builder()
                 .fileName(fileFieldInputStream.getFileName())
