d47b5e0d47154d51a2772d26fbcc513ad6a5caa6
renczesstefan <renczes.stefan@gmail.com>
2026-05-14 17:38:20 +0200

    Add support for additional parameters in case and task APIs
    Introduced methods to handle additional parameters across case and task search and count operations. Created a constants class for default process parameter names and added overloads with parameter maps to improve flexibility.

---
+++ nae-spring-core-adapter/src/main/java/com/netgrif/application/engine/adapter/spring/actions/ActionApiDefaultParamNames.java
@@ -0,0 +1,8 @@
+package com.netgrif.application.engine.adapter.spring.actions;
+
+public final class ActionApiDefaultParamNames {
+    public static final String PROCESS_IDENTIFIER = "processIdentifier";
+    public static final String PROCESS_ID = "processId";
+    public static final String PROCESS_VERSION = "processVersion";
+    public static final String PROCESS_RESOURCE_ID = "processResourceId";
+}
