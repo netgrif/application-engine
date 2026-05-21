8420cf62ce33591aabd1b740162c24918fad041b
renczesstefan <renczes.stefan@gmail.com>
2026-05-21 14:53:25 +0200

    Refactor package structure for LoggedUserConfiguration and DefaultLoggedUserFactory
    Moved classes to the `adapter.spring` package for better modularity and alignment with the Spring adapter layer. Simplified object creation in `DefaultLoggedUserFactory#create`.

--- nae-spring-core-adapter/src/main/java/com/netgrif/application/engine/adapter/spring/configuration/LoggedUserConfiguration.java
+++ nae-spring-core-adapter/src/main/java/com/netgrif/application/engine/adapter/spring/configuration/LoggedUserConfiguration.java
@@ -1,6 +1,6 @@
-package com.netgrif.application.engine.configuration;
+package com.netgrif.application.engine.adapter.spring.configuration;
 
-import com.netgrif.application.engine.auth.service.DefaultLoggedUserFactory;
+import com.netgrif.application.engine.adapter.spring.auth.service.DefaultLoggedUserFactory;
 import com.netgrif.application.engine.objects.auth.domain.ActorTransformer;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
