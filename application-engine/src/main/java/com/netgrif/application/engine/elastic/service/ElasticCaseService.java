702824cf1eba5ce8a5a44b0c1003e429f1718d9e
renczesstefan <renczes.stefan@gmail.com>
2026-08-03 15:29:32 +0200

    Refactor `ElasticCaseService`: use `Matcher.quoteReplacement` for safe string replacement in full-text search normalization

--- application-engine/src/main/java/com/netgrif/application/engine/elastic/service/ElasticCaseService.java
+++ application-engine/src/main/java/com/netgrif/application/engine/elastic/service/ElasticCaseService.java
@@ -43,6 +43,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.function.BinaryOperator;
+import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -556,7 +557,7 @@ public class ElasticCaseService extends ElasticViewPermissionService implements 
     }
 
     private List<String> normalizeFullTextSearch(String fullText) {
-        return Arrays.stream(fullText
+        return Arrays.stream(Matcher.quoteReplacement(fullText)
                         .replace("\\\\", "")
                         .replaceAll("\\s+", " ")
                         .trim()
