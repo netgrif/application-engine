0873f41dc4addfdec62e035ae2162359f8827a87
chvostek <chvostek@netgrif.com>
2026-08-17 09:58:56 +0200

    [NAE-2475] Advanced search is broken - add javadoc

--- src/main/groovy/com/netgrif/application/engine/validation/models/FilterFieldValidation.groovy
+++ src/main/groovy/com/netgrif/application/engine/validation/models/FilterFieldValidation.groovy
@@ -5,12 +5,39 @@ import com.netgrif.application.engine.pfql.service.QueryLangEvaluator
 import com.netgrif.application.engine.pfql.service.utils.SearchUtils
 import com.netgrif.application.engine.validation.domain.ValidationDataInput
 
+/**
+ * Abstract base class for validating filter field values against PFQL queries.
+ * <p>
+ * This class provides common validation logic for filter fields that need to verify
+ * whether their values represent valid PFQL queries of a specific type.
+ * </p>
+ */
 abstract class FilterFieldValidation extends AbstractFieldValidation {
-    // todo 2466 generate doc
+
+    /**
+     * Validates the filter field value as a PFQL query.
+     * <p>
+     * Implementations must specify the expected query type for validation.
+     * </p>
+     *
+     * @param validationData the validation data input containing the field value and validation context
+     */
     abstract void query(ValidationDataInput validationData)
 
-    // todo 2466 generate doc
-    protected void doValidation(ValidationDataInput validationData, QueryType queryType) {
+    /**
+     * Performs validation of the field value against a specific PFQL query type.
+     * <p>
+     * This method checks if the field value is a valid PFQL query and verifies that
+     * it matches the expected query type. If the value is null or empty, no validation
+     * is performed. If the query is invalid or does not match the expected type,
+     * an IllegalArgumentException is thrown with the localized validation message.
+     * </p>
+     *
+     * @param validationData the validation data input containing the field value, locale, and validation message
+     * @param queryType the expected PFQL query type that the field value should match
+     * @throws IllegalArgumentException if the query is invalid or does not match the expected type
+     */
+    protected static void doValidation(ValidationDataInput validationData, QueryType queryType) {
         if (validationData.getData().getValue() != null && validationData.getData().getValue() != "") {
             try {
                 QueryLangEvaluator evaluator = SearchUtils.evaluateQuery(validationData.getData().getValue() as String)
